b15b668e8264b60c8e63ff01280a1a70b9850796
Ningyuan Xie <howard.xie25@gmail.com>
2024-06-04 00:48:03 -0400

    Update 35

---
+++ 0035-Search-Insert-Position.py
@@ -0,0 +1,31 @@
+# Link: https://leetcode.com/problems/search-insert-position/
+# Difficulty: Easy
+# Description: Given a sorted array of distinct integers and a target value,
+# return the index if the target is found.
+# If not, return the index where it would be if it were inserted in order.
+
+class Solution:
+    @staticmethod
+    def searchInsert(nums: list[int], target: int) -> int:
+        # Loop through the index of the numbers in the list
+        for i in range(len(nums)):
+            # If the target is found, return the index
+            if nums[i] == target:
+                return i
+            # If the target is less than the number at the index, return the index
+            if nums[i] > target:
+                return i
+        # If the target is greater than all the numbers in the list, return the length of the list
+        return len(nums)
+
+
+# Unit Test: Input: nums = [1, 3, 5, 6], target = 5, Output: 2
+assert Solution.searchInsert([1, 3, 5, 6], 5) == 2
+
+# Unit Test: Input: nums = [1, 3, 5, 6], target = 2, Output: 1
+assert Solution.searchInsert([1, 3, 5, 6], 2) == 1
+
+# Unit Test: Input: nums = [1, 3, 5, 6], target = 7, Output: 4
+assert Solution.searchInsert([1, 3, 5, 6], 7) == 4
+print("All unit tests are passed")
+
