cdca71df7dbd23217f45e9348453496be29ec915
Ningyuan Xie <howard.xie25@gmail.com>
2024-06-09 18:49:55 -0400

    Update 21

--- 01-Easy/0021-Merge-Two-Sorted-Lists.py
+++ 01-Easy/0021-Merge-Two-Sorted-Lists.py
@@ -9,45 +9,44 @@ class ListNode:
         self.val = val
         self.next = next
 
-
 class Solution:
     @staticmethod
     def mergeTwoLists(l1: ListNode, l2: ListNode) -> ListNode:
-        dummy = ListNode()
-        tail = dummy
-        
+        # Dummy node to keep track of the head of the linked list
+        dummy = ListNode() # dummy: 0 -> None
+        # Tail node to keep track of the last node of the linked list
+        tail = dummy # tail: 0 -> None
+        # both dummy and tail refer to the same mutable object
+
         # Traverse both linked lists until one of them is empty
         while l1 and l2:
             if l1.val < l2.val:
-                tail.next = l1
+                tail.next = l1 # append the whole l1, not just a value
                 l1 = l1.next
             else:
-                tail.next = l2
+                tail.next = l2 # append the whole l2, not just a value
                 l2 = l2.next
             # Move the tail to the next node after appending
             tail = tail.next
-        
+
         # Append the remaining nodes of l1 or l2
         if l1:
             tail.next = l1
         elif l2:
             tail.next = l2
-        
-        return dummy.next
+        # return dummy.next to exclude the head node
+        return dummy.next # dummy: 0 -> 1 -> 1 -> 2 -> 3 -> 4 -> 4 -> None
 
+    # Helper function to print the linked list
+    def printLinkedList(l: ListNode):
+        while l:
+            print(l.val, end=" -> ")
+            l = l.next
+        print("None")
 
 # Unit Test: Input: l1 = [1,2,4], l2 = [1,3,4], Output: [1,1,2,3,4,4]
-list1 = ListNode(1)
-list1.next = ListNode(2)
-list1.next.next = ListNode(4)
-list2 = ListNode(1)
-list2.next = ListNode(3)
-list2.next.next = ListNode(4)
+list1 = ListNode(1, ListNode(2, ListNode(4)))
+list2 = ListNode(1, ListNode(3, ListNode(4)))
 list3 = Solution.mergeTwoLists(list1, list2)
-assert list3.val == 1
-assert list3.next.val == 1
-assert list3.next.next.val == 2
-assert list3.next.next.next.val == 3
-assert list3.next.next.next.next.val == 4
-assert list3.next.next.next.next.next.val == 4
+Solution.printLinkedList(list3)
 print("All unit tests are passed")
