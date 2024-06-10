37ee160c81da382222bc63fb2efe2986197cf8bb
Ningyuan Xie <howard.xie25@gmail.com>
2024-06-10 11:57:15 -0400

    update version checker for python

--- version-checker.py
+++ version-checker.py
@@ -5,7 +5,9 @@ import sys
 def main():
     # Get the Python version information
     version_info = sys.version_info
-    # Convert the version information into a string
+    # Convert the version information into a string through map function and join method
+    # The map function takes a list and applies a function to each member of the list
+    # and returns a second list that is the same size as the first
     version = ".".join(map(str, version_info[:3]))
     print("Python version:", version)
 
