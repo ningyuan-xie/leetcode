0decccbadea646e72c62a318edd552d984d28fa4
Ningyuan Xie <howard.xie25@gmail.com>
2024-06-24 01:05:31 -0400

    Update 141

--- 01-Easy/0141-Linked-List-Cycle.py
+++ 01-Easy/0141-Linked-List-Cycle.py
@@ -9,33 +9,37 @@ from package.data_structures import ListNode
 
 
 class Solution:
+    # Floyd's Tortoise and Hare (Cycle Detection) Algorithm
     @staticmethod
     def hasCycle(head: ListNode) -> bool:
         # Initialize two pointers: slow and fast
-        slow = fast = head
+        slow, fast = head, head
         # Traverse the linked list
-        while fast and fast.next:
+        while fast and fast.next:  # make sure fast.next also exists as we shift fast by two nodes
             # Move slow pointer by one node
             slow = slow.next
             # Move fast pointer by two nodes
             fast = fast.next.next
-            # Check if the slow and fast pointers meet
+            # If there is a cycle, the slow and fast pointers will meet
             if slow == fast:
                 return True
         return False
 
 
 # Unit Test: Input: head = [3,2,0,-4], pos = 1, Output: True
+# There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
 head1 = ListNode(3, ListNode(2, ListNode(0, ListNode(-4))))
-head1.next.next.next.next = head1.next
+head1.next.next.next.next = head1.next  # Connect the tail to the 1st node
 assert Solution.hasCycle(head1) == True
 
 # Unit Test: Input: head = [1,2], pos = 0, Output: True
+# There is a cycle in the linked list, where the tail connects to the 0th node (0-indexed).
 head2 = ListNode(1, ListNode(2))
-head2.next.next = head2
+head2.next.next = head2  # Connect the tail to the 0th node
 assert Solution.hasCycle(head2) == True
 
 # Unit Test: Input: head = [1], pos = -1, Output: False
+# There is no cycle in the linked list.
 head3 = ListNode(1)
 assert Solution.hasCycle(head3) == False
 
