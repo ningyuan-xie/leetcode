dd332f0159630e691f533d10ce9ee737455d9899
Ningyuan Xie <howard.xie25@gmail.com>
2024-06-25 18:58:18 -0400

    Update 168

--- 01-Easy/0168-Excel-Sheet-Column-Title.py
+++ 01-Easy/0168-Excel-Sheet-Column-Title.py
@@ -12,8 +12,9 @@ class Solution:
             # Subtract 1 from columnNumber to make it 0-indexed for each digit
             columnNumber -= 1
             # Append the character corresponding to the remainder of columnNumber divided by 26
+            remainder = columnNumber % 26
             # 65 is the ASCII value of 'A': chr(65) = 'A', chr(66) = 'B', ..., chr(90) = 'Z'
-            result = chr(columnNumber % 26 + 65) + result  # Append char to the left: 'A' + 'B' = 'AB'
+            result = chr(remainder + 65) + result  # Append char to the left: 'A' + 'B' = 'AB'
             # Divide columnNumber by 26 using floor division "//" to get the next digit
             columnNumber //= 26
         return result
