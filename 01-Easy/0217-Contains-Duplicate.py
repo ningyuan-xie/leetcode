5a20020dff109b039079700e523c894909649c46
Ningyuan Xie <howard.xie25@gmail.com>
2024-06-30 20:25:27 -0400

    Update 217

---
+++ 01-Easy/0217-Contains-Duplicate.py
@@ -0,0 +1,34 @@
+# Link: https://leetcode.com/problems/contains-duplicate/
+# Difficulty: Easy
+# Description: Given an integer array nums, return true if any value
+# appears at least twice in the array, and return false if every element is distinct.
+
+from typing import List
+
+
+class Solution:
+    # Similar to 0202-Happy-Number.py
+    @staticmethod
+    def containsDuplicate(nums: List[int]) -> bool:
+        # Initialize a set to store the UNIQUE elements
+        unique_set = set()
+        # Iterate through the list
+        for num in nums:
+            # Check if the element is already in the set
+            if num in unique_set:
+                return True
+            # Otherwise, add the element to the set
+            unique_set.add(num)
+        return False
+
+
+# Unit Test: Input: nums = [1,2,3,1], Output: True
+assert Solution.containsDuplicate([1, 2, 3, 1]) == True
+
+# Unit Test: Input: nums = [1,2,3,4], Output: False
+assert Solution.containsDuplicate([1, 2, 3, 4]) == False
+
+# Unit Test: Input: nums = [1,1,1,3,3,4,3,2,4,2], Output: True
+assert Solution.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]) == True
+
+print("All unit tests are passed")
