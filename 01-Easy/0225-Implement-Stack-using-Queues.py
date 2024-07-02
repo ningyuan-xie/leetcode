fd882355fba7012edd513ab657d08c81f379d6f8
Ningyuan Xie <howard.xie25@gmail.com>
2024-07-02 02:22:02 -0400

    Update 225

--- 01-Easy/0225-Implement-Stack-using-Queues.py
+++ 01-Easy/0225-Implement-Stack-using-Queues.py
@@ -2,6 +2,7 @@
 # Difficulty: Easy
 # Description: Implement a last-in-first-out (LIFO) stack using only two queues.
 # The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
 from collections import deque
 
 
