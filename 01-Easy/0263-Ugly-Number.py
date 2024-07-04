0a86a9805c23e8327a4a64a9a3cf505354af1092
Ningyuan Xie <howard.xie25@gmail.com>
2024-07-04 13:46:14 -0400

    Update 263

---
+++ 01-Easy/0263-Ugly-Number.py
@@ -0,0 +1,33 @@
+# Link: https://leetcode.com/problems/ugly-number/
+# Difficulty: Easy
+# Description: Write a program to check whether a given number is an ugly number.
+# An ugly number is a positive number whose prime factors only include 2, 3, and/or 5.
+
+
+class Solution:
+    # Optimal Solution: Iterative Approach. Time Complexity: O(log(n)), Space Complexity: O(1)
+    @staticmethod
+    def isUgly(n: int) -> bool:
+        # Base case: if the number is less than or equal to 0, return False
+        if n <= 0:
+            return False
+
+        # Check if the number is divisible by 2, 3, and 5
+        for prime in [2, 3, 5]:
+            while n % prime == 0:  # Divisible by prime
+                n //= prime  # E.g., 6 // 2 = 3, 3 // 3 = 1; 8 // 2 = 4, 4 // 2 = 2, 2 // 2 = 1
+
+        # If the number is an ugly number, the result should be 1
+        return n == 1
+
+
+# Unit Test: Input: num = 6, Output: True
+assert Solution.isUgly(6) == True
+
+# Unit Test: Input: num = 8, Output: True
+assert Solution.isUgly(8) == True
+
+# Unit Test: Input: num = 14, Output: False
+assert Solution.isUgly(14) == False
+
+print("All unit tests are passed")
