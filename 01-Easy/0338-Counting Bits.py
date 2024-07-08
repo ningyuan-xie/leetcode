ad4710bf951358ae6f97d34b7de8466770161c71
Ningyuan Xie <howard.xie25@gmail.com>
2024-07-08 19:51:52 -0400

    Update 338

--- 01-Easy/0338-Counting Bits.py
+++ 01-Easy/0338-Counting Bits.py
@@ -8,29 +8,36 @@ from typing import List
 
 class Solution:
     # Optimal Solution: Dynamic Programming. Time Complexity: O(n), Space Complexity: O(n)
+    # Use the previous results to calculate the new results:
+    # 1 (0001) -> 1 + dp[n - 1] = 1 + dp[0] = 1;
+    # 2 (0010) -> 1 + dp[n - 2] = 1 + dp[0] = 1; 3 (0011) -> 1 + dp[n - 2] = 1 + dp[1] = 1 + 1 = 2;
+    # 4 (0100) -> 1 + dp[n - 4] = 1 + dp[0] = 1; 6 (0110) -> 1 + dp[n - 4] = 1 + dp[2] = 1 + 1 = 2;
+    # 8 (1000) -> 1 + dp[n - 8] = 1 + dp[0] = 1
     @staticmethod
     def countBits(n: int) -> List[int]:
         # Initialize an array to store the number of 1's in the binary representation
         # of each number from 0 to n
-        ans = [0] * (n + 1)
-
-        # Loop through each number from 1 to n
+        dp = [0] * (n + 1)
+        offset = 1
+        # Loop through each number from 1 to n, as 0 has 0 1's
         for i in range(1, n + 1):
-            # The number of 1's in the binary representation of i is equal to
-            # the number of 1's in the binary representation of i // 2
-            # plus 1 if i is odd, or 0 if i is even
-            ans[i] = ans[i // 2] + (i % 2)
-
-        return ans
+            if offset * 2 == i:  # reach the next power of 2, update the offset
+                offset = i  # 1, 2, 4, 8, 16, ...
+            dp[i] = 1 + dp[i - offset]
+        return dp
 
 
 # Unit Test: Input: n = 2, Output: [0, 1, 1]
+# Explanation: 0 -> 0, 1 -> 1, 2 -> 10
 assert Solution.countBits(2) == [0, 1, 1]
 
 # Unit Test: Input: n = 5, Output: [0, 1, 1, 2, 1, 2]
+# Explanation: 0 -> 0, 1 -> 1, 2 -> 10, 3 -> 11, 4 -> 100, 5 -> 101
 assert Solution.countBits(5) == [0, 1, 1, 2, 1, 2]
 
 # Unit Test: Input: n = 10, Output: [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2]
+# Explanation: 0 -> 0, 1 -> 1, 2 -> 10, 3 -> 11, 4 -> 100, 5 -> 101,
+# 6 -> 110, 7 -> 111, 8 -> 1000, 9 -> 1001, 10 -> 1010
 assert Solution.countBits(10) == [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2]
 
 print("All unit tests are passed")
