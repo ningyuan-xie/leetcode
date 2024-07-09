dca5677ca55fb2e67ebeafd3493b01f8f3aaa525
Ningyuan Xie <howard.xie25@gmail.com>
2024-07-09 10:22:40 -0400

    Update 349

---
+++ 01-Easy/0349-Intersection-of-Two-Arrays.py
@@ -0,0 +1,30 @@
+# Link: https://leetcode.com/problems/intersection-of-two-arrays/
+# Difficulty: Easy
+# Description: Given two integer arrays nums1 and nums2, return an array of their intersection.
+
+from typing import List
+
+
+class Solution:
+    # Optimal Solution: Two Sets. Time Complexity: O(n + m), Space Complexity: O(n + m)
+    @staticmethod
+    def intersection(nums1: List[int], nums2: List[int]) -> List[int]:
+        # Initialize two sets to store the unique elements of nums1 and nums2
+        set1, set2 = set(nums1), set(nums2)
+        # Initialize a list to store the intersection of nums1 and nums2
+        intersection = []
+        # Loop through each element in set1
+        for num in set1:
+            # If the element is in set2, add it to the intersection
+            if num in set2:
+                intersection.append(num)
+        return intersection
+
+
+# Unit Test: Input: nums1 = [1, 2, 2, 1], nums2 = [2, 2], Output: [2]
+assert Solution.intersection([1, 2, 2, 1], [2, 2]) == [2]
+
+# Unit Test: Input: nums1 = [4, 9, 5], nums2 = [9, 4, 9, 8, 4], Output: [9, 4]
+assert Solution.intersection([4, 9, 5], [9, 4, 9, 8, 4]) == [9, 4]
+
+print("All unit tests are passed")
