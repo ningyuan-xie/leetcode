6d2bb5ca677e29aefe98a6f59bf94a1cfdd4beb3
Ningyuan Xie <howard.xie25@gmail.com>
2024-07-12 23:17:03 -0400

    Update 409

---
+++ 01-Easy/0409-Longest-Palindrome.py
@@ -0,0 +1,38 @@
+# Link: https://leetcode.com/problems/longest-palindrome/
+# Difficulty: Easy
+# Description: Given a string s which consists of lowercase or uppercase letters,
+# return the length of the longest palindrome that can be built with those letters.
+
+class Solution:
+    # Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(1)
+    @staticmethod
+    def longestPalindrome(s: str) -> int:
+        # Initialize a dictionary to store the frequency of each character
+        char_freq = {}
+        # Initialize a variable to store the length of the longest palindrome
+        longest_palindrome = 0
+        # Iterate through each character in the string
+        for char in s:
+            # Increment the frequency of the character
+            char_freq[char] = char_freq.get(char, 0) + 1
+            # If the frequency of the character is even, add it to the length of the longest palindrome
+            if char_freq[char] % 2 == 0:
+                longest_palindrome += 2
+        # If there are any characters with odd frequency, add 1 to the length of the longest palindrome
+        for freq in char_freq.values():
+            if freq % 2 == 1:
+                longest_palindrome += 1
+                break  # break out of the loop since we only need to add 1 to the length
+        return longest_palindrome
+
+
+# Unit Test: Input: s = "abccccdd", Output: 7
+assert Solution.longestPalindrome("abccccdd") == 7
+
+# Unit Test: Input: s = "a", Output: 1
+assert Solution.longestPalindrome("a") == 1
+
+# Unit Test: Input: s = "bb", Output: 2
+assert Solution.longestPalindrome("bb") == 2
+
+print("All unit tests are passed")
