a52934a8cb5c5e59e7346880f690f7ee9a2507d3
Ningyuan Xie <howard.xie25@gmail.com>
2024-07-14 13:27:07 -0400

    Update 448

--- 01-Easy/0448-Find-All-Numbers-Disappeared-in-an-Array.py
+++ 01-Easy/0448-Find-All-Numbers-Disappeared-in-an-Array.py
@@ -2,30 +2,44 @@
 # Difficulty: Easy
 # Description: Given an array nums of n integers where nums[i] is in the range [1, n],
 # return an array of all the integers in the range [1, n] that do not appear in nums.
+# Follow up: Could you do it without extra space and in O(n) runtime?
+# You may assume the returned list does not count as extra space.
 
 from typing import List
 
 
 class Solution:
-    # Optimal Solution: Iteration. Time Complexity: O(n), Space Complexity: O(1)
+    # Optimal Solution: Negation. Time Complexity: O(n), Space Complexity: O(1)
+    # The trick is to view the numbers as indices and mark the presence of each number
     @staticmethod
     def findDisappearedNumbers(nums: List[int]) -> List[int]:
-        # Iterate through the array
-        for i in range(len(nums)):
-            # Get the absolute value of the current number
-            num = abs(nums[i])
-            # Get the index of the number
-            index = num - 1
-            # If the number at the index is positive, make it negative
-            if nums[index] > 0:
-                nums[index] *= -1
+        # The first loop marks indices as negative to denote the presence of each number
+        for num in nums:  # E.g. [4, 3, 2, 7, 8, 2, 3, 1], missing numbers: [5, 6]
+            # Get the 0-based index of the number
+            index = abs(num) - 1  # 3, 2, 1, 6, 7, 1, 2, 0
+            # Using negation allows us to mark the presence of a number without using extra space
+            nums[index] = -abs(nums[index])  # [-4, -3, -2, -7, -8, 2, 3, -1]
+            # nums[3] = -abs(nums[3]) = -abs(7) = -7, nums[2] = -abs(nums[2]) = -abs(2) = -2
+
+        # The second loop identifies the indices that remain positive, indicating the missing numbers
+        missing_nums = []
+        for i, num in enumerate(nums):
+            if num > 0:
+                missing_nums.append(i + 1)
+        return missing_nums
+
+    # Alternative Solution: Set. Time Complexity: O(n), Space Complexity: O(n)
+    @staticmethod
+    def findDisappearedNumbersSet(nums: List[int]) -> List[int]:
+        # Initialize the set of numbers
+        nums_set = set(nums)
         # Initialize the list of missing numbers
         missing_nums = []
-        # Iterate through the array
-        for i in range(len(nums)):
-            # If the number at the index is positive, add the index + 1 to the missing numbers
-            if nums[i] > 0:
-                missing_nums.append(i + 1)
+        # Iterate through the range of 1 to the length of the array
+        for i in range(1, len(nums) + 1):
+            # If the number is not in the set, add it to the missing numbers
+            if i not in nums_set:
+                missing_nums.append(i)
         return missing_nums
 
 
@@ -33,9 +47,9 @@ class Solution:
 assert Solution.findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]) == [5, 6]
 
 # Unit Test: Input: nums = [1, 1], Output: [2]
-assert Solution.findDisappearedNumbers([1, 1]) == [2]
+assert Solution.findDisappearedNumbersSet([1, 1]) == [2]
 
 # Unit Test: Input: nums = [1], Output: []
-assert Solution.findDisappearedNumbers([1]) == []
+assert Solution.findDisappearedNumbersSet([1]) == []
 
 print("All unit tests are passed")
