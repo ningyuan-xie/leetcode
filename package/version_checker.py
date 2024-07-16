cf06ca6076e6d80b821b1423e93e7c500e84a69f
Ningyuan Xie <howard.xie25@gmail.com>
2024-07-15 20:28:50 -0400

    Update version checker

--- package/version_checker.py
+++ package/version_checker.py
@@ -6,7 +6,7 @@ def main():
     # major, minor, micro, releaselevel, serial
     version_info = sys.version_info
     # version_info[:3] returns a tuple which is an iterable: (major, minor, micro)
-    # map takes this tuple and returns a map object with is an iterable
+    # map takes this tuple and returns a map object which is an iterable
     # ".".join() concatenates the iterable into a string with "." as the separator
     version = ".".join(map(str, version_info[:3]))
     print("Python version:", version)
