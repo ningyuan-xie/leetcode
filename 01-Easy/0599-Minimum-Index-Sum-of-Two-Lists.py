0bc7435e8f4799abf06a578f95a2cc36b6f8aef3
Ningyuan Xie <howard.xie25@gmail.com>
2024-07-30 19:57:59 -0400

    Update 599

--- 01-Easy/0599-Minimum-Index-Sum-of-Two-Lists.py
+++ 01-Easy/0599-Minimum-Index-Sum-of-Two-Lists.py
@@ -1,8 +1,7 @@
 # Link: https://leetcode.com/problems/minimum-index-sum-of-two-lists/
 # Difficulty: Easy
 # Description: Given two arrays of strings list1 and list2, find the common strings with the
-# least index sum.
-# A common string is a string that appeared in both list1 and list2.
+# least index sum. A common string is a string that appeared in both list1 and list2.
 # A common string with the least index sum is a common string such that if it appeared at
 # list1[i] and list2[j] then i + j should be the minimum value among all the other common strings.
 # Return all the common strings with the least index sum. Return the answer in any order.
@@ -19,24 +18,26 @@ class Solution:
         # Initialize the minimum index sum
         min_index_sum = float('inf')
         # Initialize the list of common strings with the minimum index sum
-        common_strings = []
-        # Iterate over each string in list1
+        common_strings_with_min_index_sum = []
+
+        # Collect all the common strings and their index sum
         for i, string in enumerate(list1):
             # If the string is in list2
             if string in list2:
-                # Calculate the index sum
+                # Index sum = list1's index + list2's index
                 index_sum = i + list2.index(string)
                 # Update the minimum index sum
                 min_index_sum = min(min_index_sum, index_sum)
-                # Add the string to the hash map
+                # Add the string to the hash map. E.g. {"happy": 1, "sad": 1}
                 common_index_sum[string] = index_sum
-        # Iterate over each key-value pair in the hash map
-        for key, value in common_index_sum.items():
+
+        # Only collect the common strings with the minimum index sum
+        for key, value in common_index_sum.items():  # .items() is key-value pair: ("happy", 1)
             # If the index sum is equal to the minimum index sum
             if value == min_index_sum:
                 # Add the string to the list of common strings
-                common_strings.append(key)
-        return common_strings
+                common_strings_with_min_index_sum.append(key)
+        return common_strings_with_min_index_sum
 
 
 # Unit Test: Input: list1 = ["Shogun", "Tapioca Express", "Burger King", "KFC"],
