ead33e4bad3ce357ff4443a58aa4ee6e4663643d
Ningyuan Xie <howard.xie25@gmail.com>
2024-08-05 17:46:47 -0400

    Update 125

--- 01-Easy/0125-Valid-Palindrome.py
+++ 01-Easy/0125-Valid-Palindrome.py
@@ -1,14 +1,14 @@
 """125. Valid Palindrome
 Link: https://leetcode.com/problems/valid-palindrome/
 Difficulty: Easy
-Description: Given a string s, determine if it is a palindrome,
-considering only alphanumeric characters and ignoring cases."""
+Description: Given a string s, determine if it is a palindrome, considering only alphanumeric
+characters and ignoring cases."""
 
 
 class Solution:
     @staticmethod
     def isPalindrome(s: str) -> bool:
-        """Optimal Solution: Iteration. Time Complexity: O(n), Space Complexity: O(1)"""
+        """Optimal Solution: Iteration. Time Complexity: O(n), Space Complexity: O(n)"""
         # Convert the string to lowercase and remove non-alphanumeric characters
         # isalnum() checks if the character is an alphabet or a digit: a-z, A-Z, 0-9
         # ''.join([]) concatenates list of str into str without spaces
@@ -16,18 +16,39 @@ class Solution:
         # Check if the string is a palindrome
         return s == s[::-1]
 
+    @staticmethod
+    def isPalindromeTwoPointers(s: str) -> bool:
+        """Optimal Solution: Two Pointers. Time Complexity: O(n), Space Complexity: O(1)"""
+        # Initialize the two pointers
+        left, right = 0, len(s) - 1
+
+        # Check if the string s is a palindrome
+        while left < right:
+            # Skip all the non-alphanumeric characters
+            if not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            # Check if the left and right characters are equal
+            elif s[left].lower() != s[right].lower():
+                return False
+            else:
+                left += 1
+                right -= 1
+        return True
+
 
 # Unit Test: Input: s = "A man, a plan, a canal: Panama", Output: True
 assert Solution.isPalindrome("A man, a plan, a canal: Panama") == True
 
 # Unit Test: Input: s = "race a car", Output: False
-assert Solution.isPalindrome("race a car") == False
+assert Solution.isPalindromeTwoPointers("race a car") == False
 
 # Unit Test: Input: s = "a", Output: True
 assert Solution.isPalindrome("a") == True
 
 # Unit Test: Input: s = "ab", Output: False
-assert Solution.isPalindrome("ab") == False
+assert Solution.isPalindromeTwoPointers("ab") == False
 
 # Unit Test: Input: s = "a.", Output: True
 assert Solution.isPalindrome("a.") == True
