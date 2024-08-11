39636e42a690c96d15ee7cf9dd3d9076da54870f
Ningyuan Xie <howard.xie25@gmail.com>
2024-08-11 12:17:24 -0400

    Update 303

--- 01-Easy/0303-Range-Sum-Query-Immutable.py
+++ 01-Easy/0303-Range-Sum-Query-Immutable.py
@@ -2,8 +2,8 @@
 Link: https://leetcode.com/problems/range-sum-query-immutable/
 Difficulty: Easy
 Description: Given an integer array nums, handle multiple queries of the following type:
-Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
-Implement the NumArray class:
+Calculate the sum of the elements of nums between indices left and right inclusive
+where left <= right. Implement the NumArray class:
 NumArray(int[] nums) Initializes the object with the integer array nums.
 int sumRange(int left, int right) Returns the sum of the elements of nums
 between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right])."""
@@ -12,6 +12,7 @@ from typing import List
 
 
 class NumArray:
+    """Optimal Solution: Prefix Sum. Time Complexity: O(n), Space Complexity: O(n)"""
 
     def __init__(self, nums: List[int]):
         """Constructor: Time Complexity: O(n), Space Complexity: O(n)"""
