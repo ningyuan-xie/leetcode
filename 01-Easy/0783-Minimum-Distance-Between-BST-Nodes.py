54406d061efb35b7e979c359d3ec8afa1ae8f572
Ningyuan Xie <howard.xie25@gmail.com>
2024-08-16 02:42:04 -0400

    Update 530 & 783

--- 01-Easy/0783-Minimum-Distance-Between-BST-Nodes.py
+++ 01-Easy/0783-Minimum-Distance-Between-BST-Nodes.py
@@ -16,7 +16,7 @@ class Solution:
         # Initialize the minimum absolute difference
         min_absolute_diff = float("inf")  # positive infinity
         # Initialize the previous node
-        prev_node = None
+        prev_node: Optional[TreeNode] = None
 
         def inorder_dfs_traversal(node: TreeNode) -> None:
             """Helper function: Inorder DFS Traversal: left -> root -> right
@@ -31,11 +31,13 @@ class Solution:
             # 1. Recursive Case: Traverse the left subtree
             inorder_dfs_traversal(node.left)
             # 2. Root Case: Process the current node
-            if prev_node is not None:  # cannot use "if prev_node:" will treat node 0 as False
+            if prev_node:
                 # Update the minimum absolute difference
-                min_absolute_diff = min(min_absolute_diff, node.val - prev_node)
-            # Update the previous node
-            prev_node = node.val
+                min_absolute_diff = min(min_absolute_diff, node.val - prev_node.val)
+            # Update prev_node to node not node.val,
+            # otherwise, "if prev_node:" above will be False when prev_node = 0;
+            # alternatively, we can also use "if prev_node is not None:" to avoid this issue
+            prev_node = node
             # 3. Recursive Case: Traverse the right subtree
             inorder_dfs_traversal(node.right)
 
@@ -45,16 +47,16 @@ class Solution:
 
 
 # Unit Test: Input: root = [4, 2, 6, 1, 3], Output: 1
-root_test = TreeNode.build_binary_tree([4, 2, 6, 1, 3])
-assert Solution.minDiffInBST(root_test) == 1
-
-# Unit Test: Input: root = [1, 0, 48, None, None, 12, 49], Output: 1
-root_test = TreeNode.build_binary_tree([1, 0, 48, None, None, 12, 49])
-assert Solution.minDiffInBST(root_test) == 1
-
-# Unit Test: Input: root = [1, None, 5, 3], Output: 2
-root_test = TreeNode.build_binary_tree([1, None, 5, 3])
-assert Solution.minDiffInBST(root_test) == 2
+# root_test = TreeNode.build_binary_tree([4, 2, 6, 1, 3])
+# assert Solution.minDiffInBST(root_test) == 1
+#
+# # Unit Test: Input: root = [1, 0, 48, None, None, 12, 49], Output: 1
+# root_test = TreeNode.build_binary_tree([1, 0, 48, None, None, 12, 49])
+# assert Solution.minDiffInBST(root_test) == 1
+#
+# # Unit Test: Input: root = [1, None, 5, 3], Output: 2
+# root_test = TreeNode.build_binary_tree([1, None, 5, 3])
+# assert Solution.minDiffInBST(root_test) == 2
 
 # Unit Test: Input: root = [0,null,2236,1277,2776,519], Output: 519
 root_test = TreeNode.build_binary_tree([0, None, 2236, 1277, 2776, 519])
