ab5f04c69ee57da8aff739da76397859c7b3ba2c
Ningyuan Xie <howard.xie25@gmail.com>
2024-08-21 22:12:13 -0400

    Update 830

--- 01-Easy/0830-Positions-of-Large-Groups.py
+++ 01-Easy/0830-Positions-of-Large-Groups.py
@@ -2,7 +2,8 @@
 Link: https://leetcode.com/problems/positions-of-large-groups/
 Difficulty: Easy
 Description: In a string s of lowercase letters, these letters form consecutive groups of the same
-character. For example, a string like s = "abbxxxxzyy" has the groups "a", "bb", "xxxx", "z", and "yy".
+character.
+For example, a string like s = "abbxxxxzyy" has the groups "a", "bb", "xxxx", "z", and "yy".
 A group is identified by an interval [start, end], where start and end denote the start and end
 indices (inclusive) of the group. In the above example, "xxxx" has the interval [3,6].
 A group is considered large if it has 3 or more characters.
