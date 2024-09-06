6f998c4ce50c031b2b11be6b6749d9af171a35ad
Ningyuan Xie <howard.xie25@gmail.com>
2024-09-05 22:33:17 -0400

    Update 94

--- 01-Easy/0094-Binary-Tree-Inorder-Traversal.py
+++ 01-Easy/0094-Binary-Tree-Inorder-Traversal.py
@@ -26,6 +26,17 @@ class Solution:
         # Takes effect whenever the current parent and its children have been traversed
         return left_traversal + root_value + right_traversal
 
+    @staticmethod
+    def inorderTraversalShort(root: Optional[TreeNode]) -> List[int]:
+        """Optimal Solution: Inorder DFS Traversal. Time Complexity: O(n), Space Complexity: O(n)"""
+        # Base case: if the tree root is None, return an empty list
+        if not root:
+            return []
+        # Recursive inorder DFS traversal: left -> root -> right
+        return (Solution.inorderTraversalShort(root.left) +
+                [root.val] +
+                Solution.inorderTraversalShort(root.right))
+
 
 # Unit Test: Input: root = [1,null,2,3], Output: [1,3,2]
 root_test = TreeNode.build_binary_tree([1, None, 2, 3])
@@ -33,10 +44,10 @@ assert Solution.inorderTraversal(root_test) == [1, 3, 2]
 
 # Unit Test: Input: root = [], Output: []
 root_test = TreeNode.build_binary_tree([])
-assert Solution.inorderTraversal(root_test) == []
+assert Solution.inorderTraversalShort(root_test) == []
 
 # Unit Test: Input: root = [1], Output: [1]
 root_test = TreeNode.build_binary_tree([1])
-assert Solution.inorderTraversal(root_test) == [1]
+assert Solution.inorderTraversalShort(root_test) == [1]
 
 print("All unit tests are passed")
