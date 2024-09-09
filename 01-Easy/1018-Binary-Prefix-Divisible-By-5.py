f47658cc42d15da3175d41fdf8ea5be37137df45
Ningyuan Xie <howard.xie25@gmail.com>
2024-09-09 13:37:31 -0400

    Update 1018

--- 01-Easy/1018-Binary-Prefix-Divisible-By-5.py
+++ 01-Easy/1018-Binary-Prefix-Divisible-By-5.py
@@ -22,7 +22,7 @@ class Solution:
             # prefix_sum * 2 in binary is equivalent to prefix_sum << 1, % 5 here because:
             # 1. Check if the prefix sum is divisible by 5 without growing too large;
             # 2. We don't care about the actual value, just the remainder:
-            # E.g. 11 % 5 = 1 % 5 -> 22 * 5 = 2 % 5 -> 44 % 5 = 4 % 5
+            # E.g. 11 % 5 = 1 % 5 -> 22 % 5 = 2 % 5 -> 44 % 5 = 4 % 5
             prefix_sum = (prefix_sum * 2 + num) % 5
 
             result.append(prefix_sum == 0)
