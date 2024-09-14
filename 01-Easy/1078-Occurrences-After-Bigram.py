1a40e2030758d17e993dc5af2fd718f6f12cab77
Ningyuan Xie <howard.xie25@gmail.com>
2024-09-14 00:58:12 -0400

    Update 1078

---
+++ 01-Easy/1078-Occurrences-After-Bigram.py
@@ -0,0 +1,34 @@
+"""1078. Occurrences After Bigram
+Link: https://leetcode.com/problems/occurrences-after-bigram/
+Difficulty: Easy
+Description: Given words first and second, consider occurrences in some text of the form "first
+second third", where second comes immediately after first, and third comes immediately after second.
+For each such occurrence,add "third" to the answer, and return the answer."""
+
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def findOcurrences(text: str, first: str, second: str) -> List[str]:
+        """Optimal Solution: One Pass. Time Complexity: O(n), Space Complexity: O(n)"""
+        words = text.split()
+        n, result = len(words), []
+
+        for i in range(2, n):
+            if words[i - 2] == first and words[i - 1] == second:
+                result.append(words[i])
+
+        return result
+
+
+# Unit Test: text = "alice is a good girl she is a good student", first = "a", second = "good",
+# Output: ["girl", "student"]
+assert Solution.findOcurrences("alice is a good girl she is a good student",
+                               "a", "good") == ["girl", "student"]
+
+# Unit Test: text = "we will we will rock you", first = "we", second = "will", Output: ["we","rock"]
+assert Solution.findOcurrences("we will we will rock you",
+                               "we", "will") == ["we", "rock"]
+
+print("All unit tests are passed")
