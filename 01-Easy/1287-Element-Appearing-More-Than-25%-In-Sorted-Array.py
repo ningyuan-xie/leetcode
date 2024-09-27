f3e979856b75dd6049d0e148e9ae806b318ec559
Ningyuan Xie <howard.xie25@gmail.com>
2024-09-26 23:15:58 -0600

    Update 1287

---
+++ 01-Easy/1287-Element-Appearing-More-Than-25%-In-Sorted-Array.py
@@ -0,0 +1,41 @@
+"""1287. Element Appearing More Than 25% In Sorted Array
+Link: https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/
+Difficulty: Easy
+Description: Given an integer array sorted in non-decreasing order, there is an integer in the array
+that occurs more than 25% of the time. Return that integer."""
+
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def findSpecialInteger(arr: List[int]) -> int:
+        """Optimal Solution: Sliding Window. Time Complexity: O(n), Space Complexity: O(1)"""
+        # Initialize the window size as 25% of the array length
+        window_size = len(arr) // 4
+
+        # Initialize the left and right pointers
+        left, right = 0, window_size
+
+        # Slide the window
+        while right < len(arr):
+            # Check if the current window is the answer
+            if arr[left] == arr[right]:
+                return arr[left]
+
+            # Slide the window
+            left += 1
+            right += 1
+
+        # Given that the array is sorted and the requirement is that a number appears more than 25%
+        # of the time, the last element will always be the answer if no earlier match is found
+        return arr[-1]
+
+
+# Unit Test: arr = [1, 2, 2, 6, 6, 6, 6, 7, 10], Output: 6
+assert Solution.findSpecialInteger([1, 2, 2, 6, 6, 6, 6, 7, 10]) == 6
+
+# Unit Test: arr = [1, 1], Output: 1
+assert Solution.findSpecialInteger([1, 1]) == 1
+
+print("All unit tests are passed")
