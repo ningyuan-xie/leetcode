d5a2a0894343853c119165dc0a1ab77b60c23664
Ningyuan Xie <howard.xie25@gmail.com>
2024-09-29 12:03:54 -0400

    Update 1313

---
+++ 01-Easy/1313-Decompress-Run-Length-Encoded-List.py
@@ -0,0 +1,35 @@
+"""1313. Decompress Run-Length Encoded List
+Link: https://leetcode.com/problems/decompress-run-length-encoded-list/
+Difficulty: Easy
+Description: We are given a list nums of integers representing a list compressed with run-length
+encoding.
+Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).
+For each such pair, there are freq elements with value val concatenated in a sublist.
+Concatenate all the sublists from left to right to generate the decompressed list.
+Return the decompressed list."""
+
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def decompressRLElist(nums: List[int]) -> List[int]:
+        """Optimal Solution: list.extend() method.
+           Time Complexity: O(n), Space Complexity: O(n)"""
+        # Initialize the result
+        result = []
+
+        # Traverse the list in pairs
+        for i in range(0, len(nums), 2):
+            result.extend([nums[i + 1]] * nums[i])  # E.g. .extend([4] * 3) = [4, 4, 4]
+
+        return result
+
+
+# Unit Test: nums = [1, 2, 3, 4], Output: [2, 4, 4, 4]
+assert Solution.decompressRLElist([1, 2, 3, 4]) == [2, 4, 4, 4]
+
+# Unit Test: nums = [1, 1, 2, 3], Output: [1, 3, 3]
+assert Solution.decompressRLElist([1, 1, 2, 3]) == [1, 3, 3]
+
+print("All unit tests are passed")
