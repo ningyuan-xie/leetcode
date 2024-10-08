6d18e693521283d08f4e62222257303ecda8b5dc
Ningyuan Xie <howard.xie25@gmail.com>
2024-10-08 00:15:59 -0400

    Update 1394

---
+++ 01-Easy/1394-Find-Lucky-Integer-in-an-Array.py
@@ -0,0 +1,37 @@
+"""1394. Find Lucky Integer in an Array
+Link: https://leetcode.com/problems/find-lucky-integer-in-an-array/
+Difficulty: Easy
+Description: Given an array of integers arr, a lucky integer is an integer that has a frequency in
+the array equal to its value.
+Return the largest lucky integer in the array. If there is no lucky integer return -1."""
+
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def findLucky(arr: List[int]) -> int:
+        """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(n)"""
+        frequency = {}
+        for num in arr:
+            frequency[num] = frequency.get(num, 0) + 1
+        lucky_numbers = [key for (key, value) in frequency.items() if key == value]
+        return max(lucky_numbers) if lucky_numbers else -1
+
+
+# Unit Test: arr = [2, 2, 3, 4], Output: 2
+assert Solution.findLucky([2, 2, 3, 4]) == 2
+
+# Unit Test: arr = [1, 2, 2, 3, 3, 3], Output: 3
+assert Solution.findLucky([1, 2, 2, 3, 3, 3]) == 3
+
+# Unit Test: arr = [2, 2, 2, 3, 3], Output: -1
+assert Solution.findLucky([2, 2, 2, 3, 3]) == -1
+
+# Unit Test: arr = [5], Output: -1
+assert Solution.findLucky([5]) == -1
+
+# Unit Test: arr = [7, 7, 7, 7, 7, 7, 7], Output: 7
+assert Solution.findLucky([7, 7, 7, 7, 7, 7, 7]) == 7
+
+print("All unit tests are passed")
