d8c2f61b21062320ffb79d67f55d02d405f55a3d
Ningyuan Xie <howard.xie25@gmail.com>
2024-11-11 00:53:00 -0500

    Update 1748

---
+++ 01-Easy/1748-Sum-of-Unique-Elements.py
@@ -0,0 +1,35 @@
+"""1748. Sum of Unique Elements
+Link: https://leetcode.com/problems/sum-of-unique-elements/
+Difficulty: Easy
+Description: You are given an integer array nums. The unique elements of an array are the elements
+that appear exactly once in the array.
+Return the sum of all the unique elements of nums."""
+
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def sum_of_unique(nums: List[int]) -> int:
+        """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(n)"""
+        # Initialize the frequency of each element in the array
+        freq = {}
+
+        # Count the frequency of each element in the array
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
+        # Sum the unique elements in the array
+        return sum(num for (num, count) in freq.items() if count == 1)
+
+
+# Unit Test: nums = [1, 2, 3, 2], Output: 4
+assert Solution.sum_of_unique([1, 2, 3, 2]) == 4
+
+# Unit Test: nums = [1, 1, 1, 1, 1], Output: 0
+assert Solution.sum_of_unique([1, 1, 1, 1, 1]) == 0
+
+# Unit Test: nums = [1, 2, 3, 4, 5], Output: 15
+assert Solution.sum_of_unique([1, 2, 3, 4, 5]) == 15
+
+print("All unit tests are passed")
