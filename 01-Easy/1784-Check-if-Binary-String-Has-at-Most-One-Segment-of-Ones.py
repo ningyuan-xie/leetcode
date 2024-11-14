fdcdfea97a37d24207c663e596a98a665d91429e
Ningyuan Xie <howard.xie25@gmail.com>
2024-11-14 02:02:10 -0500

    Update 1784

---
+++ 01-Easy/1784-Check-if-Binary-String-Has-at-Most-One-Segment-of-Ones.py
@@ -0,0 +1,27 @@
+"""1784. Check if Binary String Has at Most One Segment of Ones
+Link: https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/
+Difficulty: Easy
+Description: Given a binary string s without leading zeros, return true if s contains at most one
+contiguous segment of ones. Otherwise, return false."""
+
+
+class Solution:
+    @staticmethod
+    def check_ones_segment(s: str) -> bool:
+        """Optimal Solution: Check Pattern. Time Complexity: O(n), Space Complexity: O(1).
+           To determine if there's only one contiguous segment of '1's, we can check for occurrences
+           of the pattern "01" in the string. If "01" appears, it means there's a transition from
+           '1's to '0's, followed by another '1' later, indicating more than one segment of '1's"""
+        return "01" not in s
+
+
+# Unit Test: s = "1001", Output: False
+assert Solution.check_ones_segment("1001") is False
+
+# Unit Test: s = "110", Output: True
+assert Solution.check_ones_segment("110") is True
+
+# Unit Test: s = "1", Output: True
+assert Solution.check_ones_segment("1") is True
+
+print("All unit tests are passed")
