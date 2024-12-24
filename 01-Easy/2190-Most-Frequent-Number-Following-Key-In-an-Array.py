626580422597dd2ec74d2c448b305614c1b5f33e
Ningyuan Xie <howard.xie25@gmail.com>
2024-12-24 00:22:33 -0500

    Update 2190

---
+++ 01-Easy/2190-Most-Frequent-Number-Following-Key-In-an-Array.py
@@ -0,0 +1,40 @@
+"""2190. Most Frequent Number Following Key In an Array
+Link: https://leetcode.com/problems/most-frequent-number-following-key-in-an-array/
+Difficulty: Easy
+Description: You are given a 0-indexed integer array nums. You are also given an integer key, which
+is present in nums.
+For every unique integer target in nums, count the number of times target immediately follows an
+occurrence of key in nums. In other words, count the number of indices i such that:
+- 0 <= i <= nums.length - 2,
+- nums[i] == key and,
+- nums[i + 1] == target.
+Return the target with the maximum count. The test cases will be generated such that the target with
+maximum count is unique."""
+
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def mostFrequent(nums: List[int], key: int) -> int:
+        """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(n)"""
+        # Count occurrences of targets following the key
+        target_count = {}
+        for i in range(len(nums) - 1):
+            if nums[i] == key:
+                target_count[nums[i + 1]] = target_count.get(nums[i + 1], 0) + 1
+        # nums = [1,100,200,1,100], key = 1 -> target_count = {100: 2}
+        # nums = [2,2,2,2,3], key = 2 -> target_count = {2: 3, 3: 1}
+
+        # Find and return the most frequent target: finds the target with the
+        # maximum count, breaking ties by choosing the smallest number
+        return max(target_count, key=lambda x: (target_count[x], -x))
+
+
+# Unit Test: nums = [1,100,200,1,100], key = 1, Output: 100
+assert Solution.mostFrequent([1, 100, 200, 1, 100], 1) == 100
+
+# Unit Test: nums = [2,2,2,2,3], key = 2, Output: 2
+assert Solution.mostFrequent([2, 2, 2, 2, 3], 2) == 2
+
+print("All unit tests are passed")
---
+++ 01-Easy/2194-Cells-in-a-Range-on-an-Excel-Sheet.py
@@ -0,0 +1,40 @@
+"""2194. Cells in a Range on an Excel Sheet
+Link: https://leetcode.com/problems/cells-in-a-range-on-an-excel-sheet/
+Difficulty: Easy
+Description: A cell (r, c) of an excel sheet is represented as a string "<col><row>" where:
+- <col> denotes the column number c of the cell. It is represented by alphabetical letters.
+-- For example, the 1st column is denoted by 'A', the 2nd by 'B', the 3rd by 'C', and so on.
+- <row> is the row number r of the cell. The rth row is represented by the integer r.
+You are given a string s in the format "<col1><row1>:<col2><row2>", where <col1> represents the
+column c1, <row1> represents the row r1, <col2> represents the column c2, and <row2> represents
+the row r2, such that r1 <= r2 and c1 <= c2.
+Return the list of cells (x, y) such that r1 <= x <= r2 and c1 <= y <= c2. The cells should be
+represented as strings in the format mentioned above and be sorted in non-decreasing order first
+by columns and then by rows."""
+
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def cellsInRange(s: str) -> List[str]:
+        """Optimal Solution: chr() and ord() functions.
+           Time Complexity: O(n), Space Complexity: O(1)"""
+        # Parse the range string
+        start_col, start_row, end_col, end_row = s[0], s[1], s[3], s[4]
+
+        # Generate all cells in the range
+        return [
+            f"{chr(c)}{r}"
+            for c in range(ord(start_col), ord(end_col) + 1)  # chr(ord('A')) = 'A'
+            for r in range(int(start_row), int(end_row) + 1)
+        ]
+
+
+# Unit Test: s = "K1:L2", Output: ["K1", "K2", "L1", "L2"]
+assert Solution.cellsInRange("K1:L2") == ["K1", "K2", "L1", "L2"]
+
+# Unit Test: s = "A1:F1", Output: ["A1", "B1", "C1", "D1", "E1", "F1"]
+assert Solution.cellsInRange("A1:F1") == ["A1", "B1", "C1", "D1", "E1", "F1"]
+
+print("All unit tests are passed")
