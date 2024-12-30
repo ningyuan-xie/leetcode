049b1462188087cfb8e5da25a9f39366fde31a7e
Ningyuan Xie <howard.xie25@gmail.com>
2024-12-30 00:20:49 -0500

    Update 2248

---
+++ 01-Easy/2248-Intersection-of-Multiple-Arrays.py
@@ -0,0 +1,33 @@
+"""2248. Intersection of Multiple Arrays
+Link: https://www.leetcode.com/problems/intersection-of-multiple-arrays/
+Difficulty: Easy
+Description: Given a 2D integer array nums where nums[i] is a non-empty array of distinct
+positive integers, return the list of integers that are present in each array of nums sorted
+in ascending order."""
+
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def intersection(nums: List[List[int]]) -> List[int]:
+        """Optimal Solution: Set Intersection. Time Complexity: O(n), Space Complexity: O(n)"""
+        # Initialize the result with the first set of numbers
+        result = set(nums[0])  # {1, 2, 3, 4, 5}
+
+        # For each set of numbers, compute the intersection with the result
+        for i in range(1, len(nums)):
+            current_set = set(nums[i])  # {1, 2, 3, 4}, {3, 4, 5, 6}
+            # Retain only elements that are present in both sets
+            result = {x for x in result if x in current_set}  # not using set.intersection() here
+
+        return list(sorted(result))
+
+
+# Unit Test: nums = [[3,1,2,4,5],[1,2,3,4],[3,4,5,6]], Output: [3,4]
+assert Solution.intersection([[3, 1, 2, 4, 5], [1, 2, 3, 4], [3, 4, 5, 6]]) == [3, 4]
+
+# Unit Test: nums = [[1,2,3],[4,5,6]], Output: []
+assert Solution.intersection([[1, 2, 3], [4, 5, 6]]) == []
+
+print("All unit tests are passed")
