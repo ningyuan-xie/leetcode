5e2f3e3c6a33bdcd4b5b10137d8e5f3ae77ad0f2
Ningyuan Xie <howard.xie25@gmail.com>
2025-01-03 01:26:39 -0500

    Update 2287

---
+++ 01-Easy/2287-Rearrange-Characters-to-Make-Target-String.py
@@ -0,0 +1,44 @@
+"""2287. Rearrange Characters to Make Target String
+Link: https://www.leetcode.com/problems/rearrange-characters-to-make-target-string/
+Difficulty: Easy
+Description: You are given two 0-indexed strings s and target. You can take some letters from s and
+rearrange them to form new strings.
+Return the maximum number of copies of target that can be formed by taking letters from s and
+rearranging them."""
+
+
+class Solution:
+    @staticmethod
+    def rearrangeCharacters(s: str, target: str) -> int:
+        """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(1)"""
+        # Create two dictionaries to store the frequency of each character in s and target
+        frequency, target_frequency = {}, {}
+        for char in s:
+            frequency[char] = frequency.get(char, 0) + 1
+        for char in target:
+            target_frequency[char] = target_frequency.get(char, 0) + 1
+
+        # Initialize the maximum number of copies of target that can be formed
+        max_copies = float("inf")
+
+        # Iterate through the target frequency dictionary
+        for (char, count) in target_frequency.items():
+            if char not in frequency or frequency[char] < count:
+                return 0
+
+            # Calculate the maximum number of copies of target that can be formed
+            max_copies = min(max_copies, frequency[char] // count)
+
+        return max_copies
+
+
+# Unit Test: s = "ilovecodingonleetcode", target = "code", Output: 2
+assert Solution.rearrangeCharacters("ilovecodingonleetcode", "code") == 2
+
+# Unit Test: s = "abcba", target = "abc", Output: 1
+assert Solution.rearrangeCharacters("abcba", "abc") == 1
+
+# Unit Test: s = "abbaccaddaeea", target = "aaaaa", Output: 1
+assert Solution.rearrangeCharacters("abbaccaddaeea", "aaaaa") == 1
+
+print("All unit tests are passed")
