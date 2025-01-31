bc3a1e7d8bde9b170e4d1a8bf225ef3793c73824
Ningyuan Xie <howard.xie25@gmail.com>
2025-01-31 00:19:33 -0500

    Update 2562

---
+++ 01-Easy/2562-Find-the-Array-Concatenation-Value.py
@@ -0,0 +1,43 @@
+"""2562. Find the Array Concatenation Value
+Link: https://leetcode.com/problems/find-the-array-concatenation-value/
+Difficulty: Easy
+Description: You are given a 0-indexed integer array nums.
+The concatenation of two numbers is the number formed by concatenating their numerals.
+- For example, the concatenation of 15, 49 is 1549.
+The concatenation value of nums is initially equal to 0. Perform this operation until nums becomes empty:
+- If there exists more than one number in nums, pick the first element and last element in nums
+respectively and add the value of their concatenation to the concatenation value of nums, then
+delete the first and last element from nums.
+- If one element exists, add its value to the concatenation value of nums, then delete it.
+Return the concatenation value of the nums."""
+
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def findTheArrayConcVal(nums: List[int]) -> int:
+        """Optimal Solution: Two Pointers. Time Complexity: O(n), Space Complexity: O(1)"""
+        left, right = 0, len(nums) - 1
+        total = 0
+
+        while left < right:
+            # Concatenate nums[left] and nums[right] and add to total
+            total += int(str(nums[left]) + str(nums[right]))
+            left += 1
+            right -= 1
+
+        # If nums has odd length, add the middle element to total
+        if left == right:
+            total += nums[left]
+
+        return total
+
+
+# Unit Test: nums = [7,52,2,4], Output: 596
+assert Solution.findTheArrayConcVal([7, 52, 2, 4]) == 596
+
+# Unit Test: nums = [5,14,13,8,12], Output: 673
+assert Solution.findTheArrayConcVal([5, 14, 13, 8, 12]) == 673
+
+print("All unit tests are passed")
