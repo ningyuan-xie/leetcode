73ab24e2785ce1a4e837844a7de84a01e49b8e10
Ningyuan Xie <howard.xie25@gmail.com>
2025-03-15 00:15:41 -0400

    Update 3046

---
+++ 01-Easy/3046-Split-the-Array.py
@@ -0,0 +1,36 @@
+"""3046. Split the Array
+Link: https://leetcode.com/problems/split-the-array/
+Difficulty: Easy
+Description: You are given an integer array nums of even length. You have to split the
+array into two parts nums1 and nums2 such that:
+- nums1.length == nums2.length == nums.length / 2.
+- nums1 should contain distinct elements.
+- nums2 should also contain distinct elements.
+Return true if it is possible to split the array, and false otherwise."""
+
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def isPossibleToSplit(nums: List[int]) -> bool:
+        """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(n)"""
+        # Initialize the hash table
+        hash_table = {}
+
+        # Iterate through the array and count the number of occurrences
+        for num in nums:
+            hash_table[num] = hash_table.get(num, 0) + 1
+            # If the number of occurrences > 2, then impossible to split distinct elements
+            if hash_table[num] > 2:
+                return False
+        return True
+
+
+# Unit Test: nums = [1,1,2,2,3,4], Output = True
+assert Solution.isPossibleToSplit([1, 1, 2, 2, 3, 4]) is True
+
+# Unit Test: nums = [1,1,1,1], Output = False
+assert Solution.isPossibleToSplit([1, 1, 1, 1]) is False
+
+print("All unit tests are passed")
