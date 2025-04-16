67f56272eddf80f229eab05e5eb556cd176f4653
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-15 20:01:44 -0400

    Update 83

--- 01-Easy/00083-Remove-Duplicates-from-Sorted-List.py
+++ 01-Easy/00083-Remove-Duplicates-from-Sorted-List.py
@@ -1,9 +1,7 @@
 """83. Remove Duplicates from Sorted List
 Link: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
 Difficulty: Easy
-Description: Given the head of a sorted linked list,
-delete all duplicates such that each element appears only once.
-Return the linked list sorted as well."""
+Description: Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well."""
 
 from typing import Optional
 from package.data_structures import ListNode
@@ -12,26 +10,33 @@ from package.data_structures import ListNode
 class Solution:
     @staticmethod
     def deleteDuplicates(head: Optional[ListNode]) -> Optional[ListNode]:
-        """Optimal Solution: Iteration. Time Complexity: O(n), Space Complexity: O(1)."""
-        # Base case: if the linked list is empty, return None
-        if not head:
-            return None
-        # Initialize the current node as the head of the linked list
+        """Optimal Solution: Two Pointers. Time Complexity: O(n), Space Complexity: O(1)."""
         current = head
-        # Iterate through the linked list
-        while current.next:
-            # If the current node and the next node have the same value, skip the next node
+
+        # Traverse the linked list when the current node and the next node are not None
+        while current and current.next:
+            # If the current node's value is equal to the next node's value, skip the next node
             if current.val == current.next.val:
                 current.next = current.next.next
             else:
                 # Move to the next node
                 current = current.next
+
         return head
 
 
-# Input: head = [1,1,2], Output: [1,2]
-head_test = ListNode.build_linked_list([1, 1, 2])
-head_expected = ListNode.build_linked_list([1, 2])
-assert Solution.deleteDuplicates(head_test) == head_expected
+def unit_tests():
+    # Input: head = [1,1,2], Output: [1,2]
+    head = ListNode.build_linked_list([1, 1, 2])
+    output = ListNode.build_linked_list([1, 2])
+    assert Solution.deleteDuplicates(head) == output
 
-print("All unit tests are passed.")
+    # Input: head = [1,1,2,3,3], Output: [1,2,3]
+    head = ListNode.build_linked_list([1, 1, 2, 3, 3])
+    output = ListNode.build_linked_list([1, 2, 3])
+    assert Solution.deleteDuplicates(head) == output
+
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
