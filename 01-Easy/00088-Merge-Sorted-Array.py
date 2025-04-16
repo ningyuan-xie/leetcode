63c4f110799cf90beef8542e1cc35435b57f74f9
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-16 17:07:00 -0400

    Update 88

--- 01-Easy/00088-Merge-Sorted-Array.py
+++ 01-Easy/00088-Merge-Sorted-Array.py
@@ -1,56 +1,58 @@
 """88. Merge Sorted Array
 Link: https://leetcode.com/problems/merge-sorted-array/
 Difficulty: Easy
-Description: You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
-and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
-Merge nums1 and nums2 into a single array sorted in non-decreasing order."""
+Description: You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n."""
 
 from typing import List
 
 
 class Solution:
     @staticmethod
-    def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:  # merge nums2 into nums1
-        """Optimal Solution: Two Pointers. Time Complexity: O(m + n), Space Complexity: O(1).
-           Merge nums2 into nums1 in-place"""
-        # Initialize the pointers for nums1 and nums2
-        p1 = m - 1  # last index of current nums1
-        p2 = n - 1  # last index of nums2
-        last = m + n - 1  # last index of the merged array nums1
+    def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """Optimal Solution: Three Pointers. Time Complexity: O(n), Space Complexity: O(1)."""
+        # Initialize pointers for nums1, nums2 and the merged array
+        p1, p2, last = m - 1, n - 1, m + n - 1
 
-        # Iterate the merged array in reverse order
-        for p in range(last, -1, -1):  # 3rd parameter -1: decrement by 1 each loop
-            # If there are no more elements in nums2, break
+        # Merge in reverse order
+        for p in range(last, -1, -1):
+            # If nums2 is exhausted, break
             if p2 < 0:
                 break
-            # If nums1's element is greater, put in nums1's element
-            if p1 >= 0 and nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
-            # If nums2's element is greater, put in nums2's element
-            else:
+
+            # If nums1 is exhausted or nums2 is greater, place nums2's element
+            if p1 < 0 or nums1[p1] <= nums2[p2]:
                 nums1[p] = nums2[p2]
                 p2 -= 1
+            else:
+                # Place nums1's element
+                nums1[p] = nums1[p1]
+                p1 -= 1
 
 
-# Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3, Output: [1,2,2,3,5,6]
-nums1_test = [1, 2, 3, 0, 0, 0]
-Solution.merge(nums1_test, 3, [2, 5, 6], 3)
-assert nums1_test == [1, 2, 2, 3, 5, 6]
+def unit_tests():
+    # Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3, Output: [1,2,2,3,5,6]
+    nums1 = [1, 2, 3, 0, 0, 0]
+    Solution.merge(nums1, 3, [2, 5, 6], 3)
+    assert nums1 == [1, 2, 2, 3, 5, 6]
 
-# Input: nums1 = [2,2,3,0,0,0], m = 3, nums2 = [1,5,6], n = 3, Output: [1,2,2,3,5,6]
-nums1_test = [2, 2, 3, 0, 0, 0]
-Solution.merge(nums1_test, 3, [1, 5, 6], 3)
-assert nums1_test == [1, 2, 2, 3, 5, 6]
+    # Input: nums1 = [2,2,3,0,0,0], m = 3, nums2 = [1,5,6], n = 3, Output: [1,2,2,3,5,6]
+    nums1 = [2, 2, 3, 0, 0, 0]
+    Solution.merge(nums1, 3, [1, 5, 6], 3)
+    assert nums1 == [1, 2, 2, 3, 5, 6]
 
-# Input: nums1 = [1], m = 1, nums2 = [], n = 0, Output: [1]
-nums1_test = [1]
-Solution.merge(nums1_test, 1, [], 0)
-assert nums1_test == [1]
+    # Input: nums1 = [1], m = 1, nums2 = [], n = 0, Output: [1]
+    nums1 = [1]
+    Solution.merge(nums1, 1, [], 0)
+    assert nums1 == [1]
 
-# Input: nums1 = [0], m = 0, nums2 = [1], n = 1, Output: [1]
-nums1_test = [0]
-Solution.merge(nums1_test, 0, [1], 1)
-assert nums1_test == [1]
+    # Input: nums1 = [0], m = 0, nums2 = [1], n = 1, Output: [1]
+    nums1 = [0]
+    Solution.merge(nums1, 0, [1], 1)
+    assert nums1 == [1]
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
