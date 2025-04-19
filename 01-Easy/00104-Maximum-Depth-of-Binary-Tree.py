b81a7db887ae1611c1a769cd4d02ceeb28ed14da
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-19 12:54:49 -0400

    Update 104

--- 01-Easy/00104-Maximum-Depth-of-Binary-Tree.py
+++ 01-Easy/00104-Maximum-Depth-of-Binary-Tree.py
@@ -2,8 +2,7 @@
 Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
 Difficulty: Easy
 Description: Given the root of a binary tree, return its maximum depth.
-A binary tree's maximum depth is the number of nodes along the longest path
-from the root node down to the farthest leaf node = height of the tree + 1"""
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node."""
 
 from typing import Optional
 from package.data_structures import TreeNode
@@ -12,36 +11,31 @@ from package.data_structures import TreeNode
 class Solution:
     @staticmethod
     def maxDepth(root: Optional[TreeNode]) -> int:
-        """Optimal Solution: Postorder DFS Traversal. Time Complexity: O(n), Space Complexity: O(1).
-           Postorder because we need to traverse the left and right subtrees first before the root"""
-        # Base case: if the tree root is None, return 0
+        """Optimal Solution: Postorder DFS. Time Complexity: O(n), Space Complexity: O(n)."""
+        # Base Case: If the tree is empty, return 0
         if not root:
             return 0
-        # 1. Recursive Case: Traverse the left subtree
+        # Recursive Case: Calculate the depth of the left and right subtrees
         left_depth = Solution.maxDepth(root.left)
-        # 2. Recursive Case: Traverse the right subtree
         right_depth = Solution.maxDepth(root.right)
-        # 3. Root Case: Return the maximum depth of the left and right subtrees
-        # + 1 to account for the current node
-        root_depth = max(left_depth, right_depth) + 1
-
-        return root_depth
+        # Return the maximum of the two depths plus one for the current node
+        return max(left_depth, right_depth) + 1
 
 
-# Input: root = [3,9,20,null,null,15,7], Output: 3
-# The input [3,9,20,null,null,15,7] = serialized format of a binary tree using level order traversal
-# where null = a path terminator where no node exists below
-root_test = TreeNode.build_binary_tree([3, 9, 20, None, None, 15, 7])
-assert Solution.maxDepth(root_test) == 3
+def unit_tests():
+    # Input: root = [3,9,20,null,null,15,7], Output: 3
+    root = TreeNode.build_binary_tree([3, 9, 20, None, None, 15, 7])
+    assert Solution.maxDepth(root) == 3
 
-# Input: root = [1,null,2], Output: 2
-# The input [1,null,2] = serialized format of a binary tree using level order traversal
-# where null = a path terminator where no node exists below
-root_test = TreeNode.build_binary_tree([1, None, 2])
-assert Solution.maxDepth(root_test) == 2
+    # Input: root = [1,null,2], Output: 2
+    root = TreeNode.build_binary_tree([1, None, 2])
+    assert Solution.maxDepth(root) == 2
 
-# Input: root = [], Output: 0
-root_test = TreeNode.build_binary_tree([])
-assert Solution.maxDepth(root_test) == 0
+    # Input: root = [], Output: 0
+    root = TreeNode.build_binary_tree([])
+    assert Solution.maxDepth(root) == 0
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
