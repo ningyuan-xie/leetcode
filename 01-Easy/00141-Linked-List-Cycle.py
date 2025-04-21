ed4bdf9184313e357c3c3fe3b6ed204da5c98c50
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-21 00:03:08 -0400

    Update 141

--- 01-Easy/00141-Linked-List-Cycle.py
+++ 01-Easy/00141-Linked-List-Cycle.py
@@ -2,8 +2,8 @@
 Link: https://leetcode.com/problems/linked-list-cycle/
 Difficulty: Easy
 Description: Given head, the head of a linked list, determine if the linked list has a cycle in it.
-There is a cycle in a linked list if there is some node in the list that can be reached again by
-continuously following the next pointer."""
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+Return true if there is a cycle in the linked list. Otherwise, return false."""
 
 from package.data_structures import ListNode
 
@@ -11,45 +11,45 @@ from package.data_structures import ListNode
 class Solution:
     @staticmethod
     def hasCycle(head: ListNode) -> bool:
-        """Optimal Solution: Floyd's Tortoise and Hare (Cycle Detection) Algorithm.
-           Time Complexity: O(n), Space Complexity: O(1)."""
-        # Initialize two pointers: slow and fast
-        slow, fast = head, head
+        """Optimal Solution: Floyd's Cycle Detection Algorithm (Tortoise and Hare). Time Complexity: O(n), Space Complexity: O(1)."""
+        # Initialize two pointers
+        slow = head
+        fast = head
 
-        # Temporarily enable single value check for cycle detection
-        # In LeetCode environment, Python will dynamically create this class attribute on the fly
+        # Enable single value check for cycle detection
         ListNode.identity_check = True
 
         # Traverse the linked list
-        while fast and fast.next:  # make sure fast.next also exists as we shift fast by two nodes
-            # Move slow pointer by one node
+        while fast and fast.next:
+            # Move slow pointer one step
             slow = slow.next
-            # Move fast pointer by two nodes
+            # Move fast pointer two steps
             fast = fast.next.next
-            # If there is a cycle, the slow and fast pointers will meet
+
+            # If they meet, there is a cycle
             if slow == fast:
                 return True
 
-        # Disable single value check after cycle detection
-        ListNode.identity_check = False
+        # If we reach the end of the list, there is no cycle
         return False
 
 
-# Input: head = [3,2,0,-4], pos = 1, Output: True
-# There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
-head1 = ListNode.build_linked_list([3, 2, 0, -4])
-head1.next.next.next.next = head1.next  # Connect the tail to the 1st node
-assert Solution.hasCycle(head1) is True
+def unit_tests():
+    # Input: head = [3,2,0,-4], pos = 1, Output: True
+    head = ListNode.build_linked_list([3, 2, 0, -4])
+    head.next.next.next.next = head.next  # Connect the tail to the 1st node
+    assert Solution.hasCycle(head) is True
 
-# Input: head = [1,2], pos = 0, Output: True
-# There is a cycle in the linked list, where the tail connects to the 0th node (0-indexed).
-head2 = ListNode.build_linked_list([1, 2])
-head2.next.next = head2  # Connect the tail to the 0th node
-assert Solution.hasCycle(head2) is True
+    # Input: head = [1,2], pos = 0, Output: True
+    head = ListNode.build_linked_list([1, 2])
+    head.next.next = head  # Connect the tail to the 0th node
+    assert Solution.hasCycle(head) is True
 
-# Input: head = [1], pos = -1, Output: False
-# There is no cycle in the linked list.
-head3 = ListNode.build_linked_list([1])
-assert Solution.hasCycle(head3) is False
+    # Input: head = [1], pos = -1, Output: False
+    head = ListNode.build_linked_list([1])
+    assert Solution.hasCycle(head) is False
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
