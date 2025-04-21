ecc9b643d432a0c8d03580f7d96e9c297fd8bc62
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-20 20:47:29 -0400

    Update 125

--- 01-Easy/00125-Valid-Palindrome.py
+++ 01-Easy/00125-Valid-Palindrome.py
@@ -1,30 +1,20 @@
 """125. Valid Palindrome
 Link: https://leetcode.com/problems/valid-palindrome/
 Difficulty: Easy
-Description: Given a string s, determine if it is a palindrome, considering only alphanumeric
-characters and ignoring cases."""
+Description: A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise."""
 
 
 class Solution:
     @staticmethod
     def isPalindrome(s: str) -> bool:
-        """Optimal Solution: Iteration. Time Complexity: O(n), Space Complexity: O(n)."""
-        # Convert the string to lowercase and remove non-alphanumeric characters
-        # isalnum() checks if the character is an alphabet or a digit: a-z, A-Z, 0-9
-        # ''.join([]) concatenates list of str into str without spaces
-        s = ''.join([char.lower() for char in s if char.isalnum()])
-        # Check if the string is a palindrome
-        return s == s[::-1]
-
-    @staticmethod
-    def isPalindromeTwoPointers(s: str) -> bool:
         """Optimal Solution: Two Pointers. Time Complexity: O(n), Space Complexity: O(1)."""
-        # Initialize the two pointers
+        # Initialize two pointers
         left, right = 0, len(s) - 1
 
         # Check if the string s is a palindrome
         while left < right:
-            # Skip all the non-alphanumeric characters and go to the next while loop
+            # Skip all the non-alphanumeric characters
             if not s[left].isalnum():
                 left += 1
             elif not s[right].isalnum():
@@ -35,22 +25,27 @@ class Solution:
             else:
                 left += 1
                 right -= 1
+                
         return True
 
+        
+def unit_tests():
+    # Input: s = "A man, a plan, a canal: Panama", Output: True
+    assert Solution.isPalindrome("A man, a plan, a canal: Panama") is True
 
-# Input: s = "A man, a plan, a canal: Panama", Output: True
-assert Solution.isPalindrome("A man, a plan, a canal: Panama") is True
+    # Input: s = "race a car", Output: False
+    assert Solution.isPalindrome("race a car") is False
 
-# Input: s = "race a car", Output: False
-assert Solution.isPalindromeTwoPointers("race a car") is False
+    # Input: s = "a", Output: True
+    assert Solution.isPalindrome("a") is True
 
-# Input: s = "a", Output: True
-assert Solution.isPalindrome("a") is True
+    # Input: s = "ab", Output: False
+    assert Solution.isPalindrome("ab") is False
 
-# Input: s = "ab", Output: False
-assert Solution.isPalindromeTwoPointers("ab") is False
+    # Input: s = "a.", Output: True
+    assert Solution.isPalindrome("a.") is True
 
-# Input: s = "a.", Output: True
-assert Solution.isPalindrome("a.") is True
 
-print("All unit tests are passed.")
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
