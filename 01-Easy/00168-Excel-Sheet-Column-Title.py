f7081b3409a93bb0d07982b70439876512746cb9
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-22 01:23:58 -0400

    Update 168

--- 01-Easy/00168-Excel-Sheet-Column-Title.py
+++ 01-Easy/00168-Excel-Sheet-Column-Title.py
@@ -1,39 +1,43 @@
 """168. Excel Sheet Column Title
 Link: https://leetcode.com/problems/excel-sheet-column-title/
 Difficulty: Easy
-Description: Given an integer columnNumber,
-return its corresponding column title as it appears in an Excel sheet."""
+Description: Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet."""
 
 
 class Solution:
     @staticmethod
     def convertToTitle(columnNumber: int) -> str:
-        """Optimal Solution: Math. Time Complexity: O(log(columnNumber)), Space Complexity: O(1).
-           Similar to 0067-Add-Binary.py"""
+        """Optimal Solution: Math. Time Complexity: O(log(n)), Space Complexity: O(1)."""
+        # Initialize an empty string to store the result
         result = ""
-        # Title will be filled from RIGHT to LEFT
+
         while columnNumber > 0:
-            # Subtract 1 from columnNumber to make it 0-indexed for each digit
+            # Decrement columnNumber by 1 to handle 1-based indexing
             columnNumber -= 1
-            remainder = columnNumber % 26
-            # 65 is the ASCII value of 'A': chr(65) = 'A', chr(66) = 'B', ..., chr(90) = 'Z'
-            # Add char to the front of the string: 'B' -> 'A' + 'B' = 'AB'
-            result = chr(remainder + ord('A')) + result
-            # Divide columnNumber by 26 using floor division to move to the next digit on the left
+
+            # Calculate the current character and prepend it to the result
+            result = chr(columnNumber % 26 + ord('A')) + result
+
+            # Update columnNumber for the next iteration
             columnNumber //= 26
+
         return result
 
 
-# Input: columnNumber = 1, Output: "A"
-assert Solution.convertToTitle(1) == "A"
+def unit_tests():
+    # Input: columnNumber = 1, Output: "A"
+    assert Solution.convertToTitle(1) == "A"
 
-# Input: columnNumber = 28, Output: "AB"
-assert Solution.convertToTitle(28) == "AB"
+    # Input: columnNumber = 28, Output: "AB"
+    assert Solution.convertToTitle(28) == "AB"
 
-# Input: columnNumber = 701, Output: "ZY"
-assert Solution.convertToTitle(701) == "ZY"
+    # Input: columnNumber = 701, Output: "ZY"
+    assert Solution.convertToTitle(701) == "ZY"
 
-# Input: columnNumber = 2147483647, Output: "FXSHRXW"
-assert Solution.convertToTitle(2147483647) == "FXSHRXW"
+    # Input: columnNumber = 2147483647, Output: "FXSHRXW"
+    assert Solution.convertToTitle(2147483647) == "FXSHRXW"
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
