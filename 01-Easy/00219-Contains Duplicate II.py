b9550ad94ac53f7c7b109a1069ddbf453b929fb4
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-24 23:09:13 -0400

    Update 219

--- 01-Easy/00219-Contains Duplicate II.py
+++ 01-Easy/00219-Contains Duplicate II.py
@@ -1,8 +1,7 @@
 """219. Contains Duplicate II
 Link: https://leetcode.com/problems/contains-duplicate-ii/
 Difficulty: Easy
-Description: Given an integer array nums and an integer k, return true if there
-are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k."""
+Description: Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k."""
 
 from typing import List
 
@@ -11,27 +10,32 @@ class Solution:
     @staticmethod
     def containsNearbyDuplicate(nums: List[int], k: int) -> bool:
         """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(n).
-           Similar to 0001-Two-Sum.py"""
-        # Initialize a dictionary to store the element and its index
-        num_dict = {}
+        Similar to 1. Two Sum."""
+        num_indices = {}
+        
         # Iterate through the list
         for i, num in enumerate(nums):
             # Check if the element is already in the dictionary
-            # and the difference between the indices is less than or equal to k
-            if num in num_dict and i - num_dict[num] <= k:
-                return True
-            # Update the index of the element in the dictionary
-            num_dict[num] = i
+            if num in num_indices:
+                # Check if the absolute difference of indices is less than or equal to k
+                if i - num_indices[num] <= k:
+                    return True
+            # Update the last seen index of the element
+            num_indices[num] = i
         return False
 
 
-# Input: nums = [1,2,3,1], k = 3, Output: True
-assert Solution.containsNearbyDuplicate([1, 2, 3, 1], 3) is True
+def unit_tests():
+    # Input: nums = [1,2,3,1], k = 3, Output: True
+    assert Solution.containsNearbyDuplicate([1, 2, 3, 1], 3) is True
 
-# Input: nums = [1,0,1,1], k = 1, Output: True
-assert Solution.containsNearbyDuplicate([1, 0, 1, 1], 1) is True
+    # Input: nums = [1,0,1,1], k = 1, Output: True
+    assert Solution.containsNearbyDuplicate([1, 0, 1, 1], 1) is True
 
-# Input: nums = [1,2,3,1,2,3], k = 2, Output: False
-assert Solution.containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2) is False
+    # Input: nums = [1,2,3,1,2,3], k = 2, Output: False
+    assert Solution.containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2) is False
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
