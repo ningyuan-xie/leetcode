6692a56c5614e8f08ba1a06e4b049e4b87d6b47f
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-25 20:13:28 -0400

    Update 231

--- 01-Easy/00231-Power-of-Two.py
+++ 01-Easy/00231-Power-of-Two.py
@@ -1,52 +1,38 @@
 """231. Power of Two
 Link: https://leetcode.com/problems/power-of-two/
 Difficulty: Easy
-Description: Given an integer n, return true if it is a power of two. Otherwise, return false."""
+Description: Given an integer n, return true if it is a power of two. Otherwise, return false.
+An integer n is a power of two, if there exists an integer x such that n == 2x."""
 
 
 class Solution:
     @staticmethod
     def isPowerOfTwo(n: int) -> bool:
-        """Optimal Solution: Math (only for prime).
-           Time Complexity: O(1), Space Complexity: O(1)."""
-        # The maximum power of 2 that can be stored in a 32-bit integer is 2^30 = 1073741824
-        return n > 0 and (2**30) % n == 0
-
-    @staticmethod
-    def isPowerOfTwoBit(n: int) -> bool:
-        """Optimal Solution: Bit Manipulation. Time Complexity: O(1), Space Complexity: O(1).
-           Bitwise AND operator & : 1 & 1 = 1, 1 & 0 = 0, 0 & 0 = 0."""
+        """Optimal Solution: Recursion. Time Complexity: O(log(n)), Space Complexity: O(log(n))."""
         # Base case: if n is less than or equal to 0, return False
         if n <= 0:
             return False
-        # If n is a power of 2, then n only has 1 bit = 1, so n & (n - 1) = 0
-        # E.g. 4 (100) & 3 (011) = 0, 5 (101) & 4 (100) = 4 (100), 8 (1000) & 7 (0111) = 0
-        return n & (n - 1) == 0
-
-    @staticmethod
-    def isPowerOfTwoRecursion(n: int) -> bool:
-        """Alternative Solution: Recursion.
-           Time Complexity: O(log(n)), Space Complexity: O(log(n))."""
-        # Base case: if n is less than or equal to 0, return False
-        if n <= 0:
-            return False
-        # Base case: if n is equal to 1, return True
+        # Base case: if n is 1, return True (2^0 = 1)
         if n == 1:
             return True
-        # If n is divisible by 2, then recursively check if n // 2 is a power of 2
-        return n % 2 == 0 and Solution.isPowerOfTwoRecursion(n // 2)
+        # Recursive case: check if n is divisible by 2 and call the function recursively with n divided by 2
+        return n % 2 == 0 and Solution.isPowerOfTwo(n // 2)
 
 
-# Input: 1
-assert Solution.isPowerOfTwo(1) is True
+def unit_tests():
+    # Input: 1, Output: true
+    assert Solution.isPowerOfTwo(1) is True
 
-# Input: 16
-assert Solution.isPowerOfTwo(16) is True
+    # Input: 16, Output: true
+    assert Solution.isPowerOfTwo(16) is True
 
-# Input: 3
-assert Solution.isPowerOfTwoBit(3) is False
+    # Input: 3, Output: false
+    assert Solution.isPowerOfTwo(3) is False
 
-# Input: 4
-assert Solution.isPowerOfTwoRecursion(4) is True
+    # Input: 4, Output: true
+    assert Solution.isPowerOfTwo(4) is True
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
