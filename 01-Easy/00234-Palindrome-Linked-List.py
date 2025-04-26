6e4d455288f39975b8e85eaef0dfa0678decd8e4
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-26 14:40:36 -0400

    Update 234

--- 01-Easy/00234-Palindrome-Linked-List.py
+++ 01-Easy/00234-Palindrome-Linked-List.py
@@ -1,7 +1,7 @@
 """234. Palindrome Linked List
 Link: https://leetcode.com/problems/palindrome-linked-list/
 Difficulty: Easy
-Description: Given the head of a singly linked list, return true if it is a palindrome.
+Description: Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
 Follow up: Could you do it in O(n) time and O(1) space?"""
 
 from typing import Optional
@@ -11,31 +11,38 @@ from package.data_structures import ListNode
 class Solution:
     @staticmethod
     def isPalindrome(head: Optional[ListNode]) -> bool:
-        """Optimal Solution: Reverse the second half of the linked list and compare
-           the first half and the second half. Time Complexity: O(n), Space Complexity: O(1)."""
-        # Base case: if head is None or head.next is None, return True
+        """Optimal Solution: Reverse Half. Time Complexity: O(n), Space Complexity: O(1)."""
+        # Base case
         if not head or not head.next:
             return True
 
-        def reverse_linked_list(node: ListNode) -> ListNode:
-            """Helper function to reverse a linked list: same as 0206-Reverse-Linked-List.py"""
-            prev = None
-            while node:
-                next_node = node.next  # Save the next node for the next iteration
-                node.next = prev  # Break the current's next node and point to the previous node
-                prev, node = node, next_node  # Move to the next node by updating two pointers
-            return prev
+        def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
+            """Helper function to reverse a linked list: from 206. Reverse Linked List."""
+            # Initialize previous and current pointers
+            new_list = None  # always points to the head of the new list
+            current = head  # used to traverse the original list
 
-        # Find the middle of the linked list using pointers: similar to 0141-Linked-List-Cycle.py
-        slow = fast = head
+            while current:
+                # Store the next node
+                next_node = current.next
+                # Reverse the link
+                current.next = new_list
+                # Move the pointers one position forward
+                new_list = current
+                current = next_node
+
+            return new_list
+
+        # Find the middle of the linked list using slow and fast pointers
+        slow, fast = head, head
         while fast and fast.next:  # E.g. [1, 2, 3, 2, 1]
             slow, fast = slow.next, fast.next.next  # slow stops at 3, fast stops at 1
 
         # Reverse the second half of the linked list
-        second_half = reverse_linked_list(slow)  # E.g. [3, 2, 1] -> [1, 2, 3]
+        second_half = reverseList(slow)  # [3, 2, 1] -> [1, 2, 3]
 
         # Compare the first half and the second half of the linked list
-        first_half = head  # E.g. [1, 2, 3, 2, 1]
+        first_half = head
         while second_half:  # Compare [1, 2, 3, 2, 1] and [1, 2, 3]
             if first_half.val != second_half.val:
                 return False
@@ -43,16 +50,20 @@ class Solution:
         return True
 
 
-# Input: head = [1, 2, 2, 1]
-head_test = ListNode.build_linked_list([1, 2, 2, 1])
-assert Solution.isPalindrome(head_test) is True
+def unit_tests():
+    # Input: head = [1, 2, 2, 1]
+    head = ListNode.build_linked_list([1, 2, 2, 1])
+    assert Solution.isPalindrome(head) is True
 
-# Input: head = [1, 2]
-head_test = ListNode.build_linked_list([1, 2])
-assert Solution.isPalindrome(head_test) is False
+    # Input: head = [1, 2]
+    head = ListNode.build_linked_list([1, 2])
+    assert Solution.isPalindrome(head) is False
 
-# Input: head = [1, 2, 3, 2, 1]
-head_test = ListNode.build_linked_list([1, 2, 3, 2, 1])
-assert Solution.isPalindrome(head_test) is True
+    # Input: head = [1, 2, 3, 2, 1]
+    head = ListNode.build_linked_list([1, 2, 3, 2, 1])
+    assert Solution.isPalindrome(head) is True
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
