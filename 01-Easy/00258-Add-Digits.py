b2602da1a424042ecca8c012297dd224ff9789f0
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-26 20:22:12 -0400

    Update 258

--- 01-Easy/00258-Add-Digits.py
+++ 01-Easy/00258-Add-Digits.py
@@ -1,49 +1,32 @@
 """258. Add Digits
 Link: https://leetcode.com/problems/add-digits/
 Difficulty: Easy
-Description: Given an integer num, repeatedly add its digits until the result has only
-one digit, and return it."""
+Description: Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+Follow up: Could you do it without any loop/recursion in O(1) runtime?"""
 
 
 class Solution:
     @staticmethod
     def addDigits(n: int) -> int:
-        """Optimal Solution: Digital Root. Time Complexity: O(1), Space Complexity: O(1)."""
-        # The digital root (dr) of a number is given by the formula:
-        # dr(n) = 1 + ((n - 1) % 9)
+        """Optimal Solution: Math. Time Complexity: O(1), Space Complexity: O(1).
+        The digital root of a number dr(n) = 1 + ((n - 1) % 9).
+        E.g. dr(38) = 1 + ((38 - 1) % 9) = 1 + (37 % 9) = 1 + 1 = 2;
+        E.g. dr(0) = 1 + ((0 - 1) % 9) = 1 + (-1 % 9) = 1 + -1 = 0."""
+        # Digital Root Formula: 1 + (n - 1) % 9
         return 1 + ((n - 1) % 9) if n > 0 else 0
-        # E.g. dr(38) = 1 + ((38 - 1) % 9) = 1 + (37 % 9) = 1 + 1 = 2
-        # E.g. dr(0) = 1 + ((0 - 1) % 9) = 1 + (-1 % 9) = 1 + -1 = 0
-
-    @staticmethod
-    def addDigitsRecursion(n: int) -> int:
-        """Alternative Solution: Recursion.
-           Time Complexity: O(log(n)), Space Complexity: O(log(n))."""
-        # Base case: if the number is less than 10, return the number
-        if n < 10:
-            return n
-
-        # Initialize a variable to store the sum of the digits
-        sum_digits = 0
-
-        # Iterate through the digits of the number
-        while n > 0:
-            # Add the last digit to the sum
-            sum_digits += n % 10
-            # Remove the last digit from the number
-            n //= 10
-
-        # Recursively call the function with the sum of the digits
-        return Solution.addDigitsRecursion(sum_digits)
 
 
-# Input: num = 38, Output: 2
-assert Solution.addDigits(38) == 2
+def unit_tests(): 
+    # Input: num = 38, Output: 2
+    assert Solution.addDigits(38) == 2
 
-# Input: num = 0, Output: 0
-assert Solution.addDigitsRecursion(0) == 0
+    # Input: num = 0, Output: 0
+    assert Solution.addDigits(0) == 0
 
-# Input: num = 9, Output: 9
-assert Solution.addDigitsRecursion(9) == 9
+    # Input: num = 9, Output: 9
+    assert Solution.addDigits(9) == 9
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
