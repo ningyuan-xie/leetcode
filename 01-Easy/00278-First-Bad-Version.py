b0df866d043c4f4eec31e96f0448331f39a320e0
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-27 20:15:24 -0400

    Update 278

--- 01-Easy/00278-First-Bad-Version.py
+++ 01-Easy/00278-First-Bad-Version.py
@@ -1,15 +1,9 @@
 """278. First Bad Version
 Link: https://leetcode.com/problems/first-bad-version/
 Difficulty: Easy
-Description: You are a product manager and currently leading a team to develop a new product.
-Unfortunately, the latest version of your product fails the quality check.
-Since each version is developed based on the previous version,
-all the versions after a bad version are also bad.
-Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one,
-which causes all the following ones to be bad.
-You are given an API bool isBadVersion(version) which returns whether version is bad.
-Implement a function to find the first bad version.
-You should minimize the number of calls to the API."""
+Description: You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API."""
 
 
 def isBadVersion(version: int) -> bool:
@@ -20,28 +14,27 @@ def isBadVersion(version: int) -> bool:
 class Solution:
     @staticmethod
     def firstBadVersion(n: int) -> int:
-        """Optimal Solution: Binary Search. Time Complexity: O(log(n)), Space Complexity: O(1).
-           Similar to 0069-Sqrt(x).py and 0367-Valid-Perfect-Square.py"""
-        # Initialize the left and right pointers
+        """Optimal Solution: Binary Search. Time Complexity: O(log(n)), Space Complexity: O(1)."""
         left, right = 1, n
-        # Perform binary search to find the first bad version
         while left <= right:
-            mid = left + (right - left) // 2
-
-            # If the middle version is not bad, move the left pointer to the right
-            if not isBadVersion(mid):
-                left = mid + 1
-            # Otherwise, move the right pointer to the left
-            else:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
                 right = mid - 1
-        # Can return either left or right since they are equal
+            else:
+                left = mid + 1
+
+        # When the loop ends, left is the first bad version
         return left
 
 
-# Input: n = 5, bad = 4, Output: 4. Explanation: The first bad version is 4
-assert Solution.firstBadVersion(5) == 4
+def unit_tests():
+    # Input: n = 5, bad = 4, Output: 4.
+    assert Solution.firstBadVersion(5) == 4
 
-# Input: n = 10, bad = 4, Output: 4. Explanation: The first bad version is 4
-assert Solution.firstBadVersion(10) == 4
+    # Input: n = 10, bad = 4, Output: 4.
+    assert Solution.firstBadVersion(10) == 4
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
