267f0e04f31215c2a0abf3d8967e2d8d0259e373
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-27 20:03:22 -0400

    Update 263

--- 01-Easy/00263-Ugly-Number.py
+++ 01-Easy/00263-Ugly-Number.py
@@ -1,34 +1,38 @@
 """263. Ugly Number
 Link: https://leetcode.com/problems/ugly-number/
 Difficulty: Easy
-Description: Write a program to check whether a given number is an ugly number.
-An ugly number is a positive number whose prime factors only include 2, 3, and/or 5."""
+Description: An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
+Given an integer n, return true if n is an ugly number."""
 
 
 class Solution:
     @staticmethod
     def isUgly(n: int) -> bool:
-        """Optimal Solution: Iteration. Time Complexity: O(log(n)), Space Complexity: O(1)."""
-        # Base case: if the number is less than or equal to 0, return False
+        """Optimal Solution: While Loop. Time Complexity: O(log(n)), Space Complexity: O(1)."""
+        # Check if n is less than or equal to 0
         if n <= 0:
             return False
-
-        # Check if the number is divisible by 2, 3, and 5
-        for prime in [2, 3, 5]:
-            while n % prime == 0:  # Divisible by prime
-                n //= prime  # E.g., 6 // 2 = 3, 3 // 3 = 1; 8 // 2 = 4, 4 // 2 = 2, 2 // 2 = 1
-
-        # If the number is an ugly number, the result should be 1
+        
+        # Continuously divide n by 2, 3, and 5 until undivisible by any of them
+        for i in [2, 3, 5]:
+            while n % i == 0:
+                n //= i
+        
+        # If n is reduced to 1, it is an ugly number
         return n == 1
 
 
-# Input: num = 6, Output: True
-assert Solution.isUgly(6) is True
+def unit_tests():
+    # Input: num = 6, Output: True
+    assert Solution.isUgly(6) is True
 
-# Input: num = 8, Output: True
-assert Solution.isUgly(8) is True
+    # Input: num = 8, Output: True
+    assert Solution.isUgly(8) is True
 
-# Input: num = 14, Output: False
-assert Solution.isUgly(14) is False
+    # Input: num = 14, Output: False
+    assert Solution.isUgly(14) is False
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
