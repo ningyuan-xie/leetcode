30402e2817cc8046e9d38e303b31e9a47c8a940f
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-28 20:13:31 -0400

    Update 326

--- 01-Easy/00326-Power of Three.py
+++ 01-Easy/00326-Power of Three.py
@@ -2,21 +2,14 @@
 Link: https://leetcode.com/problems/power-of-three/
 Difficulty: Easy
 Description: Given an integer n, return true if it is a power of three. Otherwise, return false.
-Follow up: Could you solve it without loops/recursion?"""
+An integer n is a power of three, if there exists an integer x such that n == 3x."""
 
 
 class Solution:
     @staticmethod
     def isPowerOfThree(n: int) -> bool:
-        """Optimal Solution: Math. Time Complexity: O(1), Space Complexity: O(1).
-           Similar to 0231-Power-of-Two.py"""
-        # The maximum power of 3 that can be stored in a 32-bit integer is 3^19 = 1162261467
-        return n > 0 and (3**19) % n == 0
-
-    @staticmethod
-    def isPowerOfThreeRecursion(n: int) -> bool:
-        """Alternative Solution: Recursion. Time Complexity: O(log(n)), Space Complexity: O(log(n)).
-           Similar to 0231-Power-of-Two.py"""
+        """Optimal Solution: Recursion. Time Complexity: O(log(n)), Space Complexity: O(log(n)).
+        Similar to 231. Power of Two."""
         # Base case: if n is less than or equal to 0, return False
         if n <= 0:
             return False
@@ -24,19 +17,23 @@ class Solution:
         if n == 1:
             return True
         # If n is divisible by 3, then recursively check if n // 3 is a power of 3
-        return n % 3 == 0 and Solution.isPowerOfThreeRecursion(n // 3)
+        return n % 3 == 0 and Solution.isPowerOfThree(n // 3)
 
 
-# Input: n = 27, Output: True
-assert Solution.isPowerOfThree(27) is True
+def unit_tests():
+    # Input: n = 27, Output: True
+    assert Solution.isPowerOfThree(27) is True
 
-# Input: n = 0, Output: False
-assert Solution.isPowerOfThree(0) is False
+    # Input: n = 0, Output: False
+    assert Solution.isPowerOfThree(0) is False
 
-# Input: n = 9, Output: True
-assert Solution.isPowerOfThreeRecursion(9) is True
+    # Input: n = 9, Output: True
+    assert Solution.isPowerOfThree(9) is True
 
-# Input: n = 45, Output: False
-assert Solution.isPowerOfThreeRecursion(45) is False
+    # Input: n = 45, Output: False
+    assert Solution.isPowerOfThree(45) is False
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
