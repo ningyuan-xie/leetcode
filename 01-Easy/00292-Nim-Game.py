dc0b3f6d62b561e8f17770b4be13e21b8902f12e
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-28 20:10:22 -0400

    Update 292

--- 01-Easy/00292-Nim-Game.py
+++ 01-Easy/00292-Nim-Game.py
@@ -2,10 +2,11 @@
 Link: https://leetcode.com/problems/nim-game/
 Difficulty: Easy
 Description: You are playing the following Nim Game with your friend:
-Initially, there is a heap of stones on the table.
-You and your friend will alternate taking turns, and you go first.
-On each turn, the person whose turn it is will remove 1 to 3 stones from the heap.
-The one who removes the last stone is the winner."""
+• Initially, there is a heap of stones on the table.
+• You and your friend will alternate taking turns, and you go first.
+• On each turn, the person whose turn it is will remove 1 to 3 stones from the heap.
+• The one who removes the last stone is the winner.
+Given n, the number of stones in the heap, return true if you can win the game assuming both you and your friend play optimally, otherwise return false."""
 
 
 class Solution:
@@ -18,16 +19,20 @@ class Solution:
         return n % 4 != 0
 
 
-# Input: n = 4, Output: False
-assert Solution.canWinNim(4) is False
+def unit_tests():
+    # Input: n = 4, Output: False
+    assert Solution.canWinNim(4) is False
 
-# Input: n = 1, Output: True
-assert Solution.canWinNim(1) is True
+    # Input: n = 1, Output: True
+    assert Solution.canWinNim(1) is True
 
-# Input: n = 2, Output: True
-assert Solution.canWinNim(2) is True
+    # Input: n = 2, Output: True
+    assert Solution.canWinNim(2) is True
 
-# Input: n = 7, Output: True
-assert Solution.canWinNim(7) is True  # The player can remove 3 stones, so the remaining stones are 4
+    # Input: n = 7, Output: True
+    assert Solution.canWinNim(7) is True  # The player can remove 3 stones, so the remaining stones are 4
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
