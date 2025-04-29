5e6ddcb1747e3066e4327c06a2ae10970a7a107f
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-29 16:28:36 -0400

    Update 342

--- 01-Easy/00342-Power-of-Four.py
+++ 01-Easy/00342-Power-of-Four.py
@@ -2,14 +2,13 @@
 Link: https://leetcode.com/problems/power-of-four/
 Difficulty: Easy
 Description: Given an integer n, return true if it is a power of four. Otherwise, return false.
-Follow up: Could you solve it without loops/recursion?"""
+An integer n is a power of four, if there exists an integer x such that n == 4x."""
 
 
 class Solution:
-    def isPowerOfFour(self, n: int) -> bool:
-        """Optimal Solution: Recursion. Time Complexity: O(log(n)), Space Complexity: O(log(n)).
-           Similar to 0231-Power-of-Two.py and 0326-Power-of-Three.py.
-           Written in instance method format instead of static method format"""
+    @staticmethod
+    def isPowerOfFour(n: int) -> bool:
+        """Optimal Solution: Recursion. Time Complexity: O(log(n)), Space Complexity: O(log(n))."""
         # Base case: if n is less than or equal to 0, return False
         if n <= 0:
             return False
@@ -17,28 +16,20 @@ class Solution:
         if n == 1:
             return True
         # If n is divisible by 4, then recursively check if n // 4 is a power of 4
-        # When making a recursive call within a class method,
-        # do not need to explicitly pass self again as an argument
-        return n % 4 == 0 and self.isPowerOfFour(n // 4)
+        return n % 4 == 0 and Solution.isPowerOfFour(n // 4)
 
 
-# Input: n = 2, Output: False
-solution = Solution()  # Instantiate a Solution class to call the instance method
-assert solution.isPowerOfFour(2) is False
+def unit_tests():
+    # Input: n = 16, Output: True
+    assert Solution.isPowerOfFour(16) is True
 
-# Input: n = 8, Output: False
-assert solution.isPowerOfFour(8) is False
+    # Input: n = 5, Output: False
+    assert Solution.isPowerOfFour(5) is False
 
-# Input: n = 16, Output: True
-assert solution.isPowerOfFour(16) is True
+    # Input: n = 1, Output: True
+    assert Solution.isPowerOfFour(1) is True
 
-# Input: n = 5, Output: False
-assert solution.isPowerOfFour(5) is False
 
-# Input: n = 1, Output: True
-assert solution.isPowerOfFour(1) is True
-
-# Input: n = 0, Output: False
-assert solution.isPowerOfFour(0) is False
-
-print("All unit tests are passed.")
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
