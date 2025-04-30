fa778862b7b5c0f548cb8ecf11abc21ae304bbb3
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-30 16:41:06 -0400

    Update 350

--- 01-Easy/00350-Intersection-of-Two-Arrays-II.py
+++ 01-Easy/00350-Intersection-of-Two-Arrays-II.py
@@ -1,9 +1,7 @@
 """350. Intersection of Two Arrays II
 Link: https://leetcode.com/problems/intersection-of-two-arrays-ii/
 Difficulty: Easy
-Description: Given two integer arrays nums1 and nums2, return an array of their intersection.
-Each element in the result should appear as many times as it shows in both arrays,
-and you may return the result in any order."""
+Description: Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order."""
 
 from typing import List
 
@@ -12,26 +10,31 @@ class Solution:
     @staticmethod
     def intersect(nums1: List[int], nums2: List[int]) -> List[int]:
         """Optimal Solution: Hash Table. Time Complexity: O(n + m), Space Complexity: O(n).
-           Similar to 0242-Valid-Anagram.py"""
-        # Initialize a hash table to store the frequency of each element in nums1
-        num_freq = {}
+        Similar to 242. Valid Anagram."""
+        # Create a dictionary to count occurrences of each number in nums1
+        count = {}
         for num in nums1:
-            num_freq[num] = num_freq.get(num, 0) + 1  # E.g., num_freq = {1: 2, 2: 2}
-        # Initialize a list to store the intersection of nums1 and nums2
+            count[num] = count.get(num, 0) + 1
+
+        # Create a list to store the intersection
         intersection = []
+
+        # Iterate through nums2 and check if the number is in the count dictionary
         for num in nums2:
-            # If the element is in the hash table and the frequency is greater than 0
-            if num in num_freq and num_freq[num] > 0:
+            if num in count and count[num] > 0:
                 intersection.append(num)
-                # Decrement the frequency of the element in the hash table after using it
-                num_freq[num] -= 1
+                count[num] -= 1
         return intersection
 
 
-# Input: nums1 = [1, 2, 2, 1], nums2 = [2, 2], Output: [2, 2]
-assert Solution.intersect([1, 2, 2, 1], [2, 2]) == [2, 2]
+def unit_tests():
+    # Input: nums1 = [1, 2, 2, 1], nums2 = [2, 2], Output: [2, 2]
+    assert Solution.intersect([1, 2, 2, 1], [2, 2]) == [2, 2]
 
-# Input: nums1 = [4, 9, 5], nums2 = [9, 4, 9, 8, 4], Output: [9, 4]
-assert Solution.intersect([4, 9, 5], [9, 4, 9, 8, 4]) == [9, 4]
+    # Input: nums1 = [4, 9, 5], nums2 = [9, 4, 9, 8, 4], Output: [9, 4]
+    assert Solution.intersect([4, 9, 5], [9, 4, 9, 8, 4]) == [9, 4]
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
