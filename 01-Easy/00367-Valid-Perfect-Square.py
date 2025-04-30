efcaa1afffe1f7f057385c7e65c0b8b7398ede2b
Ningyuan Xie <howard.xie25@gmail.com>
2025-04-30 16:59:19 -0400

    Update 367

--- 01-Easy/00367-Valid-Perfect-Square.py
+++ 01-Easy/00367-Valid-Perfect-Square.py
@@ -1,46 +1,41 @@
 """367. Valid Perfect Square
 Link: https://leetcode.com/problems/valid-perfect-square/
 Difficulty: Easy
-Description: Given a positive integer num, write a function which returns True
-if num is a perfect square else False."""
+Description: Given a positive integer num, return true if num is a perfect square or false otherwise.
+A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+You must not use any built-in library function, such as sqrt."""
 
 
 class Solution:
     @staticmethod
     def isPerfectSquare(num: int) -> bool:
         """Optimal Solution: Binary Search. Time Complexity: O(log(n)), Space Complexity: O(1).
-           Similar to 0069-Sqrt(x).py"""
-        # Base case: 0 and 1 are perfect squares
-        if num < 2:
-            return True
-        # Initialize the left and right pointers for binary search
-        left, right = 2, num  # E.g., num = 16 -> left = 2, right = 16
-        # Loop until left pointer > right pointer
+        Similar to 69. Sqrt(x)."""
+        left, right = 1, num
         while left <= right:
-            mid = left + (right - left) // 2
-            # Calculate the square of the middle pointer
-            mid_squared = mid ** 2
-            if mid_squared == num:
+            mid = (left + right) // 2
+            if mid * mid == num:
                 return True
-            # The square of the middle is less than the number, so the number is on the right side
-            elif mid_squared < num:
+            elif mid * mid < num:
                 left = mid + 1
-            # The square of the middle is greater than the number, so the number is on the left side
             else:
                 right = mid - 1
+        
+        # If the loop ends, it means num is not a perfect square
         return False
 
 
-# Input: num = 16, Output: True
-# Explanation: 16 is a perfect square because 4 * 4 = 16
-assert Solution.isPerfectSquare(16) is True
+def unit_tests():
+    # Input: num = 16, Output: True
+    assert Solution.isPerfectSquare(16) is True
 
-# Input: num = 14, Output: False
-# Explanation: 14 is not a perfect square
-assert Solution.isPerfectSquare(14) is False
+    # Input: num = 14, Output: False
+    assert Solution.isPerfectSquare(14) is False
 
-# Input: num = 1, Output: True
-# Explanation: 1 is a perfect square because 1 * 1 = 1
-assert Solution.isPerfectSquare(1) is True
+    # Input: num = 1, Output: True
+    assert Solution.isPerfectSquare(1) is True
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
