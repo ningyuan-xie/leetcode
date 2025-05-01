ed64343b4dfd9a0c3bf595332c00da78f76b55db
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-01 11:54:56 -0400

    Update 387

--- 01-Easy/00387-First-Unique-Character-in-a-String.py
+++ 01-Easy/00387-First-Unique-Character-in-a-String.py
@@ -1,33 +1,36 @@
 """387. First Unique Character in a String
 Link: https://leetcode.com/problems/first-unique-character-in-a-string/
 Difficulty: Easy
-Description: Given a string s, return the first non-repeating character
-in it and return its index. If it does not exist, return -1."""
+Description: Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1."""
 
 
 class Solution:
     @staticmethod
     def firstUniqChar(s: str) -> int:
-        """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(1).
-           Similar to 0383-Ransom-Note.py"""
-        # Initialize a hash table to store the frequency of characters in the string
-        char_freq = {}
+        """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(n)."""
+        # Create a dictionary to count occurrences of each character
+        char_count = {}
         for char in s:
-            char_freq[char] = char_freq.get(char, 0) + 1  # E.g., char_freq = {'l': 1, 'e': 3}
-        # Traverse the string to find the first non-repeating character
+            char_count[char] = char_count.get(char, 0) + 1
+
+        # Iterate through the string to find the first unique character
         for i, char in enumerate(s):
-            if char_freq[char] == 1:
+            if char_count[char] == 1:
                 return i
         return -1
 
 
-# Input: s = "leetcode", Output: 0
-assert Solution.firstUniqChar("leetcode") == 0
+def unit_tests():
+    # Input: s = "leetcode", Output: 0
+    assert Solution.firstUniqChar("leetcode") == 0
 
-# Input: s = "loveleetcode", Output: 2
-assert Solution.firstUniqChar("loveleetcode") == 2
+    # Input: s = "loveleetcode", Output: 2
+    assert Solution.firstUniqChar("loveleetcode") == 2
 
-# Input: s = "aabb", Output: -1
-assert Solution.firstUniqChar("aabb") == -1
+    # Input: s = "aabb", Output: -1
+    assert Solution.firstUniqChar("aabb") == -1
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
