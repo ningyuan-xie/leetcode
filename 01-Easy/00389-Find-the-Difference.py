ced898d55d11369de1bb2a4c582792b021488d6c
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-01 12:40:58 -0400

    Update 389

--- 01-Easy/00389-Find-the-Difference.py
+++ 01-Easy/00389-Find-the-Difference.py
@@ -9,30 +9,31 @@ Return the letter that was added to t."""
 class Solution:
     @staticmethod
     def findTheDifference(s: str, t: str) -> str:
-        """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(1).
-           Similar to 0383-Ransom-Note.py"""
-        # Initialize a hash table to store the frequency of characters in string s
-        char_freq = {}
+        """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(n)."""
+        # Create a dictionary to count occurrences of each character in s
+        char_count = {}
         for char in s:
-            char_freq[char] = char_freq.get(char, 0) + 1  # E.g., char_freq = {'a': 1, 'b': 1}
-        # Traverse string t and decrement the frequency of characters in the hash table
+            char_count[char] = char_count.get(char, 0) + 1
+
+        # Check if t contains the extra character
         for char in t:
-            # If the character is in the hash table and the frequency is greater than 0
-            if char in char_freq and char_freq[char] > 0:
-                # Decrement the frequency of the character in the hash table after using it
-                char_freq[char] -= 1
-            else:
+            if char not in char_count or char_count[char] == 0:
                 return char
+            char_count[char] -= 1
         return ""
 
 
-# Input: s = "abcd", t = "abcde", Output: "e"
-assert Solution.findTheDifference("abcd", "abcde") == "e"
+def unit_tests():
+    # Input: s = "abcd", t = "abcde", Output: "e"
+    assert Solution.findTheDifference("abcd", "abcde") == "e"
 
-# Input: s = "", t = "y", Output: "y"
-assert Solution.findTheDifference("", "y") == "y"
+    # Input: s = "", t = "y", Output: "y"
+    assert Solution.findTheDifference("", "y") == "y"
 
-# Input: s = "a", t = "aa", Output: "a"
-assert Solution.findTheDifference("a", "aa") == "a"
+    # Input: s = "a", t = "aa", Output: "a"
+    assert Solution.findTheDifference("a", "aa") == "a"
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
