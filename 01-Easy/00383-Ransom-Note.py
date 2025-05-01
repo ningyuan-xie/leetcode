c1da86e88e1e844ecce4b6e6a0d360ad555e50cb
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-01 11:52:12 -0400

    Update 383

--- 01-Easy/00383-Ransom-Note.py
+++ 01-Easy/00383-Ransom-Note.py
@@ -1,38 +1,39 @@
 """383. Ransom Note
 Link: https://leetcode.com/problems/ransom-note/
 Difficulty: Easy
-Description: Given two strings ransomNote and magazine, return true
-if ransomNote can be constructed by using the letters from magazine and false otherwise.
+Description: Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
 Each letter in magazine can only be used once in ransomNote."""
 
 
 class Solution:
     @staticmethod
     def canConstruct(ransomNote: str, magazine: str) -> bool:
-        """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(1).
-           Similar to 0350-Intersection-of-Two-Arrays-II.py"""
-        # Create a hash table to store the frequency of each character in the magazine
-        char_freq = {}
+        """Optimal Solution: Hash Table. Time Complexity: O(n + m), Space Complexity: O(n).
+        Similar to 350. Intersection of Two Arrays II."""
+        # Create a dictionary to count occurrences of each character in magazine
+        char_count = {}
         for char in magazine:
-            char_freq[char] = char_freq.get(char, 0) + 1  # E.g., char_freq = {'a': 2, 'b': 1}
-        # Iterate through the ransom note to check if the magazine can construct the ransom note
+            char_count[char] = char_count.get(char, 0) + 1
+
+        # Check if ransomNote can be constructed from magazine
         for char in ransomNote:
-            # If the character is in the hash table and the frequency is greater than 0
-            if char in char_freq and char_freq[char] > 0:
-                # Decrement the frequency of the character in the hash table after using it
-                char_freq[char] -= 1
-            else:
+            if char not in char_count or char_count[char] == 0:
                 return False
+            char_count[char] -= 1
         return True
 
 
-# Input: ransomNote = "a", magazine = "b", Output: False
-assert Solution.canConstruct("a", "b") is False
+def unit_tests():
+    # Input: ransomNote = "a", magazine = "b", Output: False
+    assert Solution.canConstruct("a", "b") is False
 
-# Input: ransomNote = "aa", magazine = "ab", Output: False
-assert Solution.canConstruct("aa", "ab") is False
+    # Input: ransomNote = "aa", magazine = "ab", Output: False
+    assert Solution.canConstruct("aa", "ab") is False
 
-# Input: ransomNote = "aa", magazine = "aab", Output: True
-assert Solution.canConstruct("aa", "aab") is True
+    # Input: ransomNote = "aa", magazine = "aab", Output: True
+    assert Solution.canConstruct("aa", "aab") is True
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
