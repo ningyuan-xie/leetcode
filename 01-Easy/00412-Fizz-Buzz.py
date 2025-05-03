89b449089b1fdbcb17ce867c20a03fde8925148d
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-03 12:19:54 -0400

    Update 412

--- 01-Easy/00412-Fizz-Buzz.py
+++ 01-Easy/00412-Fizz-Buzz.py
@@ -2,10 +2,10 @@
 Link: https://leetcode.com/problems/fizz-buzz/
 Difficulty: Easy
 Description: Given an integer n, return a string array answer (1-indexed) where:
-- answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
-- answer[i] == "Fizz" if i is divisible by 3.
-- answer[i] == "Buzz" if i is divisible by 5.
-- answer[i] == i (as a string) if none of the above conditions are true."""
+• answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+• answer[i] == "Fizz" if i is divisible by 3.
+• answer[i] == "Buzz" if i is divisible by 5.
+• answer[i] == i (as a string) if none of the above conditions are true."""
 
 from typing import List
 
@@ -13,36 +13,32 @@ from typing import List
 class Solution:
     @staticmethod
     def fizzBuzz(n: int) -> List[str]:
-        """Optimal Solution: Modulo Operator. Time Complexity: O(n), Space Complexity: O(1)."""
-        # Initialize the output list
-        output = []
-        # Iterate from 1 to n (inclusive)
+        """Optimal Solution: Linear Scan. Time Complexity: O(n), Space Complexity: O(1)."""
+        result = []
+
         for i in range(1, n + 1):
-            # Check if the number is divisible by both 3 and 5
             if i % 3 == 0 and i % 5 == 0:
-                output.append("FizzBuzz")
-            # Check if the number is divisible by 3
+                result.append("FizzBuzz")
             elif i % 3 == 0:
-                output.append("Fizz")
-            # Check if the number is divisible by 5
+                result.append("Fizz")
             elif i % 5 == 0:
-                output.append("Buzz")
-            # If none of the above conditions are true, add the number as a string
+                result.append("Buzz")
             else:
-                output.append(str(i))
-        return output
+                result.append(str(i))
+        return result
 
 
-# Input = 3, Output = ["1", "2", "Fizz"]
-assert Solution.fizzBuzz(3) == ["1", "2", "Fizz"]
+def unit_tests():
+    # Input = 3, Output = ["1", "2", "Fizz"]
+    assert Solution.fizzBuzz(3) == ["1", "2", "Fizz"]
 
-# Input = 5, Output = ["1", "2", "Fizz", "4", "Buzz"]
-assert Solution.fizzBuzz(5) == ["1", "2", "Fizz", "4", "Buzz"]
+    # Input = 5, Output = ["1", "2", "Fizz", "4", "Buzz"]
+    assert Solution.fizzBuzz(5) == ["1", "2", "Fizz", "4", "Buzz"]
 
-# Input = 15,
-# Output = ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
-assert Solution.fizzBuzz(15) == ["1", "2", "Fizz", "4", "Buzz",
-                                 "Fizz", "7", "8", "Fizz", "Buzz",
-                                 "11", "Fizz", "13", "14", "FizzBuzz"]
+    # Input = 15, Output = ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+    assert Solution.fizzBuzz(15) == ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
