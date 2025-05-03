5acd729bfc18c5f2bc36e6040275c36c0305b3a5
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-03 12:26:50 -0400

    Update 434

--- 01-Easy/00434-Number-of-Segments-in-a-String.py
+++ 01-Easy/00434-Number-of-Segments-in-a-String.py
@@ -1,32 +1,28 @@
 """434. Number of Segments in a String
 Link: https://leetcode.com/problems/number-of-segments-in-a-string/
 Difficulty: Easy
-Description: You are given a string s, return the number of segments in the string.
+Description: Given a string s, return the number of segments in the string.
 A segment is defined to be a contiguous sequence of non-space characters."""
 
 
 class Solution:
     @staticmethod
     def countSegments(s: str) -> int:
-        """Optimal Solution: Iteration. Time Complexity: O(n), Space Complexity: O(1)."""
-        # Initialize the count of segments
-        count = 0
-        # Iterate through the string
-        for i in range(len(s)):
-            # If the current character is not a space
-            # and the previous character is a space or the first character
-            if s[i] != " " and (i == 0 or s[i - 1] == " "):
-                count += 1
-        return count
+        """Optimal Solution: String Manipulation. Time Complexity: O(n), Space Complexity: O(1)."""
+        return len(s.split()) if s else 0
 
 
-# Input: s = "Hello, my name is John", Output: 5
-assert Solution.countSegments("Hello, my name is John") == 5
+def unit_tests():
+    # Input: s = "Hello, my name is John", Output: 5
+    assert Solution.countSegments("Hello, my name is John") == 5
 
-# Input: s = "Hello", Output: 1
-assert Solution.countSegments("Hello") == 1
+    # Input: s = "Hello", Output: 1
+    assert Solution.countSegments("Hello") == 1
 
-# Input: s = "love live! mu'sic forever", Output: 4
-assert Solution.countSegments("love live! mu'sic forever") == 4
+    # Input: s = "love live! mu'sic forever", Output: 4
+    assert Solution.countSegments("love live! mu'sic forever") == 4
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
