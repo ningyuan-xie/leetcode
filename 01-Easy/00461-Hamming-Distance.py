5dbce2d42949b8eb60d30bd45922b823e57b4a00
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-04 17:00:14 -0400

    Update 461

--- 01-Easy/00461-Hamming-Distance.py
+++ 01-Easy/00461-Hamming-Distance.py
@@ -1,36 +1,39 @@
 """461. Hamming Distance
 Link: https://leetcode.com/problems/hamming-distance/
 Difficulty: Easy
-Description: The Hamming distance between two integers is the number of positions at which the
-corresponding bits are different.
+Description: The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
 Given two integers x and y, return the Hamming distance between them."""
 
 
 class Solution:
     @staticmethod
     def hammingDistance(x: int, y: int) -> int:
-        """Optimal Solution: Bit Manipulation. Time Complexity: O(1), Space Complexity: O(1).
-           Bitwise XOR operator ^ : 1 ^ 1 = 0, 1 ^ 0 = 1, 0 ^ 0 = 0.
-           Bitwise AND operator & : 1 & 1 = 1, 1 & 0 = 0, 0 & 0 = 0.
-           Similar to 0190-Reverse-Bits.py and 0191-Number-of-1-Bits.py"""
-        # Initialize the Hamming distance
-        hamming_distance = 0
-        # Calculate the XOR of x and y
-        xor = x ^ y  # E.g. x = 1 (0001), y = 4 (0100) -> xor = 5 (0101)
-        # Count the number of set bits in the XOR from RIGHT to LEFT
-        while xor:
-            hamming_distance += xor & 1  # Add the rightmost bit of the XOR to the Hamming distance
-            xor >>= 1  # Shift the xor to the right by 1, removing the bit just added
-        return hamming_distance
+        """Optimal Solution: Bit Manipulation. Time Complexity: O(1), Space Complexity: O(1)."""
+        # XOR the two integers to find the positions where the bits differ
+        xor_result = x ^ y
+        distance = 0
+
+        # Count the number of 1s in the binary representation of the XOR result
+        while xor_result:
+            # Increment the distance for each 1 found
+            distance += xor_result & 1
+            # Right shift to check the next bit
+            xor_result >>= 1
+
+        return distance
 
 
-# Input: x = 1, y = 4, Output: 2
-assert Solution.hammingDistance(1, 4) == 2
+def unit_tests():
+    # Input: x = 1, y = 4, Output: 2
+    assert Solution.hammingDistance(1, 4) == 2
 
-# Input: x = 3, y = 1, Output: 1
-assert Solution.hammingDistance(3, 1) == 1
+    # Input: x = 3, y = 1, Output: 1
+    assert Solution.hammingDistance(3, 1) == 1
 
-# Input: x = 0, y = 0, Output: 0
-assert Solution.hammingDistance(0, 0) == 0
+    # Input: x = 0, y = 0, Output: 0
+    assert Solution.hammingDistance(0, 0) == 0
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
