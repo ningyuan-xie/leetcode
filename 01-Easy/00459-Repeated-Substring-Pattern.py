42a718144d37788a9a0c5ec7e09e40e6c221a370
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-04 12:18:09 -0400

    Update 459

--- 01-Easy/00459-Repeated-Substring-Pattern.py
+++ 01-Easy/00459-Repeated-Substring-Pattern.py
@@ -1,41 +1,37 @@
 """459. Repeated Substring Pattern
 Link: https://leetcode.com/problems/repeated-substring-pattern/
 Difficulty: Easy
-Description: Given a string s, check if it can be constructed by taking a substring of it
-and appending multiple copies of the substring together."""
+Description: Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together."""
 
 
 class Solution:
     @staticmethod
     def repeatedSubstringPattern(s: str) -> bool:
-        """Sub-Optimal Solution: Brute Force. Time Complexity: O(n^2), Space Complexity: O(n)."""
-        # Substring's length ranges from 1 to len(s) // 2 + 1
-        for i in range(1, len(s) // 2 + 1):  # O(n/2). E.g. s = "abab", i = 1, 2
-            # If the length of the substring divides the length of the string
-            if len(s) % i == 0:
-                # Construct the substring by repeating the first i characters
-                substring = s[:i]
-                # Recover the string by repeating the substring: O(n)
-                repeated_string = substring * (len(s) // i)  # i = 1, "aaaa" -> i = 2, "abab"
-                # Compare the recovered string with the original string: O(n)
-                if repeated_string == s:
-                    return True
-        return False
+        """Optimal Solution: String Rotation. Time Complexity: O(n), Space Complexity: O(1).
+        If s can be formed by repeating a substring, then its rotation will appear inside (s + s)[1:-1]."""
+        return s in (s + s)[1:-1]
 
 
-# Input: s = "abab", Output: True
-assert Solution.repeatedSubstringPattern("abab") is True
+def unit_tests():
+    # Input: s = "abab", Output: True
+    assert Solution.repeatedSubstringPattern("abab") is True
 
-# Input: s = "aba", Output: False
-assert Solution.repeatedSubstringPattern("aba") is False
+    # Input: s = "aba", Output: False
+    assert Solution.repeatedSubstringPattern("aba") is False
 
-# Input: s = "abcabcabcabc", Output: True
-assert Solution.repeatedSubstringPattern("abcabcabcabc") is True
+    # Input: s = "abcabcabcabc", Output: True
+    assert Solution.repeatedSubstringPattern("abcabcabcabc") is True
 
-# Input: s = "abaababaab", Output: True
-assert Solution.repeatedSubstringPattern("abaababaab") is True
+    # Input: s = "abaababaab", Output: True
+    assert Solution.repeatedSubstringPattern("abaababaab") is True
 
-# Input: s = "ababba", Output: False
-assert Solution.repeatedSubstringPattern("ababba") is False
+    # Input: s = "ababba", Output: False
+    assert Solution.repeatedSubstringPattern("ababba") is False
 
-print("All unit tests are passed.")
+    # Input: s = "ababab", Output: True
+    assert Solution.repeatedSubstringPattern("ababab") is True
+
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
