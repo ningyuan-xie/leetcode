150b79d7a80a8b73042f935d53cfb5378ff32f10
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-04 12:46:38 -0400

    Update 455

--- 01-Easy/00455-Assign-Cookies.py
+++ 01-Easy/00455-Assign-Cookies.py
@@ -1,12 +1,8 @@
 """455. Assign Cookies
 Link: https://leetcode.com/problems/assign-cookies/
 Difficulty: Easy
-Description: Assume you are an awesome parent and want to give your children some cookies.
-But, you should give each child at most one cookie.
-Each child i has a greed factor g[i], which is the minimum size of a cookie that the child
-will be content with; and each cookie j has a size s[j]. If s[j] >= g[i], we can assign
-the cookie j to the child i, and the child i will be content.
-Your goal is to maximize the number of your content children and output the maximum number."""
+Description: Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie.
+Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with; and each cookie j has a size s[j]. If s[j] >= g[i], we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number of your content children and output the maximum number."""
 
 from typing import List
 
@@ -14,36 +10,37 @@ from typing import List
 class Solution:
     @staticmethod
     def findContentChildren(g: List[int], s: List[int]) -> int:
-        """Optimal Solution: Greedy Algorithm. Time Complexity: O(nlog(n)), Space Complexity: O(1).
-           The solution uses a greedy approach by matching the smallest available cookie to
-           the smallest possible greed factor, ensuring each decision maximizes the number
-           of content children."""
-        # Sort the greed factors and cookie sizes in ascending order
+        """Optimal Solution: Greedy. Time Complexity: O(nlog(n)), Space Complexity: O(1)."""
+        # Sort the greed factor and cookie sizes
         g.sort()
         s.sort()
-        # Initialize two pointers for the greed factors (children) and cookie sizes
-        i, j = 0, 0
 
-        # Greedy approach: the algorithm assigns each cookie to the first child it can satisfy,
-        # without reconsidering past assignments or backtracking
-        while i < len(g) and j < len(s):
-            # If the cookie satisfies the greed factor
-            if s[j] >= g[i]:
-                # Assign the cookie to the child and move to the next child
-                i += 1
-            # Regardless of whether the current cookie satisfies the current child,
-            # the cookie pointer j is incremented to consider the next available cookie
-            j += 1
-        return i
+        # Initialize pointers for children and cookies
+        child_i, cookie_i = 0, 0
+        content_children = 0
+
+        # Iterate through both lists
+        while child_i < len(g) and cookie_i < len(s):
+            # If the current cookie can satisfy the current child
+            if s[cookie_i] >= g[child_i]:
+                content_children += 1
+                child_i += 1
+            # Move to the next cookie
+            cookie_i += 1
+        return content_children
 
 
-# Input: g = [1, 2, 3], s = [1, 1], Output: 1
-assert Solution.findContentChildren([1, 2, 3], [1, 1]) == 1
+def unit_tests():
+    # Input: g = [1, 2, 3], s = [1, 1], Output: 1
+    assert Solution.findContentChildren([1, 2, 3], [1, 1]) == 1
 
-# Input: g = [1, 2], s = [1, 2, 3], Output: 2
-assert Solution.findContentChildren([1, 2], [1, 2, 3]) == 2
+    # Input: g = [1, 2], s = [1, 2, 3], Output: 2
+    assert Solution.findContentChildren([1, 2], [1, 2, 3]) == 2
 
-# Input: g = [1, 2, 3], s = [3], Output: 1
-assert Solution.findContentChildren([1, 2, 3], [3]) == 1
+    # Input: g = [1, 2, 3], s = [3], Output: 1
+    assert Solution.findContentChildren([1, 2, 3], [3]) == 1
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
