731eba6de27079fefba4efb49f0877cdc49707d8
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-04 17:47:47 -0400

    Update 476

--- 01-Easy/00476-Number-Complement.py
+++ 01-Easy/00476-Number-Complement.py
@@ -1,37 +1,40 @@
 """476. Number Complement
 Link: https://leetcode.com/problems/number-complement/
 Difficulty: Easy
-Description: Given a positive integer num, output its complement number.
-The complement strategy is to flip the bits of its binary representation."""
+Description: The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+Given an integer num, return its complement."""
 
 
 class Solution:
     @staticmethod
     def findComplement(num: int) -> int:
-        """Optimal Solution: Bit Manipulation. Time Complexity: O(1), Space Complexity: O(1).
-           Bitwise XOR operator ^ : 1 ^ 1 = 0, 1 ^ 0 = 1, 0 ^ 0 = 0.
-           Similar to 0190-Reverse-Bits.py"""
-        # Find the number of bits in the number
-        num_bits = num.bit_length()  # E.g. num = 5 (101) -> num_bits = 3
+        """Optimal Solution: Bit Manipulation. Time Complexity: O(log(n)), Space Complexity: O(1)."""
+        # Initialize mask to 1
+        mask = 1
 
-        # Construct a mask with all bits set to 1, which can be used to flip all the bits
-        # Shift 1 to the left by the number of bits and subtract 1
-        mask = (1 << num_bits) - 1  # 1000 -1 = 111
+        # Create a mask with all bits set to 1 for the length of num
+        while mask < num:
+            mask = (mask << 1) | 1
 
-        # XOR the number with the mask to flip all the bits: 5 (101) ^ 7 (111) = 2 (010)
+        # XOR num with the mask to get the complement
         return num ^ mask
 
 
-# Input: num = 5 (101), Output: 2 (010)
-assert Solution.findComplement(5) == 2
+def unit_tests():
+    # Input: num = 5, Output: 2
+    assert Solution.findComplement(5) == 2
 
-# Input: num = 1, Output: 0
-assert Solution.findComplement(1) == 0
+    # Input: num = 1, Output: 0
+    assert Solution.findComplement(1) == 0
 
-# Input: num = 7, Output: 0
-assert Solution.findComplement(7) == 0
+    # Input: num = 7, Output: 0
+    assert Solution.findComplement(7) == 0
 
-# Input: num = 10, Output: 5
-assert Solution.findComplement(10) == 5
+    # Input: num = 10, Output: 5
+    assert Solution.findComplement(10) == 5
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
