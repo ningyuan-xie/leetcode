0d351252c00a8b10d2758589e66376b9718dc400
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-05 18:04:38 -0400

    Update 485

--- 01-Easy/00485-Max-Consecutive-Ones.py
+++ 01-Easy/00485-Max-Consecutive-Ones.py
@@ -9,25 +9,37 @@ from typing import List
 class Solution:
     @staticmethod
     def findMaxConsecutiveOnes(nums: List[int]) -> int:
-        """Optimal Solution: Iteration. Time Complexity: O(n), Space Complexity: O(1)."""
-        max_consecutive, current_consecutive = 0, 0
+        """Optimal Solution: Linear Scan. Time Complexity: O(n), Space Complexity: O(1)."""
+        # Initialize the maximum count and current count
+        max_count = 0
+        current_count = 0
+
+        # Iterate through the array
         for num in nums:
             if num == 1:
-                current_consecutive += 1
+                # Increment the current count if the number is 1
+                current_count += 1
             else:
-                # Update the maximum consecutive and reset the current consecutive
-                max_consecutive = max(max_consecutive, current_consecutive)
-                current_consecutive = 0
-        return max(max_consecutive, current_consecutive)
+                # Update the maximum count if the current count is greater
+                max_count = max(max_count, current_count)
+                # Reset the current count
+                current_count = 0
+
+        # Final check to update max_count in case the last element was 1
+        return max(max_count, current_count)
 
 
-# Input: [1, 1, 0, 1, 1, 1], Output: 3
-assert Solution.findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]) == 3
+def unit_tests():
+    # Input: [1, 1, 0, 1, 1, 1], Output: 3
+    assert Solution.findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]) == 3
 
-# Input: [1, 0, 1, 1, 0, 1], Output: 2
-assert Solution.findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1]) == 2
+    # Input: [1, 0, 1, 1, 0, 1], Output: 2
+    assert Solution.findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1]) == 2
 
-# Input: [1, 1, 1, 1, 1, 1], Output: 6
-assert Solution.findMaxConsecutiveOnes([1, 1, 1, 1, 1, 1]) == 6
+    # Input: [1, 1, 1, 1, 1, 1], Output: 6
+    assert Solution.findMaxConsecutiveOnes([1, 1, 1, 1, 1, 1]) == 6
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
