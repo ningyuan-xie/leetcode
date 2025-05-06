ba83f22571859987cd9c455a66481fb91976b36e
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-06 16:55:14 -0400

    Update 504

--- 01-Easy/00504-Base-7.py
+++ 01-Easy/00504-Base-7.py
@@ -7,33 +7,38 @@ Description: Given an integer num, return a string of its base 7 representation.
 class Solution:
     @staticmethod
     def convertToBase7(num: int) -> str:
-        """Optimal Solution: Iterative Division.
-           Time Complexity: O(log(n)), Space Complexity: O(log(n)).
-           Similar to 0405-Convert-a-Number-to-Hexadecimal.py"""
-        # Base Case: If the number is 0, return "0"
+        """Optimal Solution: Math. Time Complexity: O(log(n)), Space Complexity: O(1).
+        Similar to 405. Convert a Number to Hexadecimal."""
+        # Handle the case for negative numbers
+        if num < 0:
+            return "-" + Solution.convertToBase7(-num)
+
+        # Base case: if num is 0, return "0"
         if num == 0:
             return "0"
-        # Initialize the result string and the sign
-        result = ""
-        sign, num = ("-", -num) if num < 0 else ("", num)
-        # Iterate through the iterative division process from RIGHT to LEFT
+
+        # Initialize an empty string to store the base 7 representation
+        base_7_str = ""
+
+        # Convert the number to base 7
         while num > 0:
-            remainder = num % 7
-            # Append the remainder to the front of the result string
-            result = str(remainder) + result
-            # Divide the number by 7 using floor division to move to the next digit on the left
+            base_7_str = str(num % 7) + base_7_str
             num //= 7
-        # Return the base 7 representation of the number
-        return sign + result
+
+        return base_7_str
 
 
-# Input: num = 100, Output: "202". Explanation: 100 = 2*7^2 + 0*7^1 + 2*7^0 = 202
-assert Solution.convertToBase7(100) == "202"
+def unit_tests():
+    # Input: num = 100, Output: "202".
+    assert Solution.convertToBase7(100) == "202"
 
-# Input: num = -7, Output: "-10". Explanation: -7 = -1*7^1 + 0*7^0 = -10
-assert Solution.convertToBase7(-7) == "-10"
+    # Input: num = -7, Output: "-10".
+    assert Solution.convertToBase7(-7) == "-10"
 
-# Input: num = 0, Output: "0". Explanation: 0 = 0*7^0 = 0
-assert Solution.convertToBase7(0) == "0"
+    # Input: num = 0, Output: "0".
+    assert Solution.convertToBase7(0) == "0"
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
