433b0524bbab4d892844b9d0e828f89717b2a196
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-07 19:36:17 -0400

    Update 520

--- 01-Easy/00520-Detect-Capital.py
+++ 01-Easy/00520-Detect-Capital.py
@@ -1,31 +1,31 @@
 """520. Detect Capital
 Link: https://leetcode.com/problems/detect-capital/
 Difficulty: Easy
-Description: Given a word, you need to judge whether the usage of capitals in it is right or not.
+Description: We define the usage of capitals in a word to be right when one of the following cases holds:
+• All letters in this word are capitals, like "USA".
+• All letters in this word are not capitals, like "leetcode".
+• Only the first letter in this word is capital, like "Google".
 Given a string word, return true if the usage of capitals in it is right."""
 
 
 class Solution:
     @staticmethod
     def detectCapitalUse(word: str) -> bool:
-        """Optimal Solution: Count the number of capital letters in the word.
-           Time Complexity: O(n), Space Complexity: O(1)."""
-        # Count the number of capital letters in the word
-        capital_count = sum(1 for char in word if char.isupper())
-        # Return True if the word is all lowercase or all uppercase
-        # or the first letter is uppercase and the rest are lowercase
-        return (capital_count == 0
-                or capital_count == len(word)
-                or (capital_count == 1 and word[0].isupper()))
+        """Optimal Solution: String Manipulation. Time Complexity: O(n), Space Complexity: O(1)."""
+        return word == word.upper() or word == word.lower() or word == word.capitalize()
 
 
-# Input = "USA", Output = True
-assert Solution.detectCapitalUse("USA") is True
+def unit_tests():
+    # Input = "USA", Output = True
+    assert Solution.detectCapitalUse("USA") is True
 
-# Input = "FlaG", Output = False
-assert Solution.detectCapitalUse("FlaG") is False
+    # Input = "FlaG", Output = False
+    assert Solution.detectCapitalUse("FlaG") is False
 
-# Input = "leetcode", Output = True
-assert Solution.detectCapitalUse("leetcode") is True
+    # Input = "leetcode", Output = True
+    assert Solution.detectCapitalUse("leetcode") is True
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
