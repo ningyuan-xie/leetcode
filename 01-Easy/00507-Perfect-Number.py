640c76c700edab93068432c4c8b2089a1cdbeb9e
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-07 17:17:38 -0400

    Update 507

--- 01-Easy/00507-Perfect-Number.py
+++ 01-Easy/00507-Perfect-Number.py
@@ -1,45 +1,45 @@
 """507. Perfect Number
 Link: https://leetcode.com/problems/perfect-number/
 Difficulty: Easy
-Description: A perfect number is a positive integer that is equal to the sum of its positive
-divisors, excluding itself. A divisor of an integer x is an integer that can divide x evenly.
+Description: A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
 Given an integer n, return true if n is a perfect number, otherwise return false."""
 
 
 class Solution:
     @staticmethod
     def checkPerfectNumber(num: int) -> bool:
-        """Optimal Solution: Brute Force. Time Complexity: O(sqrt(n)), Space Complexity: O(1).
-           Similar to 0492-Construct-the-Rectangle.py: use square root as an upper bound"""
-        # Return False if n is less than 2
-        if num < 2:
+        """Optimal Solution: Math. Time Complexity: O(sqrt(n)), Space Complexity: O(1)."""
+        # Handle edge cases
+        if num <= 1:
             return False
-        # Initialize a variable to store the sum of divisors: 1st divisor is 1
+        
+        # Initialize sum with 1
         divisor_sum = 1
+
         # Iterate through every number from 2 to the square root of n
-        for i in range(2, int(num ** 0.5) + 1):  # E.g. if n = 28, check 2, 3, 4, 5
-            # If i is a divisor of n
+        for i in range(2, int(num**0.5) + 1):
             if num % i == 0:
-                # Add i to the sum of divisors
+                # Add both divisor and its complement
                 divisor_sum += i
-                # If i is not the square root of n, add n/i to the sum of divisors
-                # E.g. if n = 28, num // i includes 14, 7
-                if i != num // i:
-                    divisor_sum += num // i
+                complement = num // i
+                if complement != i:
+                    divisor_sum += complement
 
         # Return True if the sum of divisors is equal to n
         return divisor_sum == num
 
 
-# Input = 28, Output = True.
-# Explanation: 28 = 1 + 2 + 4 + 7 + 14. 1, 2, 4, 7, and 14 are divisors of 28.
-assert Solution.checkPerfectNumber(28) is True
+def unit_tests():
+    # Input = 28, Output = True
+    assert Solution.checkPerfectNumber(28) is True
 
-# Input = 6, Output = True
-# Explanation: 6 = 1 + 2 + 3. 1, 2, and 3 are divisors of 6.
-assert Solution.checkPerfectNumber(6) is True
+    # Input = 6, Output = True
+    assert Solution.checkPerfectNumber(6) is True
 
-# Input = 7, Output = False
-assert Solution.checkPerfectNumber(7) is False
+    # Input = 7, Output = False
+    assert Solution.checkPerfectNumber(7) is False
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()    
+    print("All unit tests are passed.")
