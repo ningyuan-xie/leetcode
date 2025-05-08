9c8f0461ad40043bb79c783525dafc024f36a1c3
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-08 02:02:06 -0400

    Update 530

--- 01-Easy/00530-Minimum-Absolute-Difference-in-BST.py
+++ 01-Easy/00530-Minimum-Absolute-Difference-in-BST.py
@@ -1,8 +1,7 @@
 """530. Minimum Absolute Difference in BST
 Link: https://leetcode.com/problems/minimum-absolute-difference-in-bst/
 Difficulty: Easy
-Description: Given the root of a Binary Search Tree (BST), return the minimum absolute
-difference between the values of any two different nodes in the tree."""
+Description: Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree."""
 
 from typing import Optional
 from package.data_structures import TreeNode
@@ -11,55 +10,45 @@ from package.data_structures import TreeNode
 class Solution:
     @staticmethod
     def getMinimumDifference(root: Optional[TreeNode]) -> int:
-        """Optimal Solution: Inorder DFS Traversal. Time Complexity: O(n), Space Complexity: O(1).
-           Similar to 0501-Find-Mode-in-Binary-Search-Tree.py"""
-        # Initialize the minimum absolute difference
-        min_absolute_diff = float("inf")  # positive infinity
-        # Initialize the previous node
-        prev_node: Optional[TreeNode] = None
-
-        def inorder_dfs_traversal(node: TreeNode) -> None:
-            """Helper function: Inorder DFS Traversal: left -> root -> right
-               Inorder because for BST, the inorder traversal will be in ascending order,
-               which allows us to find the minimum absolute difference in a single traversal"""
-            # nonlocal variables to access the outer scope immutable variables
-            nonlocal min_absolute_diff, prev_node
-            # Base Case: If the current node is None, do nothing and return
+        """Optimal Solution: Inorder DFS. Time Complexity: O(n), Space Complexity: O(n)."""
+        # Initialize min difference and previous node
+        min_diff = float('inf')
+        prev = None
+        
+        def inorder(node: Optional[TreeNode]) -> None:
+            """Helper function to traverse BST nodes in sorted order, allowing min difference to be found by comparing adjacent nodes."""
+            nonlocal min_diff, prev
             if not node:
                 return
-            # Recursive inorder DFS traversal: left -> root -> right
-            # 1. Recursive Case: Traverse the left subtree
-            inorder_dfs_traversal(node.left)
-            # 2. Root Case: Process the current node
-            if prev_node:
-                # Update the minimum absolute difference
-                min_absolute_diff = min(min_absolute_diff, node.val - prev_node.val)
-            # Update prev_node to node not node.val,
-            # otherwise, "if prev_node:" above will be False when prev_node = 0;
-            # alternatively, we can also use "if prev_node is not None:" to avoid this issue
-            prev_node = node
-            # 3. Recursive Case: Traverse the right subtree
-            inorder_dfs_traversal(node.right)
-
-        # Perform inorder traversal
-        inorder_dfs_traversal(root)
-        return int(min_absolute_diff)
+            
+            inorder(node.left)
+            if prev is not None:
+                min_diff = min(min_diff, node.val - prev.val)
+            prev = node
+            inorder(node.right)
+            
+        inorder(root)
+        return min_diff
 
 
-# Input: root = [4, 2, 6, 1, 3], Output: 1
-root_test = TreeNode.build_binary_tree([4, 2, 6, 1, 3])
-assert Solution.getMinimumDifference(root_test) == 1
+def unit_tests():
+    # Input: root = [4, 2, 6, 1, 3], Output: 1
+    root = TreeNode.build_binary_tree([4, 2, 6, 1, 3])
+    assert Solution.getMinimumDifference(root) == 1
 
-# Input: root = [1, 0, 48, None, None, 12, 49], Output: 1
-root_test = TreeNode.build_binary_tree([1, 0, 48, None, None, 12, 49])
-assert Solution.getMinimumDifference(root_test) == 1
+    # Input: root = [1, 0, 48, None, None, 12, 49], Output: 1
+    root = TreeNode.build_binary_tree([1, 0, 48, None, None, 12, 49])
+    assert Solution.getMinimumDifference(root) == 1
 
-# Input: root = [1, None, 5, 3], Output: 2
-root_test = TreeNode.build_binary_tree([1, None, 5, 3])
-assert Solution.getMinimumDifference(root_test) == 2
+    # Input: root = [1, None, 5, 3], Output: 2
+    root = TreeNode.build_binary_tree([1, None, 5, 3])
+    assert Solution.getMinimumDifference(root) == 2
 
-# Input: root = [0,null,2236,1277,2776,519], Output: 519
-root_test = TreeNode.build_binary_tree([0, None, 2236, 1277, 2776, 519])
-assert Solution.getMinimumDifference(root_test) == 519
+    # Input: root = [0,null,2236,1277,2776,519], Output: 519
+    root = TreeNode.build_binary_tree([0, None, 2236, 1277, 2776, 519])
+    assert Solution.getMinimumDifference(root) == 519
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
