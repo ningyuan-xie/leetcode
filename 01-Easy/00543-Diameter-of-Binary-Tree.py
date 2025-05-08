45714b9ae5decff71641a3364d3269c56e36a490
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-08 13:32:42 -0400

    Update 543

--- 01-Easy/00543-Diameter-of-Binary-Tree.py
+++ 01-Easy/00543-Diameter-of-Binary-Tree.py
@@ -2,8 +2,8 @@
 Link: https://leetcode.com/problems/diameter-of-binary-tree/
 Difficulty: Easy
 Description: Given the root of a binary tree, return the length of the diameter of the tree.
-The diameter of a binary tree is the length of the longest path between any two nodes in a tree.
-This path may or may not pass through the root."""
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+The length of a path between two nodes is represented by the number of edges between them."""
 
 from typing import Optional
 from package.data_structures import TreeNode
@@ -12,44 +12,39 @@ from package.data_structures import TreeNode
 class Solution:
     @staticmethod
     def diameterOfBinaryTree(root: Optional[TreeNode]) -> int:
-        """Optimal Solution: Postorder DFS Traversal. Time Complexity: O(n), Space Complexity: O(1).
-           The trick is while calculating the height, use height to also get the diameter.
-           Similar to 0104-Maximum-Depth-of-Binary-Tree.py"""
-        # Initialize the diameter of the binary tree
-        diameter = 0
+        """Optimal Solution: Postorder DFS. Time Complexity: O(n), Space Complexity: O(n).
+        Similar to 104. Maximum Depth of Binary Tree."""
+        max_diameter = 0
 
-        def postorder_dfs_traversal(node: TreeNode) -> int:
-            """Helper function: Postorder DFS Traversal: left -> right -> root.
-               Postorder because we need to calculate the height of the left and right subtrees"""
-            # nonlocal variables to access the outer scope mutable variables
-            nonlocal diameter
-            # Base Case: If the current node is None, return -1
+        def postorder(node: Optional[TreeNode]) -> int:
+            """Helper function to calculate the depth of each subtree and update the maximum diameter."""
+            nonlocal max_diameter
             if not node:
-                return -1
-            # Recursive postorder DFS traversal: left -> right -> root
-            # 1. Recursive Case: Traverse the left subtree
-            left_height = postorder_dfs_traversal(node.left)
-            # 2. Recursive Case: Traverse the right subtree
-            right_height = postorder_dfs_traversal(node.right)
-            # 3. Root Case: Process the current node and update the diameter of the binary tree
-            # The + 2 accounts for the two edges that connect the node to its left and right subtrees,
-            # as these edges are not included in the heights of the subtrees themselves
-            diameter = max(diameter, left_height + right_height + 2)
-
-            # Return the height of the current node
-            return max(left_height, right_height) + 1
-
-        # Perform postorder traversal
-        postorder_dfs_traversal(root)
-        return diameter
+                return 0
+            
+            left_depth = postorder(node.left)
+            right_depth = postorder(node.right)
+            
+            # Update the maximum diameter found so far
+            max_diameter = max(max_diameter, left_depth + right_depth)
+            
+            # Return the depth of the current node
+            return max(left_depth, right_depth) + 1
+            
+        postorder(root)
+        return max_diameter
 
 
-# Input: root = [1, 2, 3, 4, 5], Output: 3
-root_test = TreeNode.build_binary_tree([1, 2, 3, 4, 5])
-assert Solution.diameterOfBinaryTree(root_test) == 3
+def unit_tests():
+    # Input: root = [1, 2, 3, 4, 5], Output: 3
+    root = TreeNode.build_binary_tree([1, 2, 3, 4, 5])
+    assert Solution.diameterOfBinaryTree(root) == 3
 
-# Input: root = [1, 2], Output: 1
-root_test = TreeNode.build_binary_tree([1, 2])
-assert Solution.diameterOfBinaryTree(root_test) == 1
+    # Input: root = [1, 2], Output: 1
+    root = TreeNode.build_binary_tree([1, 2])
+    assert Solution.diameterOfBinaryTree(root) == 1
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
