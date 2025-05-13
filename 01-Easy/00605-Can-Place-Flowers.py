c3c10c0a89912adbeaabec1e935248d67bf564d1
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-13 00:48:09 -0400

    Update 605

--- 01-Easy/00605-Can-Place-Flowers.py
+++ 01-Easy/00605-Can-Place-Flowers.py
@@ -1,11 +1,8 @@
 """605. Can Place Flowers
 Link: https://leetcode.com/problems/can-place-flowers/
 Difficulty: Easy
-Description: You have a long flowerbed in which some of the plots are planted, and some are not.
-However, flowers cannot be planted in adjacent plots.
-Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not
-empty, and an integer n, return true if n new flowers can be planted in the flowerbed without
-violating the no-adjacent-flowers rule and false otherwise."""
+Description: You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise."""
 
 from typing import List
 
@@ -13,40 +10,39 @@ from typing import List
 class Solution:
     @staticmethod
     def canPlaceFlowers(flowerbed: List[int], n: int) -> bool:
-        """Optimal Solution: Iteration. Time Complexity: O(n), Space Complexity: O(1)."""
-        # Initialize the number of flowers that can be planted
-        flowers_planted = 0
-        # Initialize the length of the flowerbed and the index
-        length, index = len(flowerbed), 0
+        """Optimal Solution: Greedy. Time Complexity: O(n), Space Complexity: O(1)."""
+        count = 0
 
         # Iterate over the flowerbed
-        while index < length:
+        for i in range(len(flowerbed)):
             # If the current plot is empty
-            if flowerbed[index] == 0:
+            if flowerbed[i] == 0:
                 # Check if the previous and next plots are empty or out of bounds
-                prev_plot = flowerbed[index - 1] if index > 0 else 0
-                next_plot = flowerbed[index + 1] if index < length - 1 else 0
-                # If the previous and next plots are empty or out of bounds
-                if prev_plot == 0 and next_plot == 0:
-                    # Plant a flower
-                    flowerbed[index] = 1
-                    # Increment the number of flowers planted
-                    flowers_planted += 1
-            # Move to the next plot
-            index += 1
-        return flowers_planted >= n
+                empty_left = (i == 0) or (flowerbed[i-1] == 0)
+                empty_right = (i == len(flowerbed)-1) or (flowerbed[i+1] == 0)
+                if empty_left and empty_right:
+                    flowerbed[i] = 1
+                    count += 1
+                    # Stop early if we have planted enough flowers
+                    if count >= n:
+                        return True
+        return count >= n
 
 
-# Input: flowerbed = [1,0,0,0,1], n = 1, Output: True
-assert Solution.canPlaceFlowers([1, 0, 0, 0, 1], 1) is True
+def unit_test():
+    # Input: flowerbed = [1,0,0,0,1], n = 1, Output: True
+    assert Solution.canPlaceFlowers([1, 0, 0, 0, 1], 1) is True
 
-# Input: flowerbed = [1,0,0,0,1], n = 2, Output: False
-assert Solution.canPlaceFlowers([1, 0, 0, 0, 1], 2) is False
+    # Input: flowerbed = [1,0,0,0,1], n = 2, Output: False
+    assert Solution.canPlaceFlowers([1, 0, 0, 0, 1], 2) is False
 
-# Input: flowerbed = [0,0,1,0,0], n = 1, Output: True
-assert Solution.canPlaceFlowers([0, 0, 1, 0, 0], 1) is True
+    # Input: flowerbed = [0,0,1,0,0], n = 1, Output: True
+    assert Solution.canPlaceFlowers([0, 0, 1, 0, 0], 1) is True
 
-# Input: flowerbed = [0,0,1,0,0], n = 2, Output: True
-assert Solution.canPlaceFlowers([0, 0, 1, 0, 0], 2) is True
+    # Input: flowerbed = [0,0,1,0,0], n = 2, Output: True
+    assert Solution.canPlaceFlowers([0, 0, 1, 0, 0], 2) is True
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_test()
+    print("All unit tests are passed.")
