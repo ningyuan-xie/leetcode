f85730d8afe29a61d9b9c0465f63aa2d9e559923
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-13 22:54:24 -0400

    Update 637

--- 01-Easy/00637-Average-of-Levels-in-Binary-Tree.py
+++ 01-Easy/00637-Average-of-Levels-in-Binary-Tree.py
@@ -1,8 +1,7 @@
 """637. Average of Levels in Binary Tree
 Link: https://leetcode.com/problems/average-of-levels-in-binary-tree/
 Difficulty: Easy
-Description: Given the root of a binary tree, return the average value of the nodes on each
-level in the form of an array."""
+Description: Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. Answers within 10-5 of the actual answer will be accepted."""
 
 from typing import List
 from package.data_structures import TreeNode
@@ -11,9 +10,7 @@ from package.data_structures import TreeNode
 class Solution:
     @staticmethod
     def averageOfLevels(root: TreeNode) -> List[float]:
-        """Optimal Solution: BFS Traversal. Time Complexity: O(n), Space Complexity: O(n).
-           Similar to the reverse of TreeNode.build_binary_tree() method.
-           [3, 9, 20, None, None, 15, 7] -> pop 3, append 9, 20 -> pop 9 -> pop 20, append 15, 7"""
+        """Optimal Solution: BFS. Time Complexity: O(n), Space Complexity: O(n)."""
         # Initialize the output list, and the queue for BFS traversal
         average_values = []
         queue = [root]
@@ -26,32 +23,36 @@ class Solution:
 
             # Traverse all the nodes in the current level
             for _ in range(current_level_num_nodes):
-                # Pop the first node in the queue
-                current = queue.pop(0)  # FIFO: 1st element is the new parent
-                # Add the node value to the sum
+                # FIFO: 1st element is the new parent
+                current = queue.pop(0)
                 sum_values += current.val
 
                 # Add the left and right children to the queue
                 if current.left:
-                    queue.append(current.left)  # Append the left subtree
+                    queue.append(current.left)
                 if current.right:
-                    queue.append(current.right)  # Append the right subtree
+                    queue.append(current.right)
 
             # Calculate the average value of the current level
             average_values.append(sum_values / current_level_num_nodes)
+            
         return average_values
 
 
-# Input: root = [3, 9, 20, None, None, 15, 7], Output: [3.0, 14.5, 11.0]
-root_test = TreeNode.build_binary_tree([3, 9, 20, None, None, 15, 7])
-assert Solution.averageOfLevels(root_test) == [3.0, 14.5, 11.0]
+def unit_test():
+    # Input: root = [3, 9, 20, None, None, 15, 7], Output: [3.0, 14.5, 11.0]
+    root = TreeNode.build_binary_tree([3, 9, 20, None, None, 15, 7])
+    assert Solution.averageOfLevels(root) == [3.0, 14.5, 11.0]
 
-# Input: root = [3, 9, 20, 15, 7], Output: [3.0, 14.5, 11.0]
-root_test = TreeNode.build_binary_tree([3, 9, 20, 15, 7])
-assert Solution.averageOfLevels(root_test) == [3.0, 14.5, 11.0]
+    # Input: root = [3, 9, 20, 15, 7], Output: [3.0, 14.5, 11.0]
+    root = TreeNode.build_binary_tree([3, 9, 20, 15, 7])
+    assert Solution.averageOfLevels(root) == [3.0, 14.5, 11.0]
 
-# Input: root = [3, 9, 20, 15, 7, 10, 5], Output: [3.0, 14.5, 9.25]
-root_test = TreeNode.build_binary_tree([3, 9, 20, 15, 7, 10, 5])
-assert Solution.averageOfLevels(root_test) == [3.0, 14.5, 9.25]
+    # Input: root = [3, 9, 20, 15, 7, 10, 5], Output: [3.0, 14.5, 9.25]
+    root = TreeNode.build_binary_tree([3, 9, 20, 15, 7, 10, 5])
+    assert Solution.averageOfLevels(root) == [3.0, 14.5, 9.25]
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_test()
+    print("All unit tests are passed.")
