96d3fa92e25007f6ca289bc8fd3dbb2bbed08582
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-14 20:02:15 -0400

    Update 645

--- 01-Easy/00645-Set-Mismatch.py
+++ 01-Easy/00645-Set-Mismatch.py
@@ -1,12 +1,9 @@
 """645. Set Mismatch
 Link: https://leetcode.com/problems/set-mismatch/
 Difficulty: Easy
-Description: You have a set of integers s, which originally contains all the numbers from 1 to n.
-Unfortunately, due to some error, one of the numbers in s got duplicated to another number in
-the set, which results in repetition of one number and loss of another number.
+Description: You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
 You are given an integer array nums representing the data status of this set after the error.
-Find the number that occurs twice and the number that is missing and
-return them in the form of an array."""
+Find the number that occurs twice and the number that is missing and return them in the form of an array."""
 
 from typing import List
 
@@ -14,38 +11,39 @@ from typing import List
 class Solution:
     @staticmethod
     def findErrorNums(nums: List[int]) -> List[int]:
-        """Optimal Solution: Mark the visited number. Time Complexity: O(n), Space Complexity: O(1).
-           Similar to 0448-Find-All-Numbers-Disappeared-in-an-Array.py"""
+        """Optimal Solution: Cyclic Sort. Time Complexity: O(n), Space Complexity: O(1).
+        Similar to 448. Find All Numbers Disappeared in an Array."""
         # Initialize the duplicate and missing numbers
         duplicate, missing = 0, 0
-
-        # Traverse all the numbers in the array to find the duplicate number first
-        for num in nums:
-            # Starts with the positive number
-            num = abs(num)
-            # If the number is already visited, then it is the duplicate number
-            if nums[num - 1] < 0:  # num - 1 because 0-based index and nums should start from 1
-                duplicate = num
-            # Mark the number as visited
-            else:
-                nums[num - 1] *= -1  # num - 1 because 0-based index and nums should start from 1
-
-        # Now that all the visited numbers are negative, the remaining positive is the missing number
-        for i, num in enumerate(nums):
-            if num > 0:
-                # Get missing number from index
+        
+        # Cyclic sort to find the duplicate
+        for i in range(len(nums)):
+            while nums[i] != nums[nums[i] - 1]:
+                # Swap the elements until nums[i] is at its rightful home
+                correct_index = nums[i] - 1
+                nums[i], nums[correct_index] = nums[correct_index], nums[i]
+        
+        # After sorting, find the first number not in its correct position
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                duplicate = nums[i]
                 missing = i + 1
                 break
+                
         return [duplicate, missing]
 
 
-# Input: nums = [1, 2, 2, 4], Output: [2, 3]
-assert Solution.findErrorNums([1, 2, 2, 4]) == [2, 3]
+def unit_test():
+    # Input: nums = [1, 2, 2, 4], Output: [2, 3]
+    assert Solution.findErrorNums([1, 2, 2, 4]) == [2, 3]
 
-# Input: nums = [1, 1], Output: [1, 2]
-assert Solution.findErrorNums([1, 1]) == [1, 2]
+    # Input: nums = [1, 1], Output: [1, 2]
+    assert Solution.findErrorNums([1, 1]) == [1, 2]
 
-# Input: nums = [2, 2], Output: [2, 1]
-assert Solution.findErrorNums([2, 2]) == [2, 1]
+    # Input: nums = [2, 2], Output: [2, 1]
+    assert Solution.findErrorNums([2, 2]) == [2, 1]
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_test()
+    print("All unit tests are passed.")
