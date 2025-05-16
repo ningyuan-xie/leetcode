d3de2cf705f9866f3de036b4d87c0d71d9fe8628
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-16 01:08:05 -0400

    Update 657

--- 01-Easy/00657-Robot-Return-to-Origin.py
+++ 01-Easy/00657-Robot-Return-to-Origin.py
@@ -1,40 +1,40 @@
 """657. Robot Return to Origin
 Link: https://leetcode.com/problems/robot-return-to-origin/
 Difficulty: Easy
-Description: There is a robot starting at the position (0, 0), the origin, on a 2D plane.
-Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves."""
+Description: There is a robot starting at the position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
+You are given a string moves that represents the move sequence of the robot where moves[i] represents its ith move. Valid moves are 'R' (right), 'L' (left), 'U' (up), and 'D' (down).
+Return true if the robot returns to the origin after it finishes all of its moves, or false otherwise.
+Note: The way that the robot is "facing" is irrelevant. 'R' will always make the robot move to the right once, 'L' will always make it move left, etc. Also, assume that the magnitude of the robot's movement is the same for each move."""
 
 
 class Solution:
     @staticmethod
     def judgeCircle(moves: str) -> bool:
-        """Optimal Solution: Count the number of moves.
-           Time Complexity: O(n), Space Complexity: O(1)."""
-        # Initialize the counts of the four moves
-        up = down = left = right = 0
-
-        # Count the number of 'U', 'D', 'L', and 'R' moves
+        """Optimal Solution: Linear Scan. Time Complexity: O(n), Space Complexity: O(1)."""
+        x = y = 0
         for move in moves:
             if move == 'U':
-                up += 1
+                y += 1
             elif move == 'D':
-                down += 1
+                y -= 1
             elif move == 'L':
-                left += 1
+                x -= 1
             elif move == 'R':
-                right += 1
-
-        # Check if the robot returns to the origin
-        return up == down and left == right
+                x += 1
+        return x == 0 and y == 0
 
 
-# Input: moves = "UD", Output: True
-assert Solution.judgeCircle("UD") is True
+def unit_test():
+    # Input: moves = "UD", Output: True
+    assert Solution.judgeCircle("UD") is True
 
-# Input: moves = "LL", Output: False
-assert Solution.judgeCircle("LL") is False
+    # Input: moves = "LL", Output: False
+    assert Solution.judgeCircle("LL") is False
 
-# Input: moves = "UDLR", Output: True
-assert Solution.judgeCircle("UDLR") is True
+    # Input: moves = "UDLR", Output: True
+    assert Solution.judgeCircle("UDLR") is True
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_test()
+    print("All unit tests are passed.")
