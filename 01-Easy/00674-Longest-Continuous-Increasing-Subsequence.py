1fb3a164842f3e509b8022b4fa3c885af7dc6266
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-17 00:30:53 -0400

    Update 674

--- 01-Easy/00674-Longest-Continuous-Increasing-Subsequence.py
+++ 01-Easy/00674-Longest-Continuous-Increasing-Subsequence.py
@@ -1,8 +1,8 @@
 """671. Longest Continuous Increasing Subsequence
 Link: https://leetcode.com/problems/longest-continuous-increasing-subsequence/
 Difficulty: Easy
-Description: Given an unsorted array of integers nums, return the length of the longest
-continuous (i.e. subarray). The subarray must be strictly increasing."""
+Description: Given an unsorted array of integers nums, return the length of the longest continuous increasing subsequence (i.e. subarray). The subsequence must be strictly increasing.
+A continuous increasing subsequence is defined by two indices l and r (l < r) such that it is [nums[l], nums[l + 1], ..., nums[r - 1], nums[r]] and for each l <= i < r, nums[i] < nums[i + 1]."""
 
 from typing import List
 
@@ -10,28 +10,28 @@ from typing import List
 class Solution:
     @staticmethod
     def findLengthOfLCIS(nums: List[int]) -> int:
-        """Optimal Solution: Sliding Window. Time Complexity: O(n), Space Complexity: O(1)."""
-        # Base case: If the input array is empty
-        if not nums:
-            return 0
-
-        # Initialize the variables
-        n = len(nums)
-        start, max_length = 0, 1
-
-        for end in range(1, n):
-            # Check if the current number is greater than the previous number
-            if nums[end] > nums[end - 1]:  # Strictly increasing and continuous
-                max_length = max(max_length, end - start + 1)
+        """Optimal Solution: Linear Scan. Time Complexity: O(n), Space Complexity: O(1)."""
+        # Initialize the maximum length and current length
+        max_len = curr_len = 1
+        
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                curr_len += 1
+                max_len = max(max_len, curr_len)
             else:
-                start = end  # Reset the start index
-        return max_length
+                curr_len = 1
+                
+        return max_len
 
 
-# Input: nums = [1, 3, 5, 4, 7], Output: 3
-assert Solution.findLengthOfLCIS([1, 3, 5, 4, 7]) == 3
+def unit_test():
+    # Input: nums = [1, 3, 5, 4, 7], Output: 3
+    assert Solution.findLengthOfLCIS([1, 3, 5, 4, 7]) == 3
 
-# Input: nums = [2, 2, 2, 2, 2], Output: 1
-assert Solution.findLengthOfLCIS([2, 2, 2, 2, 2]) == 1
+    # Input: nums = [2, 2, 2, 2, 2], Output: 1
+    assert Solution.findLengthOfLCIS([2, 2, 2, 2, 2]) == 1
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_test()
+    print("All unit tests are passed.")
