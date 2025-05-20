b6530a4df4627caefa7e1da75a10462fbd43f936
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-20 00:05:08 -0400

    Update 700

--- 01-easy/q00700_search_in_a_binary_search_tree.py
+++ 01-easy/q00700_search_in_a_binary_search_tree.py
@@ -2,35 +2,39 @@
 Link: https://leetcode.com/problems/search-in-a-binary-search-tree/
 Difficulty: Easy
 Description: You are given the root of a binary search tree (BST) and an integer val.
-Find the node in the BST that the node's value equals val and return the subtree rooted with that node.
-If such a node does not exist, return null."""
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null."""
 
+from typing import Optional
 from package.data_structures import TreeNode
 
 
 class Solution:
     @staticmethod
-    def searchBST(root: TreeNode, val: int) -> TreeNode:
-        """Optimal Solution: Preorder DFS Traversal. Time Complexity: O(h), Space Complexity: O(h),
-           where h is the height of the binary search tree: log(n) <= h <= n."""
-        # Base Case: If the root is None or the value is found
-        if not root or root.val == val:
+    def searchBST(root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        """Optimal Solution: Preorder DFS. Time Complexity: O(n), Space Complexity: O(n)."""
+        if not root:
+            return None
+        if root.val == val:
             return root
-        # Recursive Case: If the value is less than the root's value
+        # BST Property: left < root < right
         if val < root.val:
             return Solution.searchBST(root.left, val)
-        # Recursive Case: If the value is greater than the root's value
-        return Solution.searchBST(root.right, val)
+        else:
+            return Solution.searchBST(root.right, val)
 
 
-# Input: root = [4, 2, 7, 1, 3], val = 2, Output: [2, 1, 3]
-root_test = TreeNode.build_binary_tree([4, 2, 7, 1, 3])
-root_expected = TreeNode.build_binary_tree([2, 1, 3])
-assert Solution.searchBST(root_test, 2) == root_expected
+def unit_tests():
+    # Input: root = [4, 2, 7, 1, 3], val = 2, Output: [2, 1, 3]
+    root = TreeNode.build_binary_tree([4, 2, 7, 1, 3])
+    output = TreeNode.build_binary_tree([2, 1, 3])
+    assert Solution.searchBST(root, 2) == output
 
-# Input: root = [4, 2, 7, 1, 3], val = 5, Output: None
-root_test = TreeNode.build_binary_tree([4, 2, 7, 1, 3])
-root_expected = TreeNode.build_binary_tree([])
-assert Solution.searchBST(root_test, 5) == root_expected
+    # Input: root = [4, 2, 7, 1, 3], val = 5, Output: None
+    root = TreeNode.build_binary_tree([4, 2, 7, 1, 3])
+    output = TreeNode.build_binary_tree([])
+    assert Solution.searchBST(root, 5) == output
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
