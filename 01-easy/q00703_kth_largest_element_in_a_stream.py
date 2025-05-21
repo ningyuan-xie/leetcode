ab900ca8c0584a4e2224f9f7200adde61c96630d
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-21 01:06:41 -0400

    Update 703

--- 01-easy/q00703_kth_largest_element_in_a_stream.py
+++ 01-easy/q00703_kth_largest_element_in_a_stream.py
@@ -1,8 +1,11 @@
 """703. Kth Largest Element in a Stream
 Link: https://leetcode.com/problems/kth-largest-element-in-a-stream/
 Difficulty: Easy
-Description: Design a class to find the kth largest element in a stream. Note that it is the
-kth largest element in the sorted order, not the kth distinct element."""
+Description: You are part of a university admissions office and need to keep track of the kth highest test score from applicants in real-time. This helps to determine cut-off marks for interviews and admissions dynamically as new applicants submit their scores.
+You are tasked to implement a class which, for a given integer k, maintains a stream of test scores and continuously returns the kth highest test score after a new score has been submitted. More specifically, we are looking for the kth highest score in the sorted list of all scores.
+Implement the KthLargest class:
+• KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of test scores nums.
+• int add(int val) Adds a new test score val to the stream and returns the element representing the kth largest element in the pool of test scores so far."""
 
 import heapq  # Priority Queue (Heap) Module
 
@@ -11,8 +14,7 @@ class KthLargest:
     """Optimal Solution: Min Heap. Time Complexity: O(nlog(k)), Space Complexity: O(k)."""
 
     def __init__(self, k: int, nums: list[int]):
-        """Constructor: Initialize the instance variables. Time Complexity: O(nlog(k))."""
-        # Initialize the two variables
+        """Constructor: create a min heap of size k."""
         self.k = k
         self.min_heap = []
 
@@ -21,15 +23,11 @@ class KthLargest:
             self.add(num)
 
     def add(self, val: int) -> int:
-        """Add the number to the min heap and return the kth largest element through
-           heappush and heappop operations. Time Complexity: O(log(k))."""
-        # heappush (insert): add the new element to the end of the list, and then "bubbles up" this
-        # element to its correct position to maintain the min heap property
+        """Add the number to the min heap and return the kth largest element through heappush and heappop operations."""
+        # heappush (insert): add the new element to the end of the list, and then "bubbles up" this element to its correct position to maintain the min heap property
         heapq.heappush(self.min_heap, val)
 
-        # If the size of the min heap exceeds k, remove the smallest element:
-        # heappop (extractMin): remove the smallest element from the root, move the last value to the
-        # root, and "heapify" it (exchange with lesser child) down to maintain the min heap property
+        # heappop (extractMin): remove the smallest element from the root, move the last value to the root, and "heapify" it (exchange with lesser child) down to maintain the min heap property
         if len(self.min_heap) > self.k:
             heapq.heappop(self.min_heap)
 
@@ -37,21 +35,23 @@ class KthLargest:
         return self.min_heap[0]
 
 
-# Input: k = 3, nums = [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]],
-# Output: [null, 4, 5, 5, 8, 8]
-kth_largest = KthLargest(3, [4, 5, 8, 2])  # [4, 5, 8, 2] -> [2, 4, 8, 5] -> [4, 5, 8]
-assert kth_largest.add(3) == 4  # [4, 5, 8, 3] -> [3, 4, 8, 5] -> [4, 5, 8], returns 4
-assert kth_largest.add(5) == 5  # [4, 5, 8, 5] -> [5, 5, 8], returns 5
-assert kth_largest.add(10) == 5  # [5, 5, 8, 10] -> [5, 10, 8], returns 5
-assert kth_largest.add(9) == 8  # [5, 10, 8, 9] -> [5, 9, 8, 10] -> [8, 9, 10], returns 8
-assert kth_largest.add(4) == 8  # [8, 9, 10, 4] -> [4, 8, 10, 9] -> [8, 9, 10], returns 8
-# Since k = 3, the heap always contain the 3 largest elements: [8, 9, 10]
+def unit_tests():
+    # Input: ["KthLargest", "add", "add", "add", "add", "add"] [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]], Output: [null, 4, 5, 5, 8, 8]
+    kth_largest = KthLargest(3, [4, 5, 8, 2])
+    assert kth_largest.add(3) == 4
+    assert kth_largest.add(5) == 5
+    assert kth_largest.add(10) == 5
+    assert kth_largest.add(9) == 8
+    assert kth_largest.add(4) == 8
 
-# Input: k = 1, nums = [[1, []], [-3], [-2], [-4]], Output: [-3, -2, -2]
-kth_largest = KthLargest(1, [])  # []
-assert kth_largest.add(-3) == -3  # [-3], returns -3
-assert kth_largest.add(-2) == -2  # [-3, -2] -> [-2], returns -2
-assert kth_largest.add(-4) == -2  # [-2, -4] -> [-4, -2] -> [-2], returns -2
-# Since k = 1, the heap always contain the 1 largest element: [-2]
+    # Input: ["KthLargest", "add", "add", "add", "add"] [[4, [7, 7, 7, 7, 8, 3]], [2], [10], [9], [9]], Output: [null, 7, 7, 7, 8]
+    kth_largest = KthLargest(4, [7, 7, 7, 7, 8, 3])
+    assert kth_largest.add(2) == 7
+    assert kth_largest.add(10) == 7
+    assert kth_largest.add(9) == 7
+    assert kth_largest.add(9) == 8
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
