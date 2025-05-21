d60af1deb96800d60e1b277ac51b8e54a36e066a
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-21 00:39:42 -0400

    Update 704

--- 01-easy/q00704_binary_search.py
+++ 01-easy/q00704_binary_search.py
@@ -1,8 +1,8 @@
 """704. Binary Search
 Link: https://leetcode.com/problems/binary-search/
 Difficulty: Easy
-Description: Given an array of integers nums which is sorted in ascending order, and an integer
-target, write a function to search target in nums."""
+Description: Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+You must write an algorithm with O(log n) runtime complexity."""
 
 from typing import List
 
@@ -11,35 +11,32 @@ class Solution:
     @staticmethod
     def search(nums: List[int], target: int) -> int:
         """Optimal Solution: Binary Search. Time Complexity: O(log(n)), Space Complexity: O(1)."""
-        # Initialize the two pointers
         left, right = 0, len(nums) - 1
-
-        # Iterate through the list of numbers
+        
         while left <= right:
-            # Calculate the middle index
-            mid = left + (right - left) // 2
-
-            # If the target is found, return the index
+            mid = (left + right) // 2
+            
             if nums[mid] == target:
                 return mid
-            # If the middle element is less than the target, move the right pointer
             elif nums[mid] < target:
                 left = mid + 1
-            # If the middle element is greater than the target, move the left pointer
             else:
                 right = mid - 1
-
-        # If the target is not found, return -1
+                
         return -1
 
 
-# Input: nums = [-1, 0, 3, 5, 9, 12], target = 9, Output: 4
-assert Solution.search([-1, 0, 3, 5, 9, 12], 9) == 4
+def unit_tests():
+    # Input: nums = [-1, 0, 3, 5, 9, 12], target = 9, Output: 4
+    assert Solution.search([-1, 0, 3, 5, 9, 12], 9) == 4
 
-# Input: nums = [-1, 0, 3, 5, 9, 12], target = 2, Output: -1
-assert Solution.search([-1, 0, 3, 5, 9, 12], 2) == -1
+    # Input: nums = [-1, 0, 3, 5, 9, 12], target = 2, Output: -1
+    assert Solution.search([-1, 0, 3, 5, 9, 12], 2) == -1
 
-# Input: nums = [5], target = 5, Output: 0
-assert Solution.search([5], 5) == 0
+    # Input: nums = [5], target = 5, Output: 0
+    assert Solution.search([5], 5) == 0
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
