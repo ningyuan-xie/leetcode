0877620c60cb2bf6552bcb0871a55035cf09ce16
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-25 01:13:09 -0400

    Update 744

--- 01-easy/q00744_find_smallest_letter_greater_than_target.py
+++ 01-easy/q00744_find_smallest_letter_greater_than_target.py
@@ -1,10 +1,8 @@
 """733. Find Smallest Letter Greater Than Target
 Link: https://leetcode.com/problems/find-smallest-letter-greater-than-target/
 Difficulty: Easy
-Description: You are given an array of characters letters that is sorted in non-decreasing order,
-and a character target. There are at least two different characters in letters.
-Return the smallest character in letters that is lexicographically greater than target.
-If such a character does not exist, return the first character in letters."""
+Description: You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters."""
 
 from typing import List
 
@@ -13,44 +11,40 @@ class Solution:
     @staticmethod
     def next_greatest_letter(letters: List[str], target: str) -> str:
         """Optimal Solution: Binary Search. Time Complexity: O(log(n)), Space Complexity: O(1)."""
-        # Initialize the left and right pointers
         left, right = 0, len(letters) - 1
 
-        # Check if the target is greater than the last letter
+        # If the target is greater than the last letter, return the first letter
         if target >= letters[right]:
-            # If so, return the first letter
             return letters[0]
 
-        # Perform binary search
         while left <= right:
-            # Calculate the middle index
-            mid = left + (right - left) // 2
-
-            # Check if the middle letter is greater than the target
-            if letters[mid] <= target:
-                # If so, move the left pointer to the right
-                left = mid + 1
-            # Otherwise, move the right pointer to the left
-            else:
+            mid = (left + right) // 2
+            
+            if letters[mid] > target:
                 right = mid - 1
-
-        # Return the smallest character that is larger than the target
+            else:
+                left = mid + 1
+                
         return letters[left]
 
 
-# Input: letters = ["c", "f", "j"], target = "a", Output: "c"
-assert Solution.next_greatest_letter(["c", "f", "j"], "a") == "c"
+def unit_tests():
+    # Input: letters = ["c", "f", "j"], target = "a", Output: "c"
+    assert Solution.next_greatest_letter(["c", "f", "j"], "a") == "c"
 
-# Input: letters = ["c", "f", "j"], target = "c", Output: "f"
-assert Solution.next_greatest_letter(["c", "f", "j"], "c") == "f"
+    # Input: letters = ["c", "f", "j"], target = "c", Output: "f"
+    assert Solution.next_greatest_letter(["c", "f", "j"], "c") == "f"
 
-# Input: letters = ["c", "f", "j"], target = "d", Output: "f"
-assert Solution.next_greatest_letter(["c", "f", "j"], "d") == "f"
+    # Input: letters = ["c", "f", "j"], target = "d", Output: "f"
+    assert Solution.next_greatest_letter(["c", "f", "j"], "d") == "f"
 
-# Input: letters = ["c", "f", "j"], target = "g", Output: "j"
-assert Solution.next_greatest_letter(["c", "f", "j"], "g") == "j"
+    # Input: letters = ["c", "f", "j"], target = "g", Output: "j"
+    assert Solution.next_greatest_letter(["c", "f", "j"], "g") == "j"
 
-# Input: letters = ["c", "f", "j"], target = "k", Output: "c"
-assert Solution.next_greatest_letter(["c", "f", "j"], "k") == "c"
+    # Input: letters = ["c", "f", "j"], target = "k", Output: "c"
+    assert Solution.next_greatest_letter(["c", "f", "j"], "k") == "c"
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
