c0bc3078110236073050c7dc9870c96445f5fb3f
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-26 00:48:47 -0400

    Update 747

--- 01-easy/q00747_largest_number_at_least_twice_of_others.py
+++ 01-easy/q00747_largest_number_at_least_twice_of_others.py
@@ -2,8 +2,7 @@
 Link: https://leetcode.com/problems/largest-number-at-least-twice-of-others
 Difficulty: Easy
 Description: You are given an integer array nums where the largest integer is unique.
-Determine whether the largest element in the array is at least twice as much as every other
-number in the array. If it is, return the index of the largest element, or return -1 otherwise."""
+Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise."""
 
 from typing import List
 
@@ -11,36 +10,26 @@ from typing import List
 class Solution:
     @staticmethod
     def dominant_index(nums: List[int]) -> int:
-        """Optimal Solution: Two Passes. Time Complexity: O(n), Space Complexity: O(1)."""
-        # Initialization: Assume the largest number is the first number
-        largest_index, largest_num = 0, nums[0]
-
-        # Pass 1: Find the largest number
-        for i in range(1, len(nums)):
-            # Check if the current number is greater than the largest number
-            if nums[i] > largest_num:
-                # If so, update the largest number and its index
-                largest_index, largest_num = i, nums[i]
-
-        # Pass 2: Check if the largest number is at least twice as much as every other number
-        for i in range(len(nums)):
-            # Skip the largest number
-            if i != largest_index:
-                # If any number is greater than half of the largest number, return -1
-                if nums[i] * 2 > largest_num:
-                    return -1
-
+        """Optimal Solution: Linear Scan. Time Complexity: O(n), Space Complexity: O(1)."""
+        largest = max(nums)
+        for num in nums:
+            if num != largest and num * 2 > largest:
+                return -1
         # Return the index of the largest number
-        return largest_index
+        return nums.index(largest)
 
 
-# Input: nums = [3, 6, 1, 0], Output: 1
-assert Solution.dominant_index([3, 6, 1, 0]) == 1
+def unit_tests():
+    # Input: nums = [3, 6, 1, 0], Output: 1
+    assert Solution.dominant_index([3, 6, 1, 0]) == 1
 
-# Input: nums = [1, 2, 3, 4], Output: -1
-assert Solution.dominant_index([1, 2, 3, 4]) == -1
+    # Input: nums = [1, 2, 3, 4], Output: -1
+    assert Solution.dominant_index([1, 2, 3, 4]) == -1
 
-# Input: nums = [1], Output: 0
-assert Solution.dominant_index([1]) == 0
+    # Input: nums = [1], Output: 0
+    assert Solution.dominant_index([1]) == 0
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
