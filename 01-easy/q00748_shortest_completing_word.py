06d36a100b2a0e8bdd9603cec8504c1c8c9c8e7f
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-27 01:47:08 -0400

    Update 748

--- 01-easy/q00748_shortest_completing_word.py
+++ 01-easy/q00748_shortest_completing_word.py
@@ -1,15 +1,10 @@
 """748. Shortest Completing Word
 Link: https://leetcode.com/problems/shortest-completing-word/
 Difficulty: Easy
-Description: Given a string licensePlate and an array of strings words, find the shortest completing
-word in words.
-A completing word is a word that contains all the letters in licensePlate. Ignore numbers and
-spaces in licensePlate, and treat letters as case insensitive. If a letter appears more than once in
-licensePlate, then it must appear in the word the same number of times or more.
-For example, if licensePlate = "aBc 12c", then it contains letters 'a', 'b' (ignoring case),
-and 'c' twice. Possible completing words are "abccdef", "caaacab", and "cbca".
-Return the shortest completing word in words. It is guaranteed an answer exists.
-If there are multiple shortest completing words, return the first one that occurs in words."""
+Description: Given a string licensePlate and an array of strings words, find the shortest completing word in words.
+A completing word is a word that contains all the letters in licensePlate. Ignore numbers and spaces in licensePlate, and treat letters as case insensitive. If a letter appears more than once in licensePlate, then it must appear in the word the same number of times or more.
+For example, if licensePlate = "aBc 12c", then it contains letters 'a', 'b' (ignoring case), and 'c' twice. Possible completing words are "abccdef", "caaacab", and "cbca".
+Return the shortest completing word in words. It is guaranteed an answer exists. If there are multiple shortest completing words, return the first one that occurs in words."""
 
 from typing import List
 
@@ -17,9 +12,7 @@ from typing import List
 class Solution:
     @staticmethod
     def shortest_completing_word(licensePlate: str, words: List[str]) -> str:
-        """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(1).
-           n is the number of words in the list;
-           length of licensePlate and words are considered constant"""
+        """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(1)."""
         # Extract the letters and convert to lowercase
         license_letters = [char.lower() for char in licensePlate if char.isalpha()]
 
@@ -46,21 +39,17 @@ class Solution:
         return shortest_word
 
 
-# Input: license_plate = "1s3 PSt", words = ["step", "steps", "stripe", "stepple"],
-# Output: "steps"
-assert Solution.shortest_completing_word("1s3 PSt",
-                                         ["step", "steps", "stripe", "stepple"]) == "steps"
+def unit_tests():
+    # Input: license_plate = "1s3 PSt", words = ["step", "steps", "stripe", "stepple"], Output: "steps"
+    assert Solution.shortest_completing_word("1s3 PSt", ["step", "steps", "stripe", "stepple"]) == "steps"
 
-# Input: license_plate = "1s3 456", words = ["looks", "pest", "stew", "show"],
-# Output: "pest"
-assert Solution.shortest_completing_word("1s3 456",
-                                         ["looks", "pest", "stew", "show"]) == "pest"
+    # Input: license_plate = "1s3 456", words = ["looks", "pest", "stew", "show"], Output: "pest"
+    assert Solution.shortest_completing_word("1s3 456", ["looks", "pest", "stew", "show"]) == "pest"
 
-# Input: license_plate = "Ah71752", words = ["suggest", "letter", "of", "husband", "easy",
-# "education", "drug", "prevent", "writer", "old"], Output: "husband"
-assert (Solution.shortest_completing_word("Ah71752",
-                                          ["suggest", "letter", "of", "husband", "easy",
-                                           "education", "drug", "prevent", "writer", "old"])
-        == "husband")
+    # Input: license_plate = "Ah71752", words = ["suggest", "letter", "of", "husband", "easy", "education", "drug", "prevent", "writer", "old"], Output: "husband"
+    assert Solution.shortest_completing_word("Ah71752", ["suggest", "letter", "of", "husband", "easy", "education", "drug", "prevent", "writer", "old"]) == "husband"
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
