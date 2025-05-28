ede3bd07c792c4dc5641c89985391f5505138832
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-28 00:12:37 -0400

    Update 771

--- 01-easy/q00771_jewels_and_stones.py
+++ 01-easy/q00771_jewels_and_stones.py
@@ -1,17 +1,14 @@
 """771. Jewels and Stones
 Link: https://leetcode.com/problems/jewels-and-stones/
 Difficulty: Easy
-Description: You're given strings jewels representing the types of stones that are jewels, and stones
-representing the stones you have. Each character in stones is a type of stone you have. You want to
-know how many of the stones you have are also jewels.
-Letters are case-sensitive, so "a" is considered a different type of stone from "A"."""
+Description: You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+Letters are case sensitive, so "a" is considered a different type of stone from "A"."""
 
 
 class Solution:
     @staticmethod
     def num_jewels_in_stones(jewels: str, stones: str) -> int:
-        """Optimal Solution: Set. Time Complexity: O(n), Space Complexity: O(1).
-           Similar to 0762-Prime-Number-of-Set-Bits-in-Binary-Representation.py"""
+        """Optimal Solution: Set. Time Complexity: O(n), Space Complexity: O(1)."""
         # Initialize the count of jewels in stones
         count = 0
 
@@ -26,10 +23,14 @@ class Solution:
         return count
 
 
-# Input: jewels = "aA", stones = "aAAbbbb", Output: 3
-assert Solution.num_jewels_in_stones("aA", "aAAbbbb") == 3
+def unit_tests():
+    # Input: jewels = "aA", stones = "aAAbbbb", Output: 3
+    assert Solution.num_jewels_in_stones("aA", "aAAbbbb") == 3
 
-# Input: jewels = "z", stones = "ZZ", Output: 0
-assert Solution.num_jewels_in_stones("z", "ZZ") == 0
+    # Input: jewels = "z", stones = "ZZ", Output: 0
+    assert Solution.num_jewels_in_stones("z", "ZZ") == 0
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
