be8fdec01bd1b40fa611c994b480ea3bf7332d53
Ningyuan Xie <howard.xie25@gmail.com>
2025-05-29 21:38:11 -0400

    Update 804

--- 01-easy/q00804_unique_morse_code_words.py
+++ 01-easy/q00804_unique_morse_code_words.py
@@ -1,9 +1,15 @@
 """804. Unique Morse Code Words
 Link: https://leetcode.com/problems/unique-morse-code-words/
 Difficulty: Easy
-Description: International Morse Code defines a standard encoding where each letter is mapped
-to a series of dots and dashes. Return the number of different transformations among
-all words we have."""
+Description: International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+• 'a' maps to ".-",
+• 'b' maps to "-...",
+• 'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+• For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+Return the number of different transformations among all words we have."""
 
 from typing import List
 
@@ -11,36 +17,34 @@ from typing import List
 class Solution:
     @staticmethod
     def uniqueMorseRepresentations(words: List[str]) -> int:
-        """Optimal Solution: Set. Time Complexity: O(n), Space Complexity: O(1)."""
-        # Morse code
-        morse_codes = [".-", "-...", "-.-.", "-..", ".",  # a, b, c, d, e
-                       "..-.", "--.", "....", "..", ".---",  # f, g, h, i, j
-                       "-.-", ".-..", "--", "-.", "---",  # k, l, m, n, o
-                       ".--.", "--.-", ".-.", "...", "-",  # p, q, r, s, t
-                       "..-", "...-", ".--", "-..-", "-.--", "--.."]  # u, v, w, x, y, z
-
+        """Optimal Solution: Set. Time Complexity: O(n), Space Complexity: O(n)."""
+        morse_codes = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        
         # Use a set to store unique Morse code transformations
-        unique_transformations: set[str] = set()
-
+        seen = set()
         for word in words:
-            # ord(char) - ord('a') is the index of the Morse code for the character
-            # ''.join() joins a generator object of strings into a string without separator
-            transformation = ''.join(morse_codes[ord(char) - ord('a')] for char in word)
-            unique_transformations.add(transformation)
+            code = []
+            for c in word:
+                code.append(morse_codes[ord(c) - ord('a')])
+            seen.add(''.join(code))
+            
+        return len(seen)
 
-        return len(unique_transformations)
+
+def unit_tests():
+    # Input: words = ["gin", "zen", "gig", "msg"], Output: 2
+    assert Solution.uniqueMorseRepresentations(["gin", "zen", "gig", "msg"]) == 2
+
+    # Input: words = ["a"], Output: 1
+    assert Solution.uniqueMorseRepresentations(["a"]) == 1
+
+    # Input: words = ["b"], Output: 1
+    assert Solution.uniqueMorseRepresentations(["b"]) == 1
+
+    # Input: words = ["a", "b"], Output: 2
+    assert Solution.uniqueMorseRepresentations(["a", "b"]) == 2
 
 
-# Input: words = ["gin", "zen", "gig", "msg"], Output: 2
-assert Solution.uniqueMorseRepresentations(["gin", "zen", "gig", "msg"]) == 2
-
-# Input: words = ["a"], Output: 1
-assert Solution.uniqueMorseRepresentations(["a"]) == 1
-
-# Input: words = ["b"], Output: 1
-assert Solution.uniqueMorseRepresentations(["b"]) == 1
-
-# Input: words = ["a", "b"], Output: 2
-assert Solution.uniqueMorseRepresentations(["a", "b"]) == 2
-
-print("All unit tests are passed.")
+if __name__ == '__main__':
+    unit_tests()
+    print("All unit tests are passed.")
