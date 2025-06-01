f7bbd415c086f6c5c4148c01f70b96b5428f5a91
Ningyuan Xie <howard.xie25@gmail.com>
2025-06-01 00:14:38 -0400

    Update 821

--- 01-easy/q00821_shortest_distance_to_a_character.py
+++ 01-easy/q00821_shortest_distance_to_a_character.py
@@ -1,9 +1,8 @@
 """821. Shortest Distance to a Character
 Link: https://leetcode.com/problems/shortest-distance-to-a-character/
 Difficulty: Easy
-Description: Given a string s and a character c that occurs in s, return an array of integers
-answer where answer.length == s.length and answer[i] is the shortest distance from s[i] to
-the character c in s."""
+Description: Given a string s and a character c that occurs in s, return an array of integers answer where answer.length == s.length and answer[i] is the distance from index i to the closest occurrence of character c in s.
+The distance between two indices i and j is abs(i - j), where abs is the absolute value function."""
 
 from typing import List
 
@@ -12,38 +11,37 @@ class Solution:
     @staticmethod
     def shortestToChar(s: str, c: str) -> List[int]:
         """Optimal Solution: Two Passes. Time Complexity: O(n), Space Complexity: O(n)."""
-        # Initialize the result array
         n = len(s)
         result = [0] * n
-
-        # Initialize the last occurrence of the character as negative infinity
-        last_occurrence_index = float('-inf')
-        # Pass 1: Forward pass: Calculate the shortest distance to the left
+        
+        # Forward pass: left to right
+        prev = -float('inf')
         for i in range(n):
             if s[i] == c:
-                last_occurrence_index = i
-            result[i] = i - last_occurrence_index
-        # E.g. s = "loveleetcode", c = "e" -> [inf, inf, inf, 0, 1, 0, 0, 1, 2, 3, 4, 0]
-
-        # Initialize the last occurrence of the character as positive infinity
-        last_occurrence_index = float('inf')
-        # Pass 2: Backward pass: Calculate the shortest distance to the right
-        for i in range(n - 1, -1, -1):
+                prev = i
+            result[i] = i - prev
+        
+        # Backward pass: right to left
+        prev = float('inf')
+        for i in range(n-1, -1, -1):
             if s[i] == c:
-                last_occurrence_index = i
-            result[i] = min(result[i], last_occurrence_index - i)
-        # E.g. s = "loveleetcode", c = "e" -> [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
-
+                prev = i
+            result[i] = min(result[i], prev - i)
+        
         return result
 
 
-# Input: s = "loveleetcode", c = "e", Output: [3,2,1,0,1,0,0,1,2,2,1,0]
-assert Solution.shortestToChar("loveleetcode", "e") == [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+def unit_tests():
+    # Input: s = "loveleetcode", c = "e", Output: [3,2,1,0,1,0,0,1,2,2,1,0]
+    assert Solution.shortestToChar("loveleetcode", "e") == [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
 
-# Input: s = "aaab", c = "b", Output: [3,2,1,0]
-assert Solution.shortestToChar("aaab", "b") == [3, 2, 1, 0]
+    # Input: s = "aaab", c = "b", Output: [3,2,1,0]
+    assert Solution.shortestToChar("aaab", "b") == [3, 2, 1, 0]
 
-# Input: s = "aaba", c = "b", Output: [2,1,0,1]
-assert Solution.shortestToChar("aaba", "b") == [2, 1, 0, 1]
+    # Input: s = "aaba", c = "b", Output: [2,1,0,1]
+    assert Solution.shortestToChar("aaba", "b") == [2, 1, 0, 1]
 
-print("All unit tests are passed.")
+
+if __name__ == '__main__':
+    unit_tests()
+    print("All unit tests are passed.")
