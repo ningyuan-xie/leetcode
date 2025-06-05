e420595d02764b08268a0449c7c353275480a7b9
Ningyuan Xie <howard.xie25@gmail.com>
2025-06-05 00:54:28 -0400

    Update 867

--- 01-easy/q00867_transpose_matrix.py
+++ 01-easy/q00867_transpose_matrix.py
@@ -2,8 +2,7 @@
 Link: https://leetcode.com/problems/transpose-matrix/
 Difficulty: Easy
 Description: Given a 2D integer array matrix, return the transpose of matrix.
-The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row
-and column indices."""
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices."""
 
 from typing import List
 
@@ -11,7 +10,7 @@ from typing import List
 class Solution:
     @staticmethod
     def transpose(matrix: List[List[int]]) -> List[List[int]]:
-        """Optimal Solution: Brute Force. Time Complexity: O(n*m), Space Complexity: O(n*m)."""
+        """Optimal Solution: Brute Force. Time Complexity: O(n * m), Space Complexity: O(n * m)."""
         # Get the number of rows and columns of the matrix
         rows, cols = len(matrix), len(matrix[0])
 
@@ -26,10 +25,14 @@ class Solution:
         return transposed
 
 
-# Input: [[1, 2, 3], [4, 5, 6], [7, 8, 9]], Output: [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
-assert Solution.transpose([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+def unit_tests():
+    # Input: [[1, 2, 3], [4, 5, 6], [7, 8, 9]], Output: [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+    assert Solution.transpose([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
 
-# Input: [[1, 2, 3], [4, 5, 6]], Output: [[1, 4], [2, 5], [3, 6]]
-assert Solution.transpose([[1, 2, 3], [4, 5, 6]]) == [[1, 4], [2, 5], [3, 6]]
+    # Input: [[1, 2, 3], [4, 5, 6]], Output: [[1, 4], [2, 5], [3, 6]]
+    assert Solution.transpose([[1, 2, 3], [4, 5, 6]]) == [[1, 4], [2, 5], [3, 6]]
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
