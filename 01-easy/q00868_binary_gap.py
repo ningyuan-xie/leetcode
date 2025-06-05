67360512a6495ede8fff9ee03c519c96a9f1bedb
Ningyuan Xie <howard.xie25@gmail.com>
2025-06-05 00:54:42 -0400

    Update 868

--- 01-easy/q00868_binary_gap.py
+++ 01-easy/q00868_binary_gap.py
@@ -1,15 +1,14 @@
 """868. Binary Gap
 Link: https://leetcode.com/problems/binary-gap/
 Difficulty: Easy
-Description: Given a positive integer n, find and return the longest distance between any two
-adjacent 1's in the binary representation of n. If there are no two adjacent 1's, return 0."""
+Description: Given a positive integer n, find and return the longest distance between any two adjacent 1's in the binary representation of n. If there are no two adjacent 1's, return 0.
+Two 1's are adjacent if there are only 0's separating them (possibly no 0's). The distance between two 1's is the absolute difference between their bit positions. For example, the two 1's in "1001" have a distance of 3."""
 
 
 class Solution:
     @staticmethod
     def binaryGap(n: int) -> int:
-        """Optimal Solution: Bit Manipulation. Time Complexity: O(log(n)), Space Complexity: O(1).
-           Bitwise AND operator & : 1 & 1 = 1, 1 & 0 = 0, 0 & 0 = 0"""
+        """Optimal Solution: Bit Manipulation. Time Complexity: O(log(n)), Space Complexity: O(1)."""
         # Initialize the longest distance between two adjacent 1's
         longest_distance = 0
         # Initialize the previous position of 1 as -1
@@ -28,16 +27,20 @@ class Solution:
         return longest_distance
 
 
-# Input: 22, Output: 2. Explanation: 22 in binary is 10110, so the longest distance is 2
-assert Solution.binaryGap(22) == 2
+def unit_tests():
+    # Input: 22, Output: 2. Explanation: 22 in binary is 10110, so the longest distance is 2
+    assert Solution.binaryGap(22) == 2
 
-# Input: 5, Output: 2. Explanation: 5 in binary is 101, so the longest distance is 2
-assert Solution.binaryGap(5) == 2
+    # Input: 5, Output: 2. Explanation: 5 in binary is 101, so the longest distance is 2
+    assert Solution.binaryGap(5) == 2
 
-# Input: 6, Output: 1. Explanation: 6 in binary is 110, so the longest distance is 1
-assert Solution.binaryGap(6) == 1
+    # Input: 6, Output: 1. Explanation: 6 in binary is 110, so the longest distance is 1
+    assert Solution.binaryGap(6) == 1
 
-# Input: 8, Output: 0. Explanation: 8 in binary is 1000, so there are no two adjacent 1's
-assert Solution.binaryGap(8) == 0
+    # Input: 8, Output: 0. Explanation: 8 in binary is 1000, so there are no two adjacent 1's
+    assert Solution.binaryGap(8) == 0
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
