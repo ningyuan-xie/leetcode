b80ae6255e0b78f1f8f0ffb50f0eabfb60b05581
Ningyuan Xie <howard.xie25@gmail.com>
2025-06-06 01:36:23 -0400

    Update 876

--- 01-easy/q00876_middle_of_the_linked_list.py
+++ 01-easy/q00876_middle_of_the_linked_list.py
@@ -11,7 +11,7 @@ class Solution:
     @staticmethod
     def middleNode(head: ListNode) -> ListNode:
         """Optimal Solution: Two Pointers. Time Complexity: O(n), Space Complexity: O(1).
-           Similar to 0141-Linked-List-Cycle.py and 0234-Palindrome-Linked-List.py"""
+        Similar to 141. Linked List Cycle and 234. Palindrome Linked List."""
         # Initialize two pointers: slow and fast
         slow = fast = head
         # Traverse the linked list with two pointers
@@ -22,14 +22,18 @@ class Solution:
         return slow
 
 
-# Input: head = [1,2,3,4,5], Output: [3,4,5]
-head_test = ListNode.build_linked_list([1, 2, 3, 4, 5])
-head_expected = ListNode.build_linked_list([3, 4, 5])
-assert Solution.middleNode(head_test) == head_expected
+def unit_tests():
+    # Input: head = [1,2,3,4,5], Output: [3,4,5]
+    head = ListNode.build_linked_list([1, 2, 3, 4, 5])
+    output = ListNode.build_linked_list([3, 4, 5])
+    assert Solution.middleNode(head) == output
 
-# Input: head = [1,2,3,4,5,6], Output: [4,5,6]
-head_test = ListNode.build_linked_list([1, 2, 3, 4, 5, 6])
-head_expected = ListNode.build_linked_list([4, 5, 6])
-assert Solution.middleNode(head_test) == head_expected
+    # Input: head = [1,2,3,4,5,6], Output: [4,5,6]
+    head = ListNode.build_linked_list([1, 2, 3, 4, 5, 6])
+    output = ListNode.build_linked_list([4, 5, 6])
+    assert Solution.middleNode(head) == output
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
