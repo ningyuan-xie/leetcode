dcd471a3bb985f3431c76ebd5adba8afc2370bb3
Ningyuan Xie <howard.xie25@gmail.com>
2025-06-10 19:57:32 -0400

    Update 905

--- 01-easy/q00905_sort_array_by_parity.py
+++ 01-easy/q00905_sort_array_by_parity.py
@@ -1,8 +1,8 @@
 """905. Sort Array By Parity
 Link: https://leetcode.com/problems/sort-array-by-parity/
 Difficulty: Easy
-Description: Given an integer array nums, move all the even integers at the beginning of the array
-followed by all the odd integers. Return any array that satisfies this condition."""
+Description: Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+Return any array that satisfies this condition."""
 
 from typing import List
 
@@ -27,13 +27,17 @@ class Solution:
         return nums
 
 
-# Input: nums = [3,1,2,4], Output: [4,2,1,3]
-assert Solution.sortArrayByParity([3, 1, 2, 4]) == [4, 2, 1, 3]
+def unit_tests():
+    # Input: nums = [3,1,2,4], Output: [4,2,1,3]
+    assert Solution.sortArrayByParity([3, 1, 2, 4]) == [4, 2, 1, 3]
 
-# Input: nums = [0], Output: [0]
-assert Solution.sortArrayByParity([0]) == [0]
+    # Input: nums = [0], Output: [0]
+    assert Solution.sortArrayByParity([0]) == [0]
 
-# Input: nums = [1], Output: [1]
-assert Solution.sortArrayByParity([1]) == [1]
+    # Input: nums = [1], Output: [1]
+    assert Solution.sortArrayByParity([1]) == [1]
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
