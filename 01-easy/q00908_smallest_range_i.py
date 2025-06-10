03e260b56b4f0d243afbd2cf98850ab56a36ffd6
Ningyuan Xie <howard.xie25@gmail.com>
2025-06-10 00:48:49 -0400

    Update 908

--- 01-easy/q00908_smallest_range_i.py
+++ 01-easy/q00908_smallest_range_i.py
@@ -2,12 +2,9 @@
 Link: https://leetcode.com/problems/smallest-range-i/
 Difficulty: Easy
 Description: You are given an integer array nums and an integer k.
-In one operation, you can choose any index i where 0 <= i < nums.length and change nums[i] to
-nums[i] + x where x is an integer from the range [-k, k]. You can apply this operation at most
-once for each index i.
+In one operation, you can choose any index i where 0 <= i < nums.length and change nums[i] to nums[i] + x where x is an integer from the range [-k, k]. You can apply this operation at most once for each index i.
 The score of nums is the difference between the maximum and minimum elements in nums.
-Return the minimum score of nums after applying the mentioned operation at most once for each
-index in it."""
+Return the minimum score of nums after applying the mentioned operation at most once for each index in it."""
 
 from typing import List
 
@@ -23,13 +20,17 @@ class Solution:
         return max(0, diff - 2 * k)
 
 
-# Input: nums = [1], k = 0, Output: 0
-assert Solution.smallestRangeI([1], 0) == 0
+def unit_tests():
+    # Input: nums = [1], k = 0, Output: 0
+    assert Solution.smallestRangeI([1], 0) == 0
 
-# Input: nums = [0, 10], k = 2, Output: 6
-assert Solution.smallestRangeI([0, 10], 2) == 6
+    # Input: nums = [0, 10], k = 2, Output: 6
+    assert Solution.smallestRangeI([0, 10], 2) == 6
 
-# Input: nums = [1, 3, 6], k = 3, Output: 0
-assert Solution.smallestRangeI([1, 3, 6], 3) == 0
+    # Input: nums = [1, 3, 6], k = 3, Output: 0
+    assert Solution.smallestRangeI([1, 3, 6], 3) == 0
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
