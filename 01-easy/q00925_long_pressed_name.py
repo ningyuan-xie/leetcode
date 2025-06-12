610f1f4e7c635e2202e37bfaa3b093a516bd04a4
Ningyuan Xie <howard.xie25@gmail.com>
2025-06-12 00:47:01 -0400

    Update 925

--- 01-easy/q00925_long_pressed_name.py
+++ 01-easy/q00925_long_pressed_name.py
@@ -1,31 +1,28 @@
 """925. Long Pressed Name
 Link: https://leetcode.com/problems/long-pressed-name/
 Difficulty: Easy
-Description: Your friend is typing his name into a keyboard. Sometimes, when typing a character c,
-the key might get long pressed, and the character will be typed 1 or more times.
-You examine the typed characters of the keyboard. Return True if it is possible that it was your
-friends name, with some characters (possibly none) being long pressed."""
+Description: Your friend is typing his name into a keyboard. Sometimes, when typing a character c, the key might get long pressed, and the character will be typed 1 or more times.
+You examine the typed characters of the keyboard. Return True if it is possible that it was your friends name, with some characters (possibly none) being long pressed."""
 
 
 class Solution:
     @staticmethod
     def isLongPressedName(name: str, typed: str) -> bool:
         """Optimal Solution: Two Pointers. Time Complexity: O(n), Space Complexity: O(1).
-           Similar to 0922-Sort-Array-By-Parity-II.py"""
+        Similar to 922. Sort Array By Parity II."""
         # Initialize the two pointers
         name_pointer, typed_pointer = 0, 0
 
         # Loop through the typed string because it is the longer string
         while typed_pointer < len(typed):
-            # If name_pointer is within bounds and the characters at the two pointers are the same
+            # If name_pointer is within bounds and name char == typed char
             if name_pointer < len(name) and name[name_pointer] == typed[typed_pointer]:
                 name_pointer += 1
                 typed_pointer += 1
-            # If the chars at the two pointers are different, but the typed char is the same as the
-            # previous typed char, only move the typed pointer to the right
+            # If name char != typed char, but typed char == previous typed char
             elif typed_pointer > 0 and typed[typed_pointer] == typed[typed_pointer - 1]:
                 typed_pointer += 1
-            # Otherwise, return False
+            # Otherwise, name char != typed char, and typed char != previous typed char
             else:
                 return False
 
@@ -33,23 +30,26 @@ class Solution:
         return name_pointer == len(name) and typed_pointer == len(typed)
 
 
-# Input: name = "alex", typed = "aaleex", Output: True
-assert Solution.isLongPressedName("alex", "aaleex") is True
+def unit_tests():
+    # Input: name = "alex", typed = "aaleex", Output: True
+    assert Solution.isLongPressedName("alex", "aaleex") is True
 
-# Input: name = "saeed", typed = "ssaaedd", Output: False
-assert Solution.isLongPressedName("saeed", "ssaaedd") is False
+    # Input: name = "saeed", typed = "ssaaedd", Output: False
+    assert Solution.isLongPressedName("saeed", "ssaaedd") is False
 
-# Input: name = "leelee", typed = "lleeelee", Output: True
-assert Solution.isLongPressedName("leelee", "lleeelee") is True
+    # Input: name = "leelee", typed = "lleeelee", Output: True
+    assert Solution.isLongPressedName("leelee", "lleeelee") is True
 
-# Input: name = "laiden", typed = "laiden", Output: True
-assert Solution.isLongPressedName("laiden", "laiden") is True
+    # Input: name = "laiden", typed = "laiden", Output: True
+    assert Solution.isLongPressedName("laiden", "laiden") is True
 
-# Input: name = "vtkgn", typed = "vttkgnn", Output: True
-assert Solution.isLongPressedName("vtkgn", "vttkgnn") is True
+    # Input: name = "vtkgn", typed = "vttkgnn", Output: True
+    assert Solution.isLongPressedName("vtkgn", "vttkgnn") is True
 
-# Input: name = "alex", typed = "aaleexa", Output: False
-assert Solution.isLongPressedName("alex", "aaleexa") is False
+    # Input: name = "alex", typed = "aaleexa", Output: False
+    assert Solution.isLongPressedName("alex", "aaleexa") is False
 
-print("All unit tests are passed.")
 
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
