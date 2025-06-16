c16ff4486d7f644cc2fc6b5a82659a8d893751d9
Ningyuan Xie <howard.xie25@gmail.com>
2025-06-16 00:47:02 -0400

    Update 961

--- 01-easy/q00961_n_repeated_element_in_size_2n_array.py
+++ 01-easy/q00961_n_repeated_element_in_size_2n_array.py
@@ -2,9 +2,9 @@
 Link: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
 Difficulty: Easy
 Description: You are given an integer array nums with the following properties:
-nums.length == 2 * n.
-nums contains n + 1 unique elements.
-Exactly one element of nums is repeated n times.
+• nums.length == 2 * n.
+• nums contains n + 1 unique elements.
+• Exactly one element of nums is repeated n times.
 Return the element that is repeated n times."""
 
 from typing import List
@@ -13,7 +13,7 @@ from typing import List
 class Solution:
     @staticmethod
     def repeatedNTimes(nums: List[int]) -> int:
-        """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(1)."""
+        """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(n)."""
         # Initialize the hash table to store the frequency of each element in the array
         freq_map = {}
 
@@ -29,13 +29,17 @@ class Solution:
         return -1
 
 
-# Input: [1,2,3,3], Output: 3
-assert Solution.repeatedNTimes([1, 2, 3, 3]) == 3
+def unit_tests():
+    # Input: [1,2,3,3], Output: 3
+    assert Solution.repeatedNTimes([1, 2, 3, 3]) == 3
 
-# Input: [2,1,2,5,3,2], Output: 2
-assert Solution.repeatedNTimes([2, 1, 2, 5, 3, 2]) == 2
+    # Input: [2,1,2,5,3,2], Output: 2
+    assert Solution.repeatedNTimes([2, 1, 2, 5, 3, 2]) == 2
 
-# Input: [5,1,5,2,5,3,5,4], Output: 5
-assert Solution.repeatedNTimes([5, 1, 5, 2, 5, 3, 5, 4]) == 5
+    # Input: [5,1,5,2,5,3,5,4], Output: 5
+    assert Solution.repeatedNTimes([5, 1, 5, 2, 5, 3, 5, 4]) == 5
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
