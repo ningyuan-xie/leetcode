5fb04edc729e7adeda6ba02be5d4bcc68b908287
Ningyuan Xie <howard.xie25@gmail.com>
2025-06-18 00:02:17 -0400

    Update 977

--- 01-easy/q00977_squares_of_a_sorted_array.py
+++ 01-easy/q00977_squares_of_a_sorted_array.py
@@ -1,8 +1,8 @@
 """977. Squares of a Sorted Array
 Link: https://leetcode.com/problems/squares-of-a-sorted-array/
 Difficulty: Easy
-Description: Given an integer array nums sorted in non-decreasing order, return an array of the
-squares of each number sorted in non-decreasing order."""
+Description: Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?"""
 
 from typing import List
 
@@ -11,35 +11,29 @@ class Solution:
     @staticmethod
     def sortedSquares(nums: List[int]) -> List[int]:
         """Optimal Solution: Two Pointers. Time Complexity: O(n), Space Complexity: O(n)."""
-        # Initialize the result array
         result = [0] * len(nums)
-
-        # Initialize the left and right pointers
         left, right = 0, len(nums) - 1
 
         # Loop through the result array in reverse order
         for i in range(len(nums) - 1, -1, -1):
-            # If the absolute value of the left pointer is greater than the right pointer
             if abs(nums[left]) > abs(nums[right]):
-                # Set the result array to the square of the left pointer
-                result[i] = nums[left] * nums[left]
-                # Move the left pointer to the right
+                result[i] = nums[left] ** 2
                 left += 1
-            # Otherwise
             else:
-                # Set the result array to the square of the right pointer
-                result[i] = nums[right] * nums[right]
-                # Move the right pointer to the left
+                result[i] = nums[right] ** 2
                 right -= 1
 
-        # Return the result array
         return result
 
 
-# Input: [-4,-1,0,3,10], Output: [0,1,9,16,100]
-assert Solution.sortedSquares([-4, -1, 0, 3, 10]) == [0, 1, 9, 16, 100]
+def unit_tests():
+    # Input: [-4,-1,0,3,10], Output: [0,1,9,16,100]
+    assert Solution.sortedSquares([-4, -1, 0, 3, 10]) == [0, 1, 9, 16, 100]
 
-# Input: [-7,-3,2,3,11], Output: [4,9,9,49,121]
-assert Solution.sortedSquares([-7, -3, 2, 3, 11]) == [4, 9, 9, 49, 121]
+    # Input: [-7,-3,2,3,11], Output: [4,9,9,49,121]
+    assert Solution.sortedSquares([-7, -3, 2, 3, 11]) == [4, 9, 9, 49, 121]
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
