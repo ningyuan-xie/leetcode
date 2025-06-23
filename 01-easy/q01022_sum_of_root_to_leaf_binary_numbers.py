9ae56b012ce5cccbd68a4b8ba838e5943d428912
Ningyuan Xie <howard.xie25@gmail.com>
2025-06-23 01:04:19 -0400

    Update 1022

--- 01-easy/q01022_sum_of_root_to_leaf_binary_numbers.py
+++ 01-easy/q01022_sum_of_root_to_leaf_binary_numbers.py
@@ -1,12 +1,9 @@
 """1022. Sum of Root To Leaf Binary Numbers
 Link: https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers
 Difficulty: Easy
-Description: You are given the root of a binary tree where each node has a value 0 or 1.
-Each root-to-leaf path represents a binary number starting with the most significant bit.
-For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary,
-which is 13.
-For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
-Return the sum of these numbers.
+Description: You are given the root of a binary tree where each node has a value 0 or 1. Each root-to-leaf path represents a binary number starting with the most significant bit.
+• For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
+For all leaves in the tree, consider the numbers represented by the path from the root to that leaf. Return the sum of these numbers.
 The test cases are generated so that the answer fits in a 32-bits integer."""
 
 from typing import Optional
@@ -16,17 +13,17 @@ from package.data_structures import TreeNode
 class Solution:
     @staticmethod
     def sumRootToLeaf(root: Optional[TreeNode]) -> int:
-        """Optimal Solution: Preorder DFS. Time Complexity: O(n), Space Complexity: O(h).
-           Similar to 1018-Binary-Prefix-Divisible-By-5.py"""
+        """Optimal Solution: Preorder DFS. Time Complexity: O(n), Space Complexity: O(n).
+        Similar to 1018. Binary Prefix Divisible By 5."""
 
         def preorder_dfs(node: Optional[TreeNode], current_sum: int) -> int:
-            """Helper function to perform preorder DFS"""
+            """Helper function to perform preorder DFS."""
             # If the node is None, return 0
             if not node:
                 return 0
 
-            # Update the current sum; (current_sum << 1) is equivalent current_sum * 2
-            current_sum = (current_sum << 1) + node.val
+            # Update the current sum
+            current_sum = current_sum * 2 + node.val
 
             # If it's a leaf node, return the current sum
             if not node.left and not node.right:
@@ -38,16 +35,20 @@ class Solution:
         return preorder_dfs(root, 0)
 
 
-# Unit Test: root = [1, 0, 1, 0, 1, 0, 1], Output: 22
-root_test = TreeNode.build_binary_tree([1, 0, 1, 0, 1, 0, 1])
-assert Solution.sumRootToLeaf(root_test) == 22
+def unit_tests():
+    # Unit Test: root = [1, 0, 1, 0, 1, 0, 1], Output: 22
+    root = TreeNode.build_binary_tree([1, 0, 1, 0, 1, 0, 1])
+    assert Solution.sumRootToLeaf(root) == 22
 
-# Unit Test: root = [1, 1, 0, 0, 1, 0, 1], Output: 22
-root_test = TreeNode.build_binary_tree([1, 1, 0, 0, 1, 0, 1])
-assert Solution.sumRootToLeaf(root_test) == 22
+    # Unit Test: root = [1, 1, 0, 0, 1, 0, 1], Output: 22
+    root = TreeNode.build_binary_tree([1, 1, 0, 0, 1, 0, 1])
+    assert Solution.sumRootToLeaf(root) == 22
 
-# Unit Test: root = [1], Output: 1
-root_test = TreeNode.build_binary_tree([1])
-assert Solution.sumRootToLeaf(root_test) == 1
+    # Unit Test: root = [1], Output: 1
+    root = TreeNode.build_binary_tree([1])
+    assert Solution.sumRootToLeaf(root) == 1
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
