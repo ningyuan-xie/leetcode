f771962fdf65d7a697a9fe38121baf4b36fb5412
Ningyuan Xie <howard.xie25@gmail.com>
2025-06-24 01:28:03 -0400

    Update 1025

--- 01-easy/q01025_divisor_game.py
+++ 01-easy/q01025_divisor_game.py
@@ -2,10 +2,9 @@
 Link: https://leetcode.com/problems/divisor-game/
 Difficulty: Easy
 Description: Alice and Bob take turns playing a game, with Alice starting first.
-Initially, there is a number n on the chalkboard.
-On each player's turn, that player makes a move consisting of:
-Choosing any x with 0 < x < n and n % x == 0.
-Replacing the number n on the chalkboard with n - x.
+Initially, there is a number n on the chalkboard. On each player's turn, that player makes a move consisting of:
+• Choosing any x with 0 < x < n and n % x == 0.
+• Replacing the number n on the chalkboard with n - x.
 Also, if a player cannot make a move, they lose the game.
 Return true if and only if Alice wins the game, assuming both players play optimally."""
 
@@ -18,16 +17,17 @@ class Solution:
         return n % 2 == 0
 
 
-# Unit Test: n = 2, Output: True.
-# Explanation: Alice chooses 1, Bob cannot make a move.
-assert Solution.divisorGame(2) is True
+def unit_tests():
+    # Unit Test: n = 2, Output: True
+    assert Solution.divisorGame(2) is True
 
-# Unit Test: n = 3, Output: False.
-# Explanation: Alice chooses 1, Bob chooses 1, Alice cannot make a move.
-assert Solution.divisorGame(3) is False
+    # Unit Test: n = 3, Output: False
+    assert Solution.divisorGame(3) is False
 
-# Unit Test: n = 4, Output: True.
-# Explanation: Alice chooses 1, Bob chooses 1, Alice chooses 1, Bob cannot make a move.
-assert Solution.divisorGame(4) is True
+    # Unit Test: n = 4, Output: True
+    assert Solution.divisorGame(4) is True
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
