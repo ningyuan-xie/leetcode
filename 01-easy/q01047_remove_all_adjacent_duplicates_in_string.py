1c274a8be5bf5422da3076c5f3127e630f953bd8
Ningyuan Xie <howard.xie25@gmail.com>
2025-06-26 00:14:57 -0400

    Update 1047

--- 01-easy/q01047_remove_all_adjacent_duplicates_in_string.py
+++ 01-easy/q01047_remove_all_adjacent_duplicates_in_string.py
@@ -1,39 +1,39 @@
 """1047. Remove All Adjacent Duplicates In String
 Link: https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
 Difficulty: Easy
-Description: You are given a string s consisting of lowercase English letters.
-A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+Description: You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
 We repeatedly make duplicate removals on s until we no longer can.
-Return the final string after all such duplicate removals have been made.
-It can be proven that the answer is unique."""
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique."""
 
 
 class Solution:
     @staticmethod
     def removeDuplicates(s: str) -> str:
         """Optimal Solution: Stack. Time Complexity: O(n), Space Complexity: O(n)."""
-        # Initialize the stack
         stack = []
 
-        # Iterate through the string
         for char in s:
             # If the stack is not empty and the top of the stack is equal to the current character
             if stack and stack[-1] == char:
-                stack.pop()  # Remove the top of the stack
+                stack.pop()
             else:
-                stack.append(char)  # Add the current character to the stack
+                stack.append(char)
 
         # Return the final string by joining the stack
         return "".join(stack)
 
 
-# Input: s = "abbaca", Output: "ca"
-assert Solution.removeDuplicates("abbaca") == "ca"
+def unit_tests():
+    # Input: s = "abbaca", Output: "ca"
+    assert Solution.removeDuplicates("abbaca") == "ca"
 
-# Input: s = "azxxzy", Output: "ay"
-assert Solution.removeDuplicates("azxxzy") == "ay"
+    # Input: s = "azxxzy", Output: "ay"
+    assert Solution.removeDuplicates("azxxzy") == "ay"
 
-# Input: s = "a", Output: "a"
-assert Solution.removeDuplicates("a") == "a"
+    # Input: s = "a", Output: "a"
+    assert Solution.removeDuplicates("a") == "a"
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
