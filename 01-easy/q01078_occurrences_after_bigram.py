80362e3178ad150c6ab9f70d0a32117f69c1676d
Ningyuan Xie <howard.xie25@gmail.com>
2025-06-27 02:24:33 -0400

    Update 1078

--- 01-easy/q01078_occurrences_after_bigram.py
+++ 01-easy/q01078_occurrences_after_bigram.py
@@ -1,9 +1,7 @@
 """1078. Occurrences After Bigram
 Link: https://leetcode.com/problems/occurrences-after-bigram/
 Difficulty: Easy
-Description: Given two strings first and second, consider occurrences in some text of the form
-"first second third", where second comes immediately after first, and third comes immediately
-after second.
+Description: Given two strings first and second, consider occurrences in some text of the form "first second third", where second comes immediately after first, and third comes immediately after second.
 Return an array of all the words third for each occurrence of "first second third"."""
 
 from typing import List
@@ -12,7 +10,7 @@ from typing import List
 class Solution:
     @staticmethod
     def findOcurrences(text: str, first: str, second: str) -> List[str]:
-        """Optimal Solution: One Pass. Time Complexity: O(n), Space Complexity: O(n)."""
+        """Optimal Solution: String Manipulation. Time Complexity: O(n), Space Complexity: O(n)."""
         words = text.split()
         n, result = len(words), []
 
@@ -23,13 +21,14 @@ class Solution:
         return result
 
 
-# Input: text = "alice is a good girl she is a good student", first = "a", second = "good",
-# Output: ["girl", "student"]
-assert Solution.findOcurrences("alice is a good girl she is a good student",
-                               "a", "good") == ["girl", "student"]
+def unit_tests():
+    # Input: text = "alice is a good girl she is a good student", first = "a", second = "good", Output: ["girl", "student"]
+    assert Solution.findOcurrences("alice is a good girl she is a good student", "a", "good") == ["girl", "student"]
 
-# Input: text = "we will we will rock you", first = "we", second = "will", Output: ["we","rock"]
-assert Solution.findOcurrences("we will we will rock you",
-                               "we", "will") == ["we", "rock"]
+    # Input: text = "we will we will rock you", first = "we", second = "will", Output: ["we","rock"]
+    assert Solution.findOcurrences("we will we will rock you", "we", "will") == ["we", "rock"]
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
