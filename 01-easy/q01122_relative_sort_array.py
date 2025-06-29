b93a40953a354d0198ce4344f949aa7131ba301b
Ningyuan Xie <howard.xie25@gmail.com>
2025-06-29 01:31:17 -0400

    Update 1122

--- 01-easy/q01122_relative_sort_array.py
+++ 01-easy/q01122_relative_sort_array.py
@@ -1,10 +1,8 @@
 """1122. Relative Sort Array
 Link: https://leetcode.com/problems/relative-sort-array/
 Difficulty: Easy
-Description: Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in
-arr2 are also in arr1.
-Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2.
-Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order."""
+Description: Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order."""
 
 from typing import List
 
@@ -12,7 +10,7 @@ from typing import List
 class Solution:
     @staticmethod
     def relativeSortArray(arr1: List[int], arr2: List[int]) -> List[int]:
-        """Optimal Solution: Sort by Custom Order. Time Complexity: O(n), Space Complexity: O(n)."""
+        """Optimal Solution: Hash Table. Time Complexity: O(n), Space Complexity: O(n)."""
         # Count the occurrences of each element in arr1
         count = {}
         for num in arr1:
@@ -24,7 +22,7 @@ class Solution:
         # Sort the elements in arr1 based on the order in arr2
         for num in arr2:
             sorted_arr.extend([num] * count[num])  # [2, 2, 2, 1, 4, 3, 3, 9, 6]
-            count.pop(num)  # .pop() removes the key from the dictionary; left with {7: 1, 19: 1}
+            count.pop(num)  # .pop() removes the item from the dictionary; left with {7: 1, 19: 1}
 
         # Sort the remaining elements in arr1 that are not in arr2
         for num in sorted(count.keys()):
@@ -33,13 +31,14 @@ class Solution:
         return sorted_arr
 
 
-# Input: arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19], arr2 = [2, 1, 4, 3, 9, 6],
-# Output: [2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19]
-assert Solution.relativeSortArray([2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19],
-                                  [2, 1, 4, 3, 9, 6]) == [2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19]
+def unit_tests():
+    # Input: arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19], arr2 = [2, 1, 4, 3, 9, 6], Output: [2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19]
+    assert Solution.relativeSortArray([2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19], [2, 1, 4, 3, 9, 6]) == [2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19]
 
-# Input: arr1 = [28,6,22,8,44,17], arr2 = [22,28,8,6], Output: [22, 28, 8, 6, 17, 44]
-assert Solution.relativeSortArray([28, 6, 22, 8, 44, 17],
-                                  [22, 28, 8, 6]) == [22, 28, 8, 6, 17, 44]
+    # Input: arr1 = [28,6,22,8,44,17], arr2 = [22,28,8,6], Output: [22, 28, 8, 6, 17, 44]
+    assert Solution.relativeSortArray([28, 6, 22, 8, 44, 17], [22, 28, 8, 6]) == [22, 28, 8, 6, 17, 44]
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
