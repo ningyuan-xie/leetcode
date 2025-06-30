a3580655a9dc5d44bbae6042415440acd310e2f2
Ningyuan Xie <howard.xie25@gmail.com>
2025-06-30 01:24:32 -0400

    Update 1128

--- 01-easy/q01128_number_of_equivalent_domino_pairs.py
+++ 01-easy/q01128_number_of_equivalent_domino_pairs.py
@@ -1,11 +1,8 @@
 """1128. Number of Equivalent Domino Pairs
 Link: https://leetcode.com/problems/number-of-equivalent-domino-pairs/
 Difficulty: Easy
-Description: Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] = [c, d] if
-and only if either (a == c and b == d), or (a == d and b == c) - that is, one domino can be rotated
-to be equal to another domino.
-Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is
-equivalent to dominoes[j]."""
+Description: Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] = [c, d] if and only if either (a == c and b == d), or (a == d and b == c) - that is, one domino can be rotated to be equal to another domino.
+Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is equivalent to dominoes[j]."""
 
 from typing import List
 
@@ -19,29 +16,29 @@ class Solution:
 
         # Iterate through the list of dominoes
         for domino in dominoes:
-            # Sort the domino to ensure the internal order does not matter
-            domino.sort()  # [1, 2] -> [1, 2], [2, 1] -> [1, 2]
-
-            # Convert the sorted domino to tuple to use as key in the dictionary (list is unhashable)
-            domino_key = tuple(domino)  # [1, 2] -> (1, 2)
+            # Create a normalized key by taking min and max to avoid sorting
+            domino_key = (min(domino[0], domino[1]), max(domino[0], domino[1]))
 
             # Increment the count of equivalent domino pairs
-            num_pairs += count.get(domino_key, 0)  # 0 + 1 + 2 + 3 + 4 + 5 = 15
+            num_pairs += count.get(domino_key, 0)
 
             # Increment the count of this domino in the dictionary
-            count[domino_key] = count.get(domino_key, 0) + 1  # {(1, 2): 6}
+            count[domino_key] = count.get(domino_key, 0) + 1
 
         return num_pairs
 
 
-# Input: dominoes = [[1, 2], [2, 1], [3, 4], [5, 6]], Output: 1
-assert Solution.numEquivDominoPairs([[1, 2], [2, 1], [3, 4], [5, 6]]) == 1
+def unit_tests():
+    # Input: dominoes = [[1, 2], [2, 1], [3, 4], [5, 6]], Output: 1
+    assert Solution.numEquivDominoPairs([[1, 2], [2, 1], [3, 4], [5, 6]]) == 1
 
-# Input: dominoes = [[1, 2], [2, 1], [1, 2], [2, 1], [1, 2], [2, 1]], Output: 15
-assert Solution.numEquivDominoPairs([[1, 2], [2, 1], [1, 2], [2, 1], [1, 2], [2, 1]]) == 15
+    # Input: dominoes = [[1, 2], [2, 1], [1, 2], [2, 1], [1, 2], [2, 1]], Output: 15
+    assert Solution.numEquivDominoPairs([[1, 2], [2, 1], [1, 2], [2, 1], [1, 2], [2, 1]]) == 15
 
-# Input: dominoes = [[1, 2], [2, 1], [1, 2], [2, 1], [1, 2], [2, 1], [1, 2], [2, 1]], Output: 28
-assert (Solution.numEquivDominoPairs([[1, 2], [2, 1], [1, 2], [2, 1], [1, 2], [2, 1], [1, 2], [2, 1]])
-        == 28)
+    # Input: dominoes = [[1, 2], [2, 1], [1, 2], [2, 1], [1, 2], [2, 1], [1, 2], [2, 1]], Output: 28
+    assert Solution.numEquivDominoPairs([[1, 2], [2, 1], [1, 2], [2, 1], [1, 2], [2, 1], [1, 2], [2, 1]]) == 28
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
