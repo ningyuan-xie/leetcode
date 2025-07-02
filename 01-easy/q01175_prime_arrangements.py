da5a0f9daf951c489382ac6e619afc6c2373f5b7
Ningyuan Xie <howard.xie25@gmail.com>
2025-07-02 02:08:46 -0400

    Update 1175

--- 01-easy/q01175_prime_arrangements.py
+++ 01-easy/q01175_prime_arrangements.py
@@ -1,18 +1,15 @@
 """1175. Prime Arrangements
 Link: https://leetcode.com/problems/prime-arrangements/
 Difficulty: Easy
-Description: Return the number of permutations of 1 to n so that prime numbers are at prime indices
-(1-indexed.)
-(Recall that an integer is prime if and only if it is greater than 1, and cannot be written as a
-product of two positive integers both smaller than it.)
+Description: Return the number of permutations of 1 to n so that prime numbers are at prime indices (1-indexed.)
+(Recall that an integer is prime if and only if it is greater than 1, and cannot be written as a product of two positive integers both smaller than it.)
 Since the answer may be large, return the answer modulo 10^9 + 7."""
 
 
 class Solution:
     @staticmethod
     def numPrimeArrangements(n: int) -> int:
-        """Optimal Solution: Sieve of Eratosthenes (used to find all prime numbers less than n).
-           Time Complexity: O(n * log(log(n))), Space Complexity: O(n)."""
+        """Optimal Solution: Sieve of Eratosthenes (used to find all prime numbers less than n). Time Complexity: O(nlog(log(n))), Space Complexity: O(n)."""
         # Initialize the modulo and the prime numbers from 0 to n
         MOD, prime = 10 ** 9 + 7, [True] * (n + 1)
 
@@ -42,13 +39,17 @@ class Solution:
         return (prime_permutation * non_prime_permutation) % MOD
 
 
-# Input: n = 5, Output: 12
-# Explanation: prime numbers = [2, 3, 5], non-prime numbers = [1, 4]
-# prime permutations = 3!, non-prime permutations = 2!, total permutations = 3! * 2! = 12
-assert Solution.numPrimeArrangements(5) == 12
+def unit_tests():
+    # Input: n = 5, Output: 12
+    # Explanation: prime numbers = [2, 3, 5], non-prime numbers = [1, 4]
+    # prime permutations = 3!, non-prime permutations = 2!, total permutations = 3! * 2! = 12
+    assert Solution.numPrimeArrangements(5) == 12
 
-# Input: n = 100, Output: 682289015
-# prime permutations = 25!, non-prime permutations = 75!, total permutations = 25! * 75! = 682289015
-assert Solution.numPrimeArrangements(100) == 682289015
+    # Input: n = 100, Output: 682289015
+    # prime permutations = 25!, non-prime permutations = 75!, total permutations = 25! * 75! = 682289015
+    assert Solution.numPrimeArrangements(100) == 682289015
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
