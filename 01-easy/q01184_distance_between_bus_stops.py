a2da92c908f0d36dc4d4cf28bfaa37407b35640a
Ningyuan Xie <howard.xie25@gmail.com>
2025-07-02 01:46:43 -0400

    Update 1184

--- 01-easy/q01184_distance_between_bus_stops.py
+++ 01-easy/q01184_distance_between_bus_stops.py
@@ -1,9 +1,7 @@
 """1184. Distance Between Bus Stops
 Link: https://leetcode.com/problems/distance-between-bus-stops/
 Difficulty: Easy
-Description: A bus has n stops numbered from 0 to n - 1 that form a circle. We know the distance
-between all pairs of neighboring stops where distance[i] is the distance between the stops
-number i and (i + 1) % n.
+Description: A bus has n stops numbered from 0 to n - 1 that form a circle. We know the distance between all pairs of neighboring stops where distance[i] is the distance between the stops number i and (i + 1) % n.
 The bus goes along both directions i.e. clockwise and counterclockwise.
 Return the shortest distance between the given start and destination stops."""
 
@@ -13,14 +11,12 @@ from typing import List
 class Solution:
     @staticmethod
     def distanceBetweenBusStops(distance: List[int], start: int, destination: int) -> int:
-        """Optimal Solution: Calculate the total distance and the clockwise distance.
-           Time Complexity: O(n), Space Complexity: O(1)."""
+        """Optimal Solution: Clockwise and Counterclockwise Distance. Time Complexity: O(n), Space Complexity: O(1)."""
         # Calculate the total distance
         total_distance = sum(distance)  # 1 + 2 + 3 + 4 = 10
 
         # Calculate the clockwise distance
-        clockwise_distance = sum(distance[min(start, destination):  # min(0, 1) = 0
-                                          max(start, destination)])  # max(0, 1) = 1
+        clockwise_distance = sum(distance[min(start, destination): max(start, destination)])
 
         # Calculate the counterclockwise distance
         counterclockwise_distance = total_distance - clockwise_distance  # 10 - 1 = 9
@@ -28,13 +24,17 @@ class Solution:
         return min(clockwise_distance, counterclockwise_distance)  # min(1, 9) = 1
 
 
-# Input: distance = [1, 2, 3, 4], start = 0, destination = 1, Output: 1
-assert Solution.distanceBetweenBusStops([1, 2, 3, 4], 0, 1) == 1
+def unit_tests():
+    # Input: distance = [1, 2, 3, 4], start = 0, destination = 1, Output: 1
+    assert Solution.distanceBetweenBusStops([1, 2, 3, 4], 0, 1) == 1
 
-# Input: distance = [1, 2, 3, 4], start = 0, destination = 2, Output: 3
-assert Solution.distanceBetweenBusStops([1, 2, 3, 4], 0, 2) == 3
+    # Input: distance = [1, 2, 3, 4], start = 0, destination = 2, Output: 3
+    assert Solution.distanceBetweenBusStops([1, 2, 3, 4], 0, 2) == 3
 
-# Input: distance = [1, 2, 3, 4], start = 0, destination = 3, Output: 4
-assert Solution.distanceBetweenBusStops([1, 2, 3, 4], 0, 3) == 4
+    # Input: distance = [1, 2, 3, 4], start = 0, destination = 3, Output: 4
+    assert Solution.distanceBetweenBusStops([1, 2, 3, 4], 0, 3) == 4
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
