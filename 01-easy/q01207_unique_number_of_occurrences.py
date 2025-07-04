759c36269bc474bf9ec7514a8bf4f53c0d5ecaa5
Ningyuan Xie <howard.xie25@gmail.com>
2025-07-04 02:27:04 -0400

    Update 1207

--- 01-easy/q01207_unique_number_of_occurrences.py
+++ 01-easy/q01207_unique_number_of_occurrences.py
@@ -1,8 +1,7 @@
 """1207. Unique Number of Occurrences
 Link: https://leetcode.com/problems/unique-number-of-occurrences/
 Difficulty: Easy
-Description: Given an array of integers arr, write a function that returns true if and only if the
-number of occurrences of each value in the array is unique."""
+Description: Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise."""
 
 from typing import List
 
@@ -10,26 +9,27 @@ from typing import List
 class Solution:
     @staticmethod
     def uniqueOccurrences(arr: List[int]) -> bool:
-        """Optimal Solution: Hash Table & Set.
-           Time Complexity: O(n), Space Complexity: O(n)."""
+        """Optimal Solution: Hash Table & Set. Time Complexity: O(n), Space Complexity: O(n)."""
         # Initialize the frequency of each integer in arr
         freq = {}
         for num in arr:
             freq[num] = freq.get(num, 0) + 1
 
         # Check if the number of occurrences of each value in arr is unique
-        # freq.values(): dict_values([3, 2, 1])
         return len(freq) == len(set(freq.values()))
 
 
-# Input: arr = [1, 2, 2, 1, 1, 3], Output: True
-assert Solution.uniqueOccurrences([1, 2, 2, 1, 1, 3]) is True
+def unit_tests():
+    # Input: arr = [1, 2, 2, 1, 1, 3], Output: True
+    assert Solution.uniqueOccurrences([1, 2, 2, 1, 1, 3]) is True
 
-# Input: arr = [1, 2], Output: False
-assert Solution.uniqueOccurrences([1, 2]) is False
+    # Input: arr = [1, 2], Output: False
+    assert Solution.uniqueOccurrences([1, 2]) is False
 
-# Input: arr = [-3, 0, 1, -3, 1, 1, 1, -3, 10, 0], Output: True
-assert Solution.uniqueOccurrences([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]) is True
+    # Input: arr = [-3, 0, 1, -3, 1, 1, 1, -3, 10, 0], Output: True
+    assert Solution.uniqueOccurrences([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]) is True
 
-print("All unit tests are passed.")
 
+if __name__ == "__main__":  
+    unit_tests()
+    print("All unit tests are passed.")
