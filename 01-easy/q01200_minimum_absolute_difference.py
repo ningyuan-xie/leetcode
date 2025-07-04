e31f6a31d36a1daa04748017f481d27d0a24cb49
Ningyuan Xie <howard.xie25@gmail.com>
2025-07-04 02:26:58 -0400

    Update 1200

--- 01-easy/q01200_minimum_absolute_difference.py
+++ 01-easy/q01200_minimum_absolute_difference.py
@@ -1,12 +1,11 @@
 """1200. Minimum Absolute Difference
 Link: https://leetcode.com/problems/minimum-absolute-difference/
 Difficulty: Easy
-Description: Given an array of distinct integers arr, find all pairs of elements with the minimum
-absolute difference of any two elements.
+Description: Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements.
 Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
-- a, b are from arr
-- a < b
-- b - a equals to the minimum absolute difference of any two elements in arr"""
+• a, b are from arr
+• a < b
+• b - a equals to the minimum absolute difference of any two elements in arr"""
 
 from typing import List
 
@@ -16,7 +15,7 @@ class Solution:
     def minimumAbsDifference(arr: List[int]) -> List[List[int]]:
         """Optimal Solution: Sorting. Time Complexity: O(nlog(n)), Space Complexity: O(1)."""
         # Sort the array in ascending order
-        arr.sort()  # E.g. [4, 2, 1, 3] -> [1, 2, 3, 4]
+        arr.sort()
 
         # Initialize the minimum absolute difference
         min_diff = float("inf")
@@ -33,14 +32,17 @@ class Solution:
         return result
 
 
-# Input: arr = [4, 2, 1, 3], Output: [[1, 2], [2, 3], [3, 4]]
-assert Solution.minimumAbsDifference([4, 2, 1, 3]) == [[1, 2], [2, 3], [3, 4]]
+def unit_tests():
+    # Input: arr = [4, 2, 1, 3], Output: [[1, 2], [2, 3], [3, 4]]
+    assert Solution.minimumAbsDifference([4, 2, 1, 3]) == [[1, 2], [2, 3], [3, 4]]
 
-# Input: arr = [1, 3, 6, 10, 15], Output: [[1, 3]]
-assert Solution.minimumAbsDifference([1, 3, 6, 10, 15]) == [[1, 3]]
+    # Input: arr = [1, 3, 6, 10, 15], Output: [[1, 3]]
+    assert Solution.minimumAbsDifference([1, 3, 6, 10, 15]) == [[1, 3]]
 
-# Input: arr = [3, 8, -10, 23, 19, -4, -14, 27], Output: [[-14, -10], [19, 23], [23, 27]]
-assert (Solution.minimumAbsDifference([3, 8, -10, 23, 19, -4, -14, 27])
-        == [[-14, -10], [19, 23], [23, 27]])
+    # Input: arr = [3, 8, -10, 23, 19, -4, -14, 27], Output: [[-14, -10], [19, 23], [23, 27]]
+    assert Solution.minimumAbsDifference([3, 8, -10, 23, 19, -4, -14, 27]) == [[-14, -10], [19, 23], [23, 27]]
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
