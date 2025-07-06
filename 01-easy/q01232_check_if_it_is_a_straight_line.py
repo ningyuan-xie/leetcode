aa7acbbc7c7f8591e6054bdcb7fb292d4032f801
Ningyuan Xie <howard.xie25@gmail.com>
2025-07-06 00:04:24 -0400

    Update 1232

--- 01-easy/q01232_check_if_it_is_a_straight_line.py
+++ 01-easy/q01232_check_if_it_is_a_straight_line.py
@@ -1,9 +1,7 @@
 """1232. Check If It Is a Straight Line
 Link: https://leetcode.com/problems/check-if-it-is-a-straight-line
 Difficulty: Easy
-Description: You are given an array coordinates, coordinates[i] = [x, y], where [x, y]
-represents the coordinate of a point.
-Check if these points make a straight line in the XY plane."""
+Description:You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane."""
 
 from typing import List
 
@@ -12,7 +10,7 @@ class Solution:
     @staticmethod
     def checkStraightLine(coordinates: List[List[int]]) -> bool:
         """Optimal Solution: Slope Calculation. Time Complexity: O(n), Space Complexity: O(1).
-           Similar to 1037-Valid-Boomerang.py"""
+        Similar to 1037. Valid Boomerang."""
         # Calculate the slope of the line between the first two points
         x0, y0 = coordinates[0]
         x1, y1 = coordinates[1]
@@ -29,13 +27,17 @@ class Solution:
         return True
 
 
-# Input: coordinates = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]], Output: True
-assert Solution.checkStraightLine([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]]) is True
+def unit_tests():
+    # Input: coordinates = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]], Output: True
+    assert Solution.checkStraightLine([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]]) is True
 
-# Input: coordinates = [[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]], Output: False
-assert Solution.checkStraightLine([[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]]) is False
+    # Input: coordinates = [[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]], Output: False
+    assert Solution.checkStraightLine([[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]]) is False
 
-# Input: coordinates = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6]], Output: True
-assert Solution.checkStraightLine([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6]]) is True
+    # Input: coordinates = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6]], Output: True
+    assert Solution.checkStraightLine([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6]]) is True
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
