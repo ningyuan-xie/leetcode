f1015a0163408fa70270bb49ab7d72f2b59acbff
Ningyuan Xie <howard.xie25@gmail.com>
2025-07-10 00:03:57 -0400

    1299

--- 01-easy/q01299_replace_elements_with_greatest_element_on_right_side.py
+++ 01-easy/q01299_replace_elements_with_greatest_element_on_right_side.py
@@ -1,8 +1,7 @@
 """1299. Replace Elements with Greatest Element on Right Side
 Link: https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
 Difficulty: Easy
-Description: Given an array arr, replace every element in that array with the greatest element among the
-elements to its right, and replace the last element with -1.
+Description: Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
 After doing so, return the array."""
 
 from typing import List
@@ -17,20 +16,22 @@ class Solution:
 
         # Traverse the array in reverse order
         for i in range(len(arr) - 1, -1, -1):
-            # max_value is updated to the maximum of its current value and the original value
-            # of arr[i] (before it was replaced)
             arr[i], max_value = max_value, max(max_value, arr[i])
 
         return arr
 
 
-# Input: arr = [17, 18, 5, 4, 6, 1], Output: [18, 6, 6, 6, 1, -1]
-assert Solution.replaceElements([17, 18, 5, 4, 6, 1]) == [18, 6, 6, 6, 1, -1]
+def unit_tests():
+    # Input: arr = [17, 18, 5, 4, 6, 1], Output: [18, 6, 6, 6, 1, -1]
+    assert Solution.replaceElements([17, 18, 5, 4, 6, 1]) == [18, 6, 6, 6, 1, -1]
 
-# Input: arr = [400], Output: [-1]
-assert Solution.replaceElements([400]) == [-1]
+    # Input: arr = [400], Output: [-1]
+    assert Solution.replaceElements([400]) == [-1]
 
-# Input: arr = [1, 2, 3, 4, 5], Output: [5, 5, 5, 5, -1]
-assert Solution.replaceElements([1, 2, 3, 4, 5]) == [5, 5, 5, 5, -1]
+    # Input: arr = [1, 2, 3, 4, 5], Output: [5, 5, 5, 5, -1]
+    assert Solution.replaceElements([1, 2, 3, 4, 5]) == [5, 5, 5, 5, -1]
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
