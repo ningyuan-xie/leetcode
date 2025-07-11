7a5c80d37a2d525deddee12eb4a366466a609266
Ningyuan Xie <howard.xie25@gmail.com>
2025-07-11 00:21:35 -0400

    Update 1304

--- 01-easy/q01304_find_n_unique_integers_sum_up_to_zero.py
+++ 01-easy/q01304_find_n_unique_integers_sum_up_to_zero.py
@@ -1,8 +1,7 @@
 """1304. Find N Unique Integers Sum up to Zero
 Link: https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
 Difficulty: Easy
-Description: Given an integer n, return any array containing n unique integers such that they add up
-to 0."""
+Description: Given an integer n, return any array containing n unique integers such that they add up to 0."""
 
 from typing import List
 
@@ -10,12 +9,10 @@ from typing import List
 class Solution:
     @staticmethod
     def sumZero(n: int) -> List[int]:
-        """Optimal Solution: Construct the array with alternating positive and negative numbers.
-           Time Complexity: O(n), Space Complexity: O(n)."""
-        # Initialize the array
+        """Optimal Solution: Alternating Numbers. Time Complexity: O(n), Space Complexity: O(n)."""
         arr = []
 
-        # If n is odd, add 0 to the array
+        # If n is odd, starts with 0
         if n % 2:
             arr.append(0)
 
@@ -26,13 +23,17 @@ class Solution:
         return arr
 
 
-# Input: n = 5, Output: [0, 1, -1, 2, -2]
-assert Solution.sumZero(5) == [0, 1, -1, 2, -2]
+def unit_tests():
+    # Input: n = 5, Output: [0, 1, -1, 2, -2]
+    assert Solution.sumZero(5) == [0, 1, -1, 2, -2]
 
-# Input: n = 3, Output: [0, 1, -1]
-assert Solution.sumZero(3) == [0, 1, -1]
+    # Input: n = 3, Output: [0, 1, -1]
+    assert Solution.sumZero(3) == [0, 1, -1]
 
-# Input: n = 1, Output: [0]
-assert Solution.sumZero(1) == [0]
+    # Input: n = 1, Output: [0]
+    assert Solution.sumZero(1) == [0]
 
-print("All unit tests are passed.")
+
+if __name__ == "__main__":
+    unit_tests()
+    print("All unit tests are passed.")
